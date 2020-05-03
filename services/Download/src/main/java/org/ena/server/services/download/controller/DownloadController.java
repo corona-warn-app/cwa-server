752b758ff2d776607a3a43fa85d04bffccc0a8a3
Michael Burwig <michael.burwig@sap.com>
2020-05-03 17:18:24 +0200

    Add TemporaryExposureKey Persistence Classes

--- services/Download/src/main/java/org/ena/server/services/download/controller/DownloadController.java
+++ services/Download/src/main/java/org/ena/server/services/download/controller/DownloadController.java
@@ -1,5 +1,7 @@
 package org.ena.server.services.download.controller;
 
+import org.ena.server.services.common.persistence.service.TemporaryExposureKeyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1")
 public class DownloadController {
+
+  @Autowired
+  private TemporaryExposureKeyService exposureKeyService;
+
   @GetMapping(value = "")
-  public @ResponseBody ResponseEntity<String> hello() {
+  @ResponseBody
+  public ResponseEntity<String> hello() {
     return ResponseEntity.ok().body("Download Endpoint v1");
   }
 }
--- services/Upload/src/main/java/org/ena/server/services/upload/ServerApplication.java
+++ services/Upload/src/main/java/org/ena/server/services/upload/ServerApplication.java
@@ -2,14 +2,19 @@ package org.ena.server.services.upload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ServletComponentScan
+@EnableJpaRepositories(basePackages = "org.ena.server.services.common.persistence")
+@EntityScan(basePackages = "org.ena.server.services.common.persistence")
+@ComponentScan({"org.ena.server.services.common.persistence", "org.ena.server.services.download"})
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ServerApplication.class, args);
+  }
 }
