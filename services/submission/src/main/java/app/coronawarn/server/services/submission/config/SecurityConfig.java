75b794b4041a26ff301d2bda6e3b5cf935359420
Christian Kirschnick <christian.kirschnick@sap.com>
2020-05-08 16:56:05 +0200

    Disallow OPTIONS & remove actuator

--- services/submission/src/main/java/app/coronawarn/server/services/submission/config/SecurityConfig.java
+++ services/submission/src/main/java/app/coronawarn/server/services/submission/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package app.coronawarn.server.services.submission.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,8 +13,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-        .anyRequest()
-        .permitAll()
+        .antMatchers(HttpMethod.OPTIONS).denyAll()
+        .anyRequest().permitAll()
         .and().csrf().disable();
   }
 }
