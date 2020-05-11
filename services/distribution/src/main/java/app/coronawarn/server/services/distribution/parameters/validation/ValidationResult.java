7b07199644ad54c785ef08105be33e5937780037
Michael Burwig <michael.burwig@sap.com>
2020-05-11 14:46:54 +0200

    Revert "Refactor distribution services module structure"
    This reverts commit 7d666b56435d01494fe47193cdcb94c0c623e9f3.

---
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/parameters/validation/ValidationResult.java
@@ -0,0 +1,28 @@
+package app.coronawarn.server.services.distribution.parameters.validation;
+
+import java.util.HashSet;
+
+/**
+ * The result of a validation run for Exposure Configurations. Find details about possible
+ * errors in this collection.
+ */
+public class ValidationResult extends HashSet<ValidationError> {
+
+  /**
+   * Checks whether this validation result instance has at least one error.
+   *
+   * @return true if yes, false otherwise
+   */
+  public boolean hasErrors() {
+    return !this.isEmpty();
+  }
+
+  /**
+   * Checks whether this validation result instance has no errors.
+   *
+   * @return true if yes, false otherwise
+   */
+  public boolean isSuccessful() {
+    return !hasErrors();
+  }
+}
