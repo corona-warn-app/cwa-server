868bfb071a303233414f8d2e6455a0f889828539
Michael Burwig <michael.burwig@sap.com>
2020-05-12 15:43:49 +0200

    Test setup for distribution module

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/ApplicationInitializationTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/ApplicationInitializationTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 public class ApplicationInitializationTest {
 
   @Autowired
@@ -15,6 +15,7 @@ public class ApplicationInitializationTest {
 
 //  @Test
 //  public void jpaComponentsInjected() {
+//    Security.addProvider(new BouncyCastleProvider());
 //    assertNotNull(diagnosisKeyService);
 //  }
 }
---
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/crypto/CryptoProviederTest.java
@@ -0,0 +1,10 @@
+package app.coronawarn.server.services.distribution.crypto;
+
+import org.junit.jupiter.api.Test;
+
+public class CryptoProviederTest {
+
+  @Test
+  public void testSomething() {
+  }
+}
