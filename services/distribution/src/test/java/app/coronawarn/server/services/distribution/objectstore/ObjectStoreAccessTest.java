ca2ac128b3a39eb4c37430ab670e4bb81861c377
Christian Kirschnick <christian.kirschnick@sap.com>
2020-05-13 23:02:08 +0200

    Adding S3DistributionRunner for S3 integration (#91)
    * Add object store functionality
    
    * Add runner for kicking off S3 distribution
    
    * Fix tests
    
    * beau
    
    * Merging
    
    * Use specific types instead of var
    
    * Move print all files method
    
    * Remove properties file
    
    * Remove gitignore
    
    * Adjust the readme to explain required env vars
    
    * markdown

---
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/objectstore/ObjectStoreAccessTest.java
@@ -0,0 +1,80 @@
+package app.coronawarn.server.services.distribution.objectstore;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+
+@SpringBootTest
+@Tag("s3-integration")
+public class ObjectStoreAccessTest {
+
+
+  private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  private String testRunId = "testing/cwa/" + UUID.randomUUID().toString() + "/";
+
+  private String textFile = "objectstore/store-test-file";
+
+  @Autowired
+  private ObjectStoreAccess objectStoreAccess;
+
+  @Test
+  public void fetchFilesNotEmpty() {
+    var files = objectStoreAccess.getObjectsWithPrefix("");
+
+    assertFalse(files.contents().isEmpty(), "Contents is empty, but we should have files");
+  }
+
+  @Test
+  public void fetchFilesNothingFound() {
+    var files = objectStoreAccess.getObjectsWithPrefix("THISPREFIXDOESNOTEXIST");
+
+    assertTrue(files.contents().isEmpty(), "Found files, but should be empty!");
+  }
+
+  @Test
+  public void pushTestFileAndDelete() throws IOException {
+    objectStoreAccess.putObject(testRunId + "TESTFILE", getExampleFile());
+    var files = objectStoreAccess.getObjectsWithPrefix(testRunId);
+    assertEquals(1, files.contents().size());
+
+    this.printAllFiles();
+
+    objectStoreAccess.deleteObjectsWithPrefix(testRunId);
+    var filesAfterDeletion = objectStoreAccess.getObjectsWithPrefix(testRunId);
+    assertEquals(0, filesAfterDeletion.contents().size());
+
+    this.printAllFiles();
+
+    var allFiles = objectStoreAccess.getObjectsWithPrefix("");
+    assertFalse(allFiles.contents().isEmpty(), "Contents is empty, but we should have files");
+  }
+
+  private File getExampleFile() throws IOException {
+    return new File(new ClassPathResource(textFile).getURI());
+  }
+
+  /**
+   * Print some debug information about what is currently in the store.
+   */
+  private void printAllFiles() {
+    var out = objectStoreAccess.getObjectsWithPrefix("");
+
+    logger.info("-------");
+    logger.info(out.contents().toString());
+    logger.info("-------");
+
+    logger.info("Fetched S3");
+  }
+}
---
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/objectstore/S3PublisherTest.java
@@ -0,0 +1,69 @@
+package app.coronawarn.server.services.distribution.objectstore;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+
+@SpringBootTest
+@Tag("s3-integration")
+@TestInstance(Lifecycle.PER_CLASS)
+public class S3PublisherTest {
+
+  private String testRunId = "testing/cwa/" + UUID.randomUUID().toString() + "/";
+
+  private String rootTestFolder = "objectstore/publisher/";
+
+  private String exampleFile = rootTestFolder + "rootfile";
+
+  @Autowired
+  private S3Publisher s3Publisher;
+
+  @Test
+  public void publishFolder() throws IOException {
+    Path start = Paths.get(getFile(rootTestFolder).getPath());
+
+    s3Publisher.publishFolder(start);
+  }
+
+  @Test
+  public void publishSingleFile() throws IOException {
+    Path fileToPublish = getFile(exampleFile).toPath();
+    Path path = getFile(rootTestFolder).toPath();
+
+    s3Publisher.publishFile(fileToPublish, path);
+    assertTrue(s3Publisher.isFileExisting(fileToPublish, path), "File should exist on S3");
+
+    s3Publisher.deleteFile(fileToPublish, path);
+    assertFalse(s3Publisher.isFileExisting(fileToPublish, path), "File should have been deleted");
+  }
+
+  private File getFile(String path) throws IOException {
+    return new File(new ClassPathResource(path).getURI());
+  }
+
+  @BeforeAll
+  public void setup() {
+    s3Publisher.setPrefixPath(this.testRunId);
+  }
+
+  @AfterAll
+  public void teardown() {
+    s3Publisher.deleteFolder("");
+
+    s3Publisher.setPrefixPath("cwa/");
+  }
+}
