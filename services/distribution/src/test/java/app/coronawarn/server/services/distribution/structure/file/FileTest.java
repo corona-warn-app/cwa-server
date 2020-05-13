16000ddfe1f47b39ac356b80348b1a76e6e6b07d
Michael Burwig <michael.burwig@sap.com>
2020-05-13 22:20:36 +0200

    Revert "Switch from JUnit4 @Rule to Junit5 @TempDir (#90)"
    This reverts commit c853ec5c0bfca3433c832b6ff69cab1b61068381.

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/structure/file/FileTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/structure/file/FileTest.java
@@ -8,21 +8,22 @@ import app.coronawarn.server.services.distribution.structure.directory.Directory
 import app.coronawarn.server.services.distribution.structure.directory.DirectoryImpl;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import org.junit.Rule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
+import org.junit.rules.TemporaryFolder;
 
 public class FileTest {
 
   private byte[] bytes = "World".getBytes();
   private File file;
 
-  @TempDir
-  Path tempPath;
+  @Rule
+  private TemporaryFolder outputFolder = new TemporaryFolder();
 
   @BeforeEach
   public void setup() throws IOException {
+    outputFolder.create();
     file = new FileImpl("Hello", bytes);
   }
 
@@ -45,7 +46,7 @@ public class FileTest {
 
   @Test
   public void checkWrite() throws IOException {
-    java.io.File outputFile = tempPath.toFile();
+    java.io.File outputFile = outputFolder.newFolder();
     Directory directory = new DirectoryImpl(outputFile);
 
     directory.addFile(file);
@@ -56,4 +57,5 @@ public class FileTest {
     assertArrayEquals(bytes, writtenBytes);
     assertEquals(1, outputFile.listFiles().length);
   }
+
 }
