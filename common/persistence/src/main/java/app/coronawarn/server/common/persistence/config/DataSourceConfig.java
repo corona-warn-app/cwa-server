8f728b94ae5b2acb91b29df82b730a55c83c60b2
Johannes Eschrig <johannes.eschrig@sap.com>
2020-05-14 14:14:09 +0200

    Javadoc and empty line

--- common/persistence/src/main/java/app/coronawarn/server/common/persistence/config/DataSourceConfig.java
+++ common/persistence/src/main/java/app/coronawarn/server/common/persistence/config/DataSourceConfig.java
@@ -7,6 +7,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+/**
+ * Specifies the data source (postgres or h2) as defined
+ * in the .properties files with parameters prefixed
+ * by spring.datasource
+ */
 @Configuration
 public class DataSourceConfig {
 
