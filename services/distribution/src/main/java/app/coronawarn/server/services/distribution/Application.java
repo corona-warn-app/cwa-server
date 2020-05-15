89152dc1bd96b92372506292f1d68b61757b9787
Michael Burwig <michael.burwig@sap.com>
2020-05-15 16:50:49 +0200

    Add method to cut ApplicationRunner chain

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/Application.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/Application.java
@@ -1,8 +1,11 @@
 package app.coronawarn.server.services.distribution;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -13,7 +16,18 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
     "app.coronawarn.server.services.distribution"})
 public class Application {
 
+  private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  /**
+   * Terminates this application with exit code 1 (general error).
+   */
+  public static void killApplication(ApplicationContext appContext) {
+    SpringApplication.exit(appContext);
+    logger.error("Application terminated abnormally.");
+    System.exit(1);
   }
 }
