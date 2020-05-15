ec5b93a8440eabd09e3b79a683433340ddbda324
Pit Humke <pit.humke@sap.com>
2020-05-15 17:47:17 +0200

    Fix test class name

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/exposureconfig/validation/ExposureConfigurationValidatorTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/exposureconfig/validation/ExposureConfigurationValidatorTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class ExposureConfigurationComponentValidatorTest {
+public class ExposureConfigurationValidatorTest {
 
   private static final ValidationResult SUCCESS = new ValidationResult();
 
