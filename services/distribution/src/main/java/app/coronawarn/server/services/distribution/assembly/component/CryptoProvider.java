e19603799ee160ba38b1b21b259ea7e3a6d745e3
Michael Kusber <michael.kusber@sap.com>
2020-05-19 18:22:42 +0200

    cleanup imports

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/component/CryptoProvider.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/component/CryptoProvider.java
@@ -20,7 +20,6 @@
 package app.coronawarn.server.services.distribution.assembly.component;
 
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
