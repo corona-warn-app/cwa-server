6a65ff35024e944ffaae925be0e2e742eb6c6d9c
Pit Humke <pit.humke@sap.com>
2020-05-21 12:12:09 +0200

    Comment in S3 distribution runner again

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/runner/S3Distribution.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/runner/S3Distribution.java
@@ -55,8 +55,6 @@ public class S3Distribution implements ApplicationRunner {
 
   @Override
   public void run(ApplicationArguments args) {
-    // TODO Revert
-    /*
     try {
       Path pathToDistribute = outputDirectoryProvider.getFileOnDisk().toPath().toAbsolutePath();
       S3Publisher s3Publisher = new S3Publisher(pathToDistribute, objectStoreAccess);
@@ -66,6 +64,5 @@ public class S3Distribution implements ApplicationRunner {
     } catch (UnsupportedOperationException | GeneralSecurityException  | MinioException | IOException e) {
       logger.error("Distribution failed.", e);
     }
-   */
   }
 }
