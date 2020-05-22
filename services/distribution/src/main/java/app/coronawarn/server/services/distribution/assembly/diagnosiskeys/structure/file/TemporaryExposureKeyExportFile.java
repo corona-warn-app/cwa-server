d141e0a639813be21d0d009ea9785d9243355108
Christian Kirschnick <christian.kirschnick@sap.com>
2020-05-22 13:53:31 +0200

    Fixing file header for TEK

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/diagnosiskeys/structure/file/TemporaryExposureKeyExportFile.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/diagnosiskeys/structure/file/TemporaryExposureKeyExportFile.java
@@ -6,7 +6,10 @@ import app.coronawarn.server.common.protocols.external.exposurenotification.Temp
 import app.coronawarn.server.services.distribution.assembly.structure.archive.decorator.signing.AbstractSigningDecorator;
 import app.coronawarn.server.services.distribution.assembly.structure.file.FileOnDisk;
 import app.coronawarn.server.services.distribution.assembly.structure.util.ImmutableStack;
+import com.google.common.primitives.Bytes;
 import com.google.protobuf.ByteString;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -16,6 +19,11 @@ import java.util.stream.Collectors;
  * TemporaryExposureKeyExport}.
  */
 public class TemporaryExposureKeyExportFile extends FileOnDisk {
+
+  /**
+   * The mandatory header for all TEK Export files.
+   */
+  private static final byte[] HEADER = "EK Export v1    ".getBytes(StandardCharsets.UTF_8);
 
   private static final String INDEX_FILE_NAME = "export.bin";
 
@@ -47,7 +55,8 @@ public class TemporaryExposureKeyExportFile extends FileOnDisk {
    */
   public static TemporaryExposureKeyExportFile fromTemporaryExposureKeys(
       Collection<TemporaryExposureKey> temporaryExposureKeys, String region, long startTimestamp, long endTimestamp) {
-    return new TemporaryExposureKeyExportFile(temporaryExposureKeys, region, startTimestamp, endTimestamp);
+    return new TemporaryExposureKeyExportFile(temporaryExposureKeys, region, startTimestamp,
+        endTimestamp);
   }
 
   /**
@@ -62,16 +71,21 @@ public class TemporaryExposureKeyExportFile extends FileOnDisk {
    *                       seconds since epoch.
    * @return A new {@link TemporaryExposureKeyExportFile}.
    */
-  public static TemporaryExposureKeyExportFile fromDiagnosisKeys(Collection<DiagnosisKey> diagnosisKeys,
-      String region, long startTimestamp, long endTimestamp) {
-    return new TemporaryExposureKeyExportFile(getTemporaryExposureKeysFromDiagnosisKeys(diagnosisKeys), region,
+  public static TemporaryExposureKeyExportFile fromDiagnosisKeys(Collection<DiagnosisKey> diagnosisKeys, String region,
+      long startTimestamp, long endTimestamp) {
+    return new TemporaryExposureKeyExportFile(
+        getTemporaryExposureKeysFromDiagnosisKeys(diagnosisKeys), region,
         startTimestamp, endTimestamp);
   }
 
   @Override
   public void prepare(ImmutableStack<Object> indices) {
-    this.setBytes(createTemporaryExposureKeyExportBytes());
+    this.setBytes(createKeyExportBytesWithHeader());
     super.prepare(indices);
+  }
+
+  private byte[] createKeyExportBytesWithHeader() {
+    return Bytes.concat(HEADER, createTemporaryExposureKeyExportBytes());
   }
 
   private byte[] createTemporaryExposureKeyExportBytes() {
@@ -98,5 +112,19 @@ public class TemporaryExposureKeyExportFile extends FileOnDisk {
         .setRollingPeriod(Math.toIntExact(diagnosisKey.getRollingPeriod()))
         .build())
         .collect(Collectors.toSet());
+  }
+
+  /**
+   * Returns a new byte array based on the given byte array, without the TEK file header.
+   *
+   * @param keyExportBytes the files of the key export file (with headers).
+   * @return the original key export file bytes, representing the proto definition.
+   */
+  public static byte[] withoutFileHeader(byte[] keyExportBytes) {
+    if (!Arrays.equals(keyExportBytes, 0, HEADER.length, HEADER, 0, HEADER.length)) {
+      throw new IllegalArgumentException("Supplied bytes are not starting with EK Export File header");
+    }
+
+    return Arrays.copyOfRange(keyExportBytes, HEADER.length, keyExportBytes.length);
   }
 }
