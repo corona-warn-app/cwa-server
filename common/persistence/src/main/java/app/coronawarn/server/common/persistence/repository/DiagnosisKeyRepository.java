01fbd13b4bc1f4eb3f40eb40f7d7de7dcd5c577b
Michael Burwig <michael.burwig@sap.com>
2020-05-24 20:35:15 +0200

    Fix import in DiagnosisKeyRepository

--- common/persistence/src/main/java/app/coronawarn/server/common/persistence/repository/DiagnosisKeyRepository.java
+++ common/persistence/src/main/java/app/coronawarn/server/common/persistence/repository/DiagnosisKeyRepository.java
@@ -20,9 +20,9 @@
 package app.coronawarn.server.common.persistence.repository;
 
 import app.coronawarn.server.common.persistence.domain.DiagnosisKey;
-import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface DiagnosisKeyRepository extends JpaRepository<DiagnosisKey, Long> {
@@ -33,5 +33,5 @@ public interface DiagnosisKeyRepository extends JpaRepository<DiagnosisKey, Long
    * @param submissionTimestamp the submission timestamp up to which entries will be deleted.
    */
   @Transactional
-  public void deleteBySubmissionTimestampIsLessThanEqual(long submissionTimestamp);
+  void deleteBySubmissionTimestampIsLessThanEqual(long submissionTimestamp);
 }
