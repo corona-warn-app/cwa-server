a7ca6477738a4fb95038e7fc8b131d7baae37e60
Michael Kusber <michael.kusber@sap.com>
2020-05-26 17:38:56 +0200

    Remove substring output.

--- services/submission/src/main/java/app/coronawarn/server/services/submission/verification/TanVerifier.java
+++ services/submission/src/main/java/app/coronawarn/server/services/submission/verification/TanVerifier.java
@@ -70,7 +70,7 @@ public class TanVerifier {
     String trimmedTan = tan.trim();
 
     if (!checkTanSyntax(trimmedTan)) {
-      logger.debug("TAN Syntax check failed for TAN: {}", trimmedTan.substring(0,10));
+      logger.debug("TAN Syntax check failed for TAN: {}", trimmedTan);
       return false;
     }
 
