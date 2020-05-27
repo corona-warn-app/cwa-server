c6d9bf95ad958be9b11db9e08ba3a3df6bd16728
Pit Humke <pit.humke@sap.com>
2020-05-27 17:15:38 +0200

    Remove certificates from CryptoProvider

--- scripts/generate_keys.sh
+++ scripts/generate_keys.sh
@@ -32,8 +32,37 @@ extract_public_key()
     -out "$2"
 }
 
+# Generate certificate signing request
+# $1 = IN  New certificate private key
+# $2 = IN  "Subject" of the certificate
+# $3 = OUT Certificate signing request file
+generate_certificate_signing_request()
+{
+  openssl req                                 \
+    -new                                      \
+    -key "$1"                                 \
+    -subj "$2"                                \
+    -out "$3"
+}
+
+# Self-sign a certificate request
+# $1 = IN  Certificate signing request file
+# $2 = IN  Certificate authority private key file
+# $3 = OUT New certificate file
+self_sign_certificate_request()
+{
+  openssl x509                                \
+    -req                                      \
+    -days 365                                 \
+    -in "$1"                                  \
+    -signkey "$2"                             \
+    -out "$3"
+}
+
 generate_private_key private.pem
 extract_public_key private.pem public.pem
+generate_certificate_signing_request private.pem '/CN=CWA Test Certificate' request.csr
+self_sign_certificate_request request.csr private.pem certificate.crt
 
 popd > /dev/null || exit
 popd > /dev/null || exit
