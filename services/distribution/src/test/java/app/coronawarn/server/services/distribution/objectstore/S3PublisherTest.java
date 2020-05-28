8568e79af3451eb2a5dab6f5fc7683108d301a90
Christian Kirschnick <christian.kirschnick@sap.com>
2020-05-28 12:02:39 +0200

    review comments

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/objectstore/S3PublisherTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/objectstore/S3PublisherTest.java
@@ -4,14 +4,14 @@
  * SAP SE and all other contributors /
  * copyright owners license this file to you under the Apache
  * License, Version 2.0 (the "License"); you may not use this
- *  file except in compliance with the License.
+ * file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
@@ -116,8 +116,8 @@ class S3PublisherTest {
   private List<S3Object> twoIdenticalOneOtherOneChange() {
     return List.of(
         new S3Object("newfile.txt", "1fb772815c837b6294d9f163db89e962-1"), // new
-        FILE_1, // exist
-        FILE_2,  // exit
+        FILE_1,
+        FILE_2,
         new S3Object("file3.txt", "111772815c837b6294d9f163db89e962-1") // changed
     );
   }
