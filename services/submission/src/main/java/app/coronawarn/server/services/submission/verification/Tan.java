c01c82e1078c4384d3d5e54c6ba44af70adcacf9
Christian Kirschnick <christian.kirschnick@sap.com>
2020-05-30 19:44:59 +0200

    fixing checkstyle

--- services/submission/src/main/java/app/coronawarn/server/services/submission/verification/Tan.java
+++ services/submission/src/main/java/app/coronawarn/server/services/submission/verification/Tan.java
@@ -7,45 +7,53 @@ import java.util.UUID;
  * A representation of a tan.
  */
 class Tan {
-    private UUID tan;
 
-    private Tan(UUID tan) {
-        this.tan = tan;
-    }
+  private UUID tan;
 
-    /**
-     * Creates a new {@link #Tan} instance for the given tan string.
-     * @param tanString A valid UUID string representation.
-     * @return The Tan instance
-     * @throws IllegalArgumentException when the given tan string is not a valid UUID.
-     */
-    public static Tan of(String tanString) {
-        UUID tan = UUID.fromString(tanString.trim());
-        return new Tan(tan);
-    }
+  private Tan(UUID tan) {
+    this.tan = tan;
+  }
 
-    public UUID getTan() {
-        return tan;
-    }
+  /**
+   * Creates a new {@link #Tan} instance for the given tan string.
+   *
+   * @param tanString A valid UUID string representation.
+   * @return The Tan instance
+   * @throws IllegalArgumentException when the given tan string is not a valid UUID.
+   */
+  public static Tan of(String tanString) {
+    UUID tan = UUID.fromString(tanString.trim());
+    return new Tan(tan);
+  }
 
-    /**
-     * @return the tan UUID as a string.
-     */
-    @Override
-    public String toString() {
-        return tan.toString();
-    }
+  public UUID getTan() {
+    return tan;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Tan tan1 = (Tan) o;
-        return tan.equals(tan1.tan);
-    }
+  /**
+   * Returns the TAN in it's string representation.
+   *
+   * @return the tan UUID as a string.
+   */
+  @Override
+  public String toString() {
+    return tan.toString();
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(tan);
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Tan tan1 = (Tan) o;
+    return tan.equals(tan1.tan);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(tan);
+  }
 }
