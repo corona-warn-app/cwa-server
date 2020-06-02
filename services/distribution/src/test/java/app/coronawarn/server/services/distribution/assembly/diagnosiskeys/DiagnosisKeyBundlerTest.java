6e5740b5b3c31408db4d4412a79523ed6ad8ab8f
Pit Humke <pit.humke@sap.com>
2020-06-02 11:27:36 +0200

    Review comments

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/diagnosiskeys/DiagnosisKeyBundlerTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/diagnosiskeys/DiagnosisKeyBundlerTest.java
@@ -1,3 +1,23 @@
+/*-
+ * ---license-start
+ * Corona-Warn-App
+ * ---
+ * Copyright (C) 2020 SAP SE and all other contributors
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ---license-end
+ */
+
 package app.coronawarn.server.services.distribution.assembly.diagnosiskeys;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -155,7 +175,7 @@ class DiagnosisKeyBundlerTest {
     }
   }
 
-  public List<DiagnosisKey> buildDiagnosisKeys(int startIntervalNumber, long submissionTimestamp, int number) {
+  public static List<DiagnosisKey> buildDiagnosisKeys(int startIntervalNumber, long submissionTimestamp, int number) {
     return IntStream.range(0, number)
         .mapToObj(__ -> DiagnosisKey.builder()
             .withKeyData(new byte[16])
@@ -164,5 +184,4 @@ class DiagnosisKeyBundlerTest {
             .withSubmissionTimestamp(submissionTimestamp).build())
         .collect(Collectors.toList());
   }
-
 }
