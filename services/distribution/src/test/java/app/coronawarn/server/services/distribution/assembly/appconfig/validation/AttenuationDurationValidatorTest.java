8ed046808c14610a2bf89e2df50090572bcee18d
Michael Burwig <michael.burwig@sap.com>
2020-06-03 21:35:50 +0200

    Update AttenuationDuration spec

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/appconfig/validation/AttenuationDurationValidatorTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/appconfig/validation/AttenuationDurationValidatorTest.java
@@ -26,6 +26,10 @@ import static app.coronawarn.server.services.distribution.assembly.appconfig.val
 import static app.coronawarn.server.services.distribution.assembly.appconfig.validation.ParameterSpec.ATTENUATION_DURATION_THRESHOLD_MIN;
 import static app.coronawarn.server.services.distribution.assembly.appconfig.validation.ParameterSpec.ATTENUATION_DURATION_WEIGHT_MAX;
 import static app.coronawarn.server.services.distribution.assembly.appconfig.validation.ParameterSpec.ATTENUATION_DURATION_WEIGHT_MIN;
+import static app.coronawarn.server.services.distribution.assembly.appconfig.validation.ParameterSpec.DEFAULT_BUCKET_OFFSET_MAX;
+import static app.coronawarn.server.services.distribution.assembly.appconfig.validation.ParameterSpec.DEFAULT_BUCKET_OFFSET_MIN;
+import static app.coronawarn.server.services.distribution.assembly.appconfig.validation.ParameterSpec.RISK_SCORE_NORMALIZATION_DIVISOR_MAX;
+import static app.coronawarn.server.services.distribution.assembly.appconfig.validation.ParameterSpec.RISK_SCORE_NORMALIZATION_DIVISOR_MIN;
 import static app.coronawarn.server.services.distribution.assembly.appconfig.validation.RiskScoreClassificationValidatorTest.buildError;
 import static app.coronawarn.server.services.distribution.assembly.appconfig.validation.RiskScoreClassificationValidatorTest.buildExpectedResult;
 import static app.coronawarn.server.services.distribution.assembly.appconfig.validation.WeightValidationError.ErrorType.OUT_OF_RANGE;
@@ -44,12 +48,14 @@ public class AttenuationDurationValidatorTest {
       buildThresholds(ATTENUATION_DURATION_THRESHOLD_MIN, ATTENUATION_DURATION_THRESHOLD_MAX);
   private static final Weights VALID_WEIGHTS =
       buildWeights(ATTENUATION_DURATION_WEIGHT_MAX, ATTENUATION_DURATION_WEIGHT_MAX, ATTENUATION_DURATION_WEIGHT_MAX);
+  private static final int VALID_BUCKET_OFFSET = DEFAULT_BUCKET_OFFSET_MAX;
+  private static final int VALID_NORMALIZATION_DIVISOR = RISK_SCORE_NORMALIZATION_DIVISOR_MIN;
 
   @ParameterizedTest
   @ValueSource(ints = {ATTENUATION_DURATION_THRESHOLD_MIN - 1, ATTENUATION_DURATION_THRESHOLD_MAX + 1})
   void failsIfAttenuationDurationThresholdOutOfBounds(int invalidThresholdValue) {
     AttenuationDurationValidator validator = buildValidator(
-        buildThresholds(invalidThresholdValue, invalidThresholdValue), VALID_WEIGHTS);
+        buildThresholds(invalidThresholdValue, invalidThresholdValue));
 
     ValidationResult expectedResult = buildExpectedResult(
         buildError("attenuation-duration.thresholds.lower", invalidThresholdValue, VALUE_OUT_OF_BOUNDS),
@@ -58,10 +64,35 @@ public class AttenuationDurationValidatorTest {
     assertThat(validator.validate()).isEqualTo(expectedResult);
   }
 
+  @ParameterizedTest
+  @ValueSource(ints = {DEFAULT_BUCKET_OFFSET_MIN - 1, DEFAULT_BUCKET_OFFSET_MAX + 1})
+  void failsIfBucketOffsetOutOfBounds(int invalidDefaultBucketOffsetValue) {
+    AttenuationDurationValidator validator = buildValidator(VALID_THRESHOLDS, VALID_WEIGHTS,
+        invalidDefaultBucketOffsetValue, VALID_NORMALIZATION_DIVISOR);
+
+    ValidationResult expectedResult = buildExpectedResult(
+        buildError("attenuation-duration.default-bucket-offset", invalidDefaultBucketOffsetValue, VALUE_OUT_OF_BOUNDS));
+
+    assertThat(validator.validate()).isEqualTo(expectedResult);
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {RISK_SCORE_NORMALIZATION_DIVISOR_MIN - 1, RISK_SCORE_NORMALIZATION_DIVISOR_MAX + 1})
+  void failsIfRiskScoreNormalizationDivisorOutOfBounds(int invalidRiskScoreNormalizationDivisorValue) {
+    AttenuationDurationValidator validator = buildValidator(VALID_THRESHOLDS, VALID_WEIGHTS, VALID_BUCKET_OFFSET,
+        invalidRiskScoreNormalizationDivisorValue);
+
+    ValidationResult expectedResult = buildExpectedResult(
+        buildError("attenuation-duration.risk-score-normalization-divisor", invalidRiskScoreNormalizationDivisorValue,
+            VALUE_OUT_OF_BOUNDS));
+
+    assertThat(validator.validate()).isEqualTo(expectedResult);
+  }
+
   @Test
   void failsIfUpperAttenuationDurationThresholdLesserThanLower() {
     AttenuationDurationValidator validator = buildValidator(
-        buildThresholds(ATTENUATION_DURATION_THRESHOLD_MAX, ATTENUATION_DURATION_THRESHOLD_MIN), VALID_WEIGHTS);
+        buildThresholds(ATTENUATION_DURATION_THRESHOLD_MAX, ATTENUATION_DURATION_THRESHOLD_MIN));
 
     ValidationResult expectedResult = buildExpectedResult(
         new GeneralValidationError("attenuation-duration.thresholds.lower, attenuation-duration.thresholds.upper",
@@ -73,7 +104,7 @@ public class AttenuationDurationValidatorTest {
   @ParameterizedTest
   @ValueSource(doubles = {ATTENUATION_DURATION_WEIGHT_MIN - .1, ATTENUATION_DURATION_WEIGHT_MAX + .1})
   void failsIfWeightsOutOfBounds(double invalidWeightValue) {
-    AttenuationDurationValidator validator = buildValidator(VALID_THRESHOLDS,
+    AttenuationDurationValidator validator = buildValidator(
         buildWeights(invalidWeightValue, invalidWeightValue, invalidWeightValue));
 
     ValidationResult expectedResult = buildExpectedResult(
@@ -87,7 +118,7 @@ public class AttenuationDurationValidatorTest {
   @Test
   void failsIfWeightsHaveTooManyDecimalPlaces() {
     double invalidWeightValue = ATTENUATION_DURATION_WEIGHT_MAX - 0.0000001;
-    AttenuationDurationValidator validator = buildValidator(VALID_THRESHOLDS,
+    AttenuationDurationValidator validator = buildValidator(
         buildWeights(invalidWeightValue, invalidWeightValue, invalidWeightValue));
 
     ValidationResult expectedResult = buildExpectedResult(
@@ -98,10 +129,22 @@ public class AttenuationDurationValidatorTest {
     assertThat(validator.validate()).isEqualTo(expectedResult);
   }
 
-  private static AttenuationDurationValidator buildValidator(Thresholds thresholds, Weights weights) {
+  private static AttenuationDurationValidator buildValidator(Thresholds thresholds) {
+    return buildValidator(thresholds, VALID_WEIGHTS, VALID_BUCKET_OFFSET, VALID_NORMALIZATION_DIVISOR);
+  }
+
+  private static AttenuationDurationValidator buildValidator(Weights weights) {
+    return buildValidator(VALID_THRESHOLDS, weights, VALID_BUCKET_OFFSET, VALID_NORMALIZATION_DIVISOR);
+  }
+
+  private static AttenuationDurationValidator buildValidator(
+      Thresholds thresholds, Weights weights, int defaultBucketOffset, int riskScoreNormalizationDivisor) {
     return new AttenuationDurationValidator(AttenuationDuration.newBuilder()
         .setThresholds(thresholds)
-        .setWeights(weights).build());
+        .setWeights(weights)
+        .setDefaultBucketOffset(defaultBucketOffset)
+        .setRiskScoreNormalizationDivisor(riskScoreNormalizationDivisor)
+        .build());
   }
 
   private static Thresholds buildThresholds(int lower, int upper) {
