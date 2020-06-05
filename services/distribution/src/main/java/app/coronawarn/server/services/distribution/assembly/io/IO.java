07323eee32b86897f019bf6ae22cc252fd89152d
Michael Burwig <michael.burwig@sap.com>
2020-06-05 16:51:48 +0200

    Implement maximum file size check

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/io/IO.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/io/IO.java
@@ -20,15 +20,25 @@
 
 package app.coronawarn.server.services.distribution.assembly.io;
 
+import app.coronawarn.server.services.distribution.assembly.diagnosiskeys.structure.file.TemporaryExposureKeyExportFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A class containing helper functions for general purpose file IO.
  */
 public class IO {
+
+  private static final Logger logger = LoggerFactory.getLogger(IO.class);
+
+  /**
+   * The maximum acceptable file size in bytes.
+   */
+  public static final int MAXIMUM_FILE_SIZE = 16000000;
 
   private IO() {
   }
@@ -51,12 +61,20 @@ public class IO {
   }
 
   /**
-   * Writes bytes into a file.
+   * Writes bytes into a file. If the resulting file would exceed the specified maximum file size, it is not written but
+   * removed instead.
    *
    * @param bytes      The content to write
    * @param outputFile The file to write the content into.
    */
   public static void writeBytesToFile(byte[] bytes, File outputFile) {
+    if (bytes.length > MAXIMUM_FILE_SIZE) {
+      String fileName = outputFile.getName();
+      throw new UncheckedIOException(
+          new IOException(
+              "File size of " + bytes.length + " bytes exceeds the maximum file size. Deleting" + fileName));
+    }
+
     try (FileOutputStream outputFileStream = new FileOutputStream(outputFile)) {
       outputFileStream.write(bytes);
     } catch (IOException e) {
