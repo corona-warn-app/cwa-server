c41ee087be6b1f69cd052364b9c33bfdcd3a8afd
Christian Kirschnick <christian.kirschnick@sap.com>
2020-06-06 00:13:07 +0200

    Squashing migrations for RC (#461)

--- common/persistence/src/main/resources/db/migration/h2/V2__changeTypeToIntForRollingNumbers.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE diagnosis_key ALTER COLUMN rolling_period TYPE integer;
-ALTER TABLE diagnosis_key ALTER COLUMN rolling_start_number TYPE integer;
-ALTER TABLE diagnosis_key RENAME COLUMN rolling_start_number TO rolling_start_interval_number;
