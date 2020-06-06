cfe4d19a83c06944861f4c8463ba316a9a677f64
UnchartedBull <timon.gaebelein@icloud.com>
2020-06-06 14:50:43 +0200

    fix file header

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/objectstore/S3PublisherMultiThreadingTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/objectstore/S3PublisherMultiThreadingTest.java
@@ -1,23 +1,21 @@
-/*
- * -
- *  * ---license-start
- *  * Corona-Warn-App
- *  * ---
- *  * Copyright (C) 2020 SAP SE and all other contributors
- *  * ---
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  * you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  *      http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
- *  * ---license-end
+/*-
+ * ---license-start
+ * Corona-Warn-App
+ * ---
+ * Copyright (C) 2020 SAP SE and all other contributors
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ---license-end
  */
 
 package app.coronawarn.server.services.distribution.objectstore;
@@ -33,6 +31,7 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.config.Configurator;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,6 +45,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith({SpringExtension.class})
 @SpringBootTest(classes = {ObjectStoreAccess.class, ObjectStoreClientConfig.class, AsyncConfiguration.class})
 @EnableConfigurationProperties(value = DistributionServiceConfig.class)
+@Tag("s3-integration")
 public class S3PublisherMultiThreadingTest {
 
   private static final String PUBLISHING_PATH = "testsetups/s3publishertest/topublish";
