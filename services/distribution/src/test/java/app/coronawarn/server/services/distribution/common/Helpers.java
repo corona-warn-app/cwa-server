48431757e79d5542f0afa043e68d40ddb94b7a99
Michael Burwig <michael.burwig@sap.com>
2020-06-17 17:08:13 +0200

    Fix/appconfig validation (#598)
    - Fixes bug where app configurations could be written even if validation failed at runtime.
    - Adds unit test for AppConfigurationDirectory
    - Corrects JavaDoc in AppConfigurationDirectory
    - Extracts test helper method
    - Refactors AppConfigurationProvider into Configuration that provides ApplicationConfiguration bean

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/common/Helpers.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/common/Helpers.java
@@ -20,12 +20,20 @@
 
 package app.coronawarn.server.services.distribution.common;
 
+import static app.coronawarn.server.services.distribution.assembly.appconfig.YamlLoader.loadYamlIntoProtobufBuilder;
+
 import app.coronawarn.server.common.persistence.domain.DiagnosisKey;
+import app.coronawarn.server.common.protocols.internal.ApplicationConfiguration;
+import app.coronawarn.server.services.distribution.assembly.appconfig.UnableToLoadFileException;
 import app.coronawarn.server.services.distribution.assembly.structure.directory.Directory;
 import app.coronawarn.server.services.distribution.assembly.structure.util.ImmutableStack;
+import java.io.File;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -48,7 +56,8 @@ public class Helpers {
     return buildDiagnosisKeyForSubmissionTimestamp(dateTime.toEpochSecond(ZoneOffset.UTC) / 3600);
   }
 
-  public static List<DiagnosisKey> buildDiagnosisKeys(int startIntervalNumber, LocalDateTime submissionTimestamp, int number) {
+  public static List<DiagnosisKey> buildDiagnosisKeys(
+      int startIntervalNumber, LocalDateTime submissionTimestamp, int number) {
     long timestamp = submissionTimestamp.toEpochSecond(ZoneOffset.UTC) / 3600;
     return buildDiagnosisKeys(startIntervalNumber, timestamp, number);
   }
@@ -61,5 +70,29 @@ public class Helpers {
             .withTransmissionRiskLevel(2)
             .withSubmissionTimestamp(submissionTimestamp).build())
         .collect(Collectors.toList());
+  }
+
+  public static Set<String> getFilePaths(java.io.File root, String basePath) {
+    Set<String> files = Arrays.stream(Objects.requireNonNull(root.listFiles()))
+        .filter(File::isFile)
+        .map(File::getAbsolutePath)
+        .map(path -> path.substring(basePath.length() + 1))
+        .collect(Collectors.toSet());
+
+    Set<java.io.File> directories = Arrays.stream(Objects.requireNonNull(root.listFiles()))
+        .filter(File::isDirectory)
+        .collect(Collectors.toSet());
+
+    Set<String> subFiles = directories.stream()
+        .map(subDirectory -> getFilePaths(subDirectory, basePath))
+        .flatMap(Set::stream)
+        .collect(Collectors.toSet());
+
+    files.addAll(subFiles);
+    return files;
+  }
+
+  public static ApplicationConfiguration loadApplicationConfiguration(String path) throws UnableToLoadFileException {
+    return loadYamlIntoProtobufBuilder(path, ApplicationConfiguration.Builder.class).build();
   }
 }
