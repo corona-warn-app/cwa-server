08e31d810916d50de61aeaa57cb534f7e419d573
Pit Humke <pit.humke@sap.com>
2020-06-21 11:55:14 +0200

    Remove unused import

--- services/submission/src/main/java/app/coronawarn/server/services/submission/controller/SubmissionController.java
+++ services/submission/src/main/java/app/coronawarn/server/services/submission/controller/SubmissionController.java
@@ -32,7 +32,6 @@ import io.micrometer.core.annotation.Timed;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
