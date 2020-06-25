abbc3a1f9dac3b3a39a30fac13685f21ee86f054
Hilmar Falkenberg <hilmarf@googlemail.com>
2020-06-25 16:49:20 +0200

    Fix/mime type (#627)
    * depending on the s3Key we identify the mime type
    
    608: Index Files: Incorrect mime type  - https://github.com/corona-warn-app/cwa-server/issues/608
    
    * get the content type from the localfile and add to headers map
    
    608: Index Files: Incorrect mime type  - https://github.com/corona-warn-app/cwa-server/issues/608
    
    * forward the added CONTENT_TYPE header to the s3Client (requestBuilder)
    
    608: Index Files: Incorrect mime type  - https://github.com/corona-warn-app/cwa-server/issues/608
    
    * removed keyword: 'final'
    
    608: Index Files: Incorrect mime type
    
    Task-Url: https://github.com/corona-warn-app/cwa-server/issues/608
    
    * removed dependency on awssdk
    
    608: Index Files: Incorrect mime type
    
    Task-Url: https://github.com/corona-warn-app/cwa-server/issues/608

---
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/objectstore/publish/LocalFileTest.java
@@ -0,0 +1,51 @@
+/*-
+ * ---license-start
+ * Corona-Warn-App
+ * ---
+ * Copyright (C) 2020 SAP SE and all other contributors
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ---license-end
+ */
+
+package app.coronawarn.server.services.distribution.objectstore.publish;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.file.Path;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LocalFileTest {
+  @ParameterizedTest
+  @ValueSource(strings = { 
+      "version", 
+      "version/v1/configuration/country", 
+      "version/v1/diagnosis-keys/country",
+      "version/v1/diagnosis-keys/country/DE/date", 
+      "version/v1/diagnosis-keys/country/DE/date/2020-06-11/hour" })
+  void testGetContentTypeJson(String path) {
+    LocalFile test = new LocalIndexFile(Path.of("/root", path, "/index"), Path.of("/root"));
+    assertEquals("application/json", test.getContentType());
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = { 
+      "version/v1/configuration/country/DE/app_config", 
+      "version/v1/diagnosis-keys/country/DE/date/2020-06-11", 
+      "version/v1/diagnosis-keys/country/DE/date/2020-06-11/hour/13" })
+  void testGetContentTypeZip(String path) {
+    LocalFile test = new LocalIndexFile(Path.of("/root", path, "/index"), Path.of("/root"));
+    assertEquals("application/zip", test.getContentType());
+  }
+}
