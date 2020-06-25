896d01ac4843999c35adcb2d8bb199725ce75946
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2020-06-25 16:40:38 +0200

    removed dependency on awssdk
    608: Index Files: Incorrect mime type
    
    Task-Url: https://github.com/corona-warn-app/cwa-server/issues/608

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/objectstore/client/ObjectStoreClient.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/objectstore/client/ObjectStoreClient.java
@@ -23,7 +23,6 @@ package app.coronawarn.server.services.distribution.objectstore.client;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
-import software.amazon.awssdk.http.Header;
 
 /**
  * Simple Storage Service (aka S3) client to perform bucket and object operations.
@@ -79,8 +78,7 @@ public interface ObjectStoreClient {
     /**
      * To control which content type is sent, when objects are retrieved from the object store.
      */
-    CONTENT_TYPE(Header.CONTENT_TYPE)
-    ;
+    CONTENT_TYPE("Content-Type");
 
     public final String keyValue;
 
