1acd2ba1243749a3e982f1f47fe5a7656c4f1ff7
KevponSAP <66735382+KevponSAP@users.noreply.github.com>
2020-06-26 18:43:28 +0200

    Use native query for retention policy (#628)

--- common/persistence/src/main/java/app/coronawarn/server/common/persistence/service/DiagnosisKeyService.java
+++ common/persistence/src/main/java/app/coronawarn/server/common/persistence/service/DiagnosisKeyService.java
@@ -111,8 +111,9 @@ public class DiagnosisKeyService {
         .ofInstant(Instant.now(), UTC)
         .minusDays(daysToRetain)
         .toEpochSecond(UTC) / SECONDS_PER_HOUR;
-    int numberOfDeletions = keyRepository.deleteBySubmissionTimestampIsLessThanEqual(threshold);
-    logger.info("Deleted {} diagnosis key(s) with a submission timestamp older than {} day(s) ago.",
+    int numberOfDeletions = keyRepository.countOlderThanOrEqual(threshold);
+    logger.info("Deleting {} diagnosis key(s) with a submission timestamp older than {} day(s) ago.",
         numberOfDeletions, daysToRetain);
+    keyRepository.deleteOlderThanOrEqual(threshold);
   }
 }
