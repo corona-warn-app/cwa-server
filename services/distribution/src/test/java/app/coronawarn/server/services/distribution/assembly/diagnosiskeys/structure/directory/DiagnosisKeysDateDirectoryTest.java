e7e72d2474ddaec9d915e178a5b39c7ae26667e3
i339399 <sorin.stefan.iovita@sap.com>
2020-06-29 18:18:22 +0300

    Split parameterized test for DiagnosisKeysDateDirectory into separate tests.

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/diagnosiskeys/structure/directory/DiagnosisKeysDateDirectoryTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/diagnosiskeys/structure/directory/DiagnosisKeysDateDirectoryTest.java
@@ -21,7 +21,10 @@
 package app.coronawarn.server.services.distribution.assembly.diagnosiskeys.structure.directory;
 
 import static app.coronawarn.server.services.distribution.common.Helpers.buildDiagnosisKeys;
+import static app.coronawarn.server.services.distribution.common.Helpers.getExpectedDateAndHourFiles;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import app.coronawarn.server.common.persistence.domain.DiagnosisKey;
 import app.coronawarn.server.services.distribution.assembly.component.CryptoProvider;
 import app.coronawarn.server.services.distribution.assembly.diagnosiskeys.DiagnosisKeyBundler;
 import app.coronawarn.server.services.distribution.assembly.diagnosiskeys.ProdDiagnosisKeyBundler;
@@ -30,24 +33,20 @@ import app.coronawarn.server.services.distribution.assembly.structure.directory.
 import app.coronawarn.server.services.distribution.assembly.structure.directory.DirectoryOnDisk;
 import app.coronawarn.server.services.distribution.assembly.structure.util.ImmutableStack;
 import app.coronawarn.server.services.distribution.common.Helpers;
-import app.coronawarn.server.services.distribution.common.Helpers.TestTuple;
 import app.coronawarn.server.services.distribution.config.DistributionServiceConfig;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
-import org.assertj.core.api.Assertions;
 import org.junit.Rule;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.rules.TemporaryFolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -72,95 +71,16 @@ class DiagnosisKeysDateDirectoryTest {
 
   private File outputFile;
 
-  private static Stream<Arguments> createDiagnosisKeysAndExpectedFiles() {
-    return Stream.of(
-        //Creates correct structure for multiple dates
-        new TestTuple(IntStream.range(0, 3)
-            .mapToObj(currentDate -> IntStream.range(0, 5)
-                .mapToObj(currentHour ->
-                    buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 3 + currentDate, 0, 0).plusHours(currentHour), 5))
-                .flatMap(List::stream)
-                .collect(Collectors.toList()))
-            .flatMap(List::stream)
-            .collect(Collectors.toList()),
-            Helpers.getExpectedDateAndHourFiles(Map.of(
-                "1970-01-03", List.of("0", "1", "2", "3", "4"),
-                "1970-01-04", List.of("0", "1", "2", "3", "4"),
-                "1970-01-05", List.of("0", "1", "2", "3", "4")), "1970-01-06"),
-            LocalDateTime.of(1970, 1, 6, 0, 0)
-        ),
-        //Does not include the current date
-        new TestTuple(IntStream.range(0, 3)
-            .mapToObj(currentDate -> IntStream.range(0, 5)
-                .mapToObj(currentHour ->
-                    buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 3 + currentDate, 0, 0).plusHours(currentHour), 5))
-                .flatMap(List::stream)
-                .collect(Collectors.toList()))
-            .flatMap(List::stream)
-            .collect(Collectors.toList()),
-            Helpers.getExpectedDateAndHourFiles(Map.of(
-                "1970-01-03", List.of("0", "1", "2", "3", "4"),
-                "1970-01-04", List.of("0", "1", "2", "3", "4"),
-                "1970-01-05", List.of("0", "1", "2", "3", "4")), "1970-01-05"),
-            LocalDateTime.of(1970, 1, 5, 12, 0)
-        ),
-        //Does not include empty dates
-        new TestTuple(IntStream.range(0, 3)
-            .filter(currentDate -> currentDate != 1)
-            .mapToObj(currentDate -> IntStream.range(0, 5)
-                .mapToObj(currentHour ->
-                    buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 3 + currentDate, 0, 0).plusHours(currentHour), 5))
-                .flatMap(List::stream)
-                .collect(Collectors.toList()))
-            .flatMap(List::stream)
-            .collect(Collectors.toList()),
-            Helpers.getExpectedDateAndHourFiles(Map.of(
-                "1970-01-03", List.of("0", "1", "2", "3", "4"),
-                "1970-01-05", List.of("0", "1", "2", "3", "4")), "1970-01-05"),
-            LocalDateTime.of(1970, 1, 5, 12, 0)
-        ),
-        //Does not include dates with too few keys
-        new TestTuple(
-            List.of(
-                buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 3, 1, 0), 5),
-                buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 4, 1, 0), 4),
-                buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 5, 1, 0), 5))
-                .stream()
-                .flatMap(List::stream)
-                .collect(Collectors.toList()),
-            Helpers.getExpectedDateAndHourFiles(Map.of(
-                "1970-01-03", List.of("1"),
-                "1970-01-05", List.of("1")), "1970-01-06"),
-            LocalDateTime.of(1970, 1, 6, 12, 0)
-        ),
-        //Does not include dates in the future
-        new TestTuple(
-            List.of(
-                buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 3, 1, 0), 5),
-                buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 4, 1, 0), 5),
-                buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 5, 1, 0), 5))
-                .stream()
-                .flatMap(List::stream)
-                .collect(Collectors.toList()),
-            Helpers.getExpectedDateAndHourFiles(Map.of(
-                "1970-01-03", List.of("1"),
-                "1970-01-04", List.of("1")), "1970-01-04"),
-            LocalDateTime.of(1970, 1, 4, 12, 0)
-        )
-    ).map(Arguments::of);
-  }
-
   @BeforeEach
   void setupAll() throws IOException {
     outputFolder.create();
     outputFile = outputFolder.newFolder();
   }
 
-  @ParameterizedTest
-  @MethodSource("createDiagnosisKeysAndExpectedFiles")
-  void checkBuildsTheCorrectDateDirectoryStructure(TestTuple testTuple) {
+  private void runDateDistribution(Collection<DiagnosisKey> diagnosisKeys, LocalDateTime distributionTime) {
     DiagnosisKeyBundler bundler = new ProdDiagnosisKeyBundler(distributionServiceConfig);
-    bundler.setDiagnosisKeys(testTuple.diagnosisKeys, testTuple.distributionRun);
+    bundler
+        .setDiagnosisKeys(diagnosisKeys, distributionTime);
     DiagnosisKeysDateDirectory dateDirectory = new DiagnosisKeysDateDirectory(bundler, cryptoProvider,
         distributionServiceConfig);
     Directory<WritableOnDisk> outputDirectory = new DirectoryOnDisk(outputFile);
@@ -170,7 +90,97 @@ class DiagnosisKeysDateDirectoryTest {
         .push("country-directory")
     );
     outputDirectory.write();
+  }
+
+  @Test
+  void testCreatesCorrectDirectoryStructureForMultipleDates() {
+    Collection<DiagnosisKey> diagnosisKeys = IntStream.range(0, 3)
+        .mapToObj(currentDate -> IntStream.range(0, 5)
+            .mapToObj(currentHour ->
+                buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 3 + currentDate, 0, 0).plusHours(currentHour), 5))
+            .flatMap(List::stream)
+            .collect(Collectors.toList()))
+        .flatMap(List::stream)
+        .collect(Collectors.toList());
+    runDateDistribution(diagnosisKeys, LocalDateTime.of(1970, 1, 6, 0, 0));
     Set<String> actualFiles = Helpers.getFilePaths(outputFile, outputFile.getAbsolutePath());
-    Assertions.assertThat(actualFiles).isEqualTo(testTuple.expectedFiles);
+    Set<String> expectedDateAndHourFiles = getExpectedDateAndHourFiles(Map.of(
+        "1970-01-03", List.of("0", "1", "2", "3", "4"),
+        "1970-01-04", List.of("0", "1", "2", "3", "4"),
+        "1970-01-05", List.of("0", "1", "2", "3", "4")), "1970-01-06");
+    assertThat(actualFiles).isEqualTo(expectedDateAndHourFiles);
+  }
+
+  @Test
+  void testDoesNotIncludeCurrentDateInDirectoryStructure() {
+    Collection<DiagnosisKey> diagnosisKeys = IntStream.range(0, 3)
+        .mapToObj(currentDate -> IntStream.range(0, 5)
+            .mapToObj(currentHour ->
+                buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 3 + currentDate, 0, 0).plusHours(currentHour), 5))
+            .flatMap(List::stream)
+            .collect(Collectors.toList()))
+        .flatMap(List::stream)
+        .collect(Collectors.toList());
+    runDateDistribution(diagnosisKeys, LocalDateTime.of(1970, 1, 5, 12, 0));
+    Set<String> actualFiles = Helpers.getFilePaths(outputFile, outputFile.getAbsolutePath());
+    Set<String> expectedDateAndHourFiles = getExpectedDateAndHourFiles(Map.of(
+        "1970-01-03", List.of("0", "1", "2", "3", "4"),
+        "1970-01-04", List.of("0", "1", "2", "3", "4"),
+        "1970-01-05", List.of("0", "1", "2", "3", "4")), "1970-01-05");
+    assertThat(actualFiles).isEqualTo(expectedDateAndHourFiles);
+  }
+
+  @Test
+  void testDoesNotIncludeEmptyDatesInDirectoryStructure() {
+    Collection<DiagnosisKey> diagnosisKeys = IntStream.range(0, 3)
+        .filter(currentDate -> currentDate != 1)
+        .mapToObj(currentDate -> IntStream.range(0, 5)
+            .mapToObj(currentHour ->
+                buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 3 + currentDate, 0, 0).plusHours(currentHour), 5))
+            .flatMap(List::stream)
+            .collect(Collectors.toList()))
+        .flatMap(List::stream)
+        .collect(Collectors.toList());
+
+    runDateDistribution(diagnosisKeys, LocalDateTime.of(1970, 1, 5, 12, 0));
+    Set<String> actualFiles = Helpers.getFilePaths(outputFile, outputFile.getAbsolutePath());
+    Set<String> expectedDateAndHourFiles = getExpectedDateAndHourFiles(Map.of(
+        "1970-01-03", List.of("0", "1", "2", "3", "4"),
+        "1970-01-05", List.of("0", "1", "2", "3", "4")), "1970-01-05");
+    assertThat(actualFiles).isEqualTo(expectedDateAndHourFiles);
+  }
+
+  @Test
+  void testDoesNotIncludeDatesWithTooFewKeysInDirectoryStructure() {
+    Collection<DiagnosisKey> diagnosisKeys = List.of(
+        buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 3, 1, 0), 5),
+        buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 4, 1, 0), 4),
+        buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 5, 1, 0), 5))
+        .stream()
+        .flatMap(List::stream)
+        .collect(Collectors.toList());
+    runDateDistribution(diagnosisKeys, LocalDateTime.of(1970, 1, 6, 12, 0));
+    Set<String> actualFiles = Helpers.getFilePaths(outputFile, outputFile.getAbsolutePath());
+    Set<String> expectedDateAndHourFiles = getExpectedDateAndHourFiles(Map.of(
+        "1970-01-03", List.of("1"),
+        "1970-01-05", List.of("1")), "1970-01-06");
+    assertThat(actualFiles).isEqualTo(expectedDateAndHourFiles);
+  }
+
+  @Test
+  void testDoesNotIncludeDatesInTheFuture() {
+    Collection<DiagnosisKey> diagnosisKeys = List.of(
+        buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 3, 1, 0), 5),
+        buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 4, 1, 0), 5),
+        buildDiagnosisKeys(6, LocalDateTime.of(1970, 1, 5, 1, 0), 5))
+        .stream()
+        .flatMap(List::stream)
+        .collect(Collectors.toList());
+    runDateDistribution(diagnosisKeys, LocalDateTime.of(1970, 1, 4, 12, 0));
+    Set<String> actualFiles = Helpers.getFilePaths(outputFile, outputFile.getAbsolutePath());
+    Set<String> expectedDateAndHourFiles = getExpectedDateAndHourFiles(Map.of(
+        "1970-01-03", List.of("1"),
+        "1970-01-04", List.of("1")), "1970-01-04");
+    assertThat(actualFiles).isEqualTo(expectedDateAndHourFiles);
   }
 }
