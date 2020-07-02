92929ea40f4d9f022db0c54271acbe4459b7b3e4
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2020-07-02 13:57:28 +0200

    #589 Log4j improper shutdown
    same thing as in distribution service

--- services/submission/src/main/java/app/coronawarn/server/services/submission/ServerApplication.java
+++ services/submission/src/main/java/app/coronawarn/server/services/submission/ServerApplication.java
@@ -25,6 +25,8 @@ import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import java.util.Arrays;
 import java.util.List;
+import javax.annotation.PreDestroy;
+import org.apache.logging.log4j.LogManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -73,6 +75,11 @@ public class ServerApplication implements EnvironmentAware {
     TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
     factory.addConnectorCustomizers(gracefulShutdown);
     return factory;
+  }
+
+  @PreDestroy
+  public void onDestroy() throws Exception {
+    LogManager.shutdown();
   }
 
   @Bean
