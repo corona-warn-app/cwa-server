5952cb94ec7cee539273580de9b013d1406c7fb1
Michael <66735191+mibrasap@users.noreply.github.com>
2020-07-20 19:46:19 +0200

    Feature/padding (#659)
    * Added padding to submission payload proto, added test
    
    * Refactor test
    
    * Refactor
    
    * Change padding from String to Bytes, set max request size to 100KB
    
    * Refactor
    
    * Added test for invalid submission payload
    
    * Add filter for requests larger than 100KB
    
    * Refactor tests
    
    * Add license header
    
    * Fix checkstyle
    
    * Refactor SubmissionControllerTest
    
    * Externalize max request size to application configuration
    
    * Update Javadoc, max request size as environment variable with default
    
    * Use Datasizeunit instead of Integer for maxRequestSize
    
    * Check limits of maximum-request-size
    
    * Adjust comment, refactor validator, javadoc
    
    * Remove unused import
    
    * Added tests for the SubmissionsServiceConfigValidator
    
    * Refactor tests
    
    Co-authored-by: Kevin Pontes <kevin.pontes@sap.com>
    Co-authored-by: Christian Kirschnick <christian.kirschnick@sap.com>

---
+++ services/submission/src/main/java/app/coronawarn/server/services/submission/config/SubmissionPayloadSizeFilter.java
@@ -0,0 +1,58 @@
+/*-
+ * ---license-start
+ * Corona-Warn-App
+ * ---
+ * Copyright (C) 2020 SAP SE and all other contributors
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ---license-end
+ */
+
+package app.coronawarn.server.services.submission.config;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * {@link SubmissionPayloadSizeFilter} instances filter requests exceeding a certain size limit.
+ */
+@Component
+public class SubmissionPayloadSizeFilter extends OncePerRequestFilter {
+
+  private final long maximumRequestSize;
+
+  public SubmissionPayloadSizeFilter(SubmissionServiceConfig config) {
+    this.maximumRequestSize = config.getMaximumRequestSize().toBytes();
+  }
+
+  /**
+   * Filters each request that exceeds the maximum size.
+   */
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    if (request.getContentLengthLong() > maximumRequestSize) {
+      response.setStatus(HttpStatus.BAD_REQUEST.value());
+      response.getWriter().write("Request size exceeded limit of " + maximumRequestSize + " bytes");
+    } else {
+      filterChain.doFilter(request, response);
+    }
+  }
+
+}
