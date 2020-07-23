eab31c00aa97e782168ac349ff790e773d18420f
Kevin Pontes <kevin.pontes@sap.com>
2020-07-23 14:01:55 +0200

    Added feign client for batch download

---
+++ services/federationdownload/src/main/java/app/coronawarn/server/services/federationdownload/download/BatchDownloader.java
@@ -0,0 +1,66 @@
+/*-
+ * ---license-start
+ * Corona-Warn-App
+ * ---
+ * Copyright (C) 2020 SAP SE and all other contributors
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ---license-end
+ */
+
+package app.coronawarn.server.services.federationdownload.download;
+
+import app.coronawarn.server.common.persistence.domain.FederationBatchDownload;
+import feign.FeignException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+
+/**
+ * The BatchDownloader downloads the batches containing the keys.
+ */
+@Service
+public class BatchDownloader {
+
+  private static final Logger logger = LoggerFactory.getLogger(BatchDownloader.class);
+  private final DownloadServerClient downloadServerClient;
+
+  /**
+   * This class can be used to get the batches of the configured federation gateway.
+   *
+   * @param downloadServerClient The REST client to communicate with the federation gateway server
+   */
+  public BatchDownloader(DownloadServerClient downloadServerClient) {
+    this.downloadServerClient = downloadServerClient;
+  }
+
+  /**
+   * Used to download the batches.
+   *
+   * @param federationBatchDownload Contains the BatchTag and date
+   * @return Returns the downloaded batch
+   * @throws RestClientException if status code is neither 2xx nor 4xx
+   */
+  public String downloadBatch(FederationBatchDownload federationBatchDownload) throws Exception {
+    try {
+      logger.info("Calling federation gateway download service for batch download ...");
+      String result = downloadServerClient.downloadBatch(federationBatchDownload);
+      logger.info("Received batch from federation gateway service");
+      return result;
+    } catch (Exception e) {
+      logger.info("Federation gateway service error");
+      throw new Exception(e.getMessage());
+    }
+  }
+}
--- services/federationdownload/src/main/java/app/coronawarn/server/services/federationdownload/runner/Download.java
+++ services/federationdownload/src/main/java/app/coronawarn/server/services/federationdownload/runner/Download.java
@@ -20,13 +20,11 @@
 
 package app.coronawarn.server.services.federationdownload.runner;
 
-import app.coronawarn.server.common.persistence.domain.DiagnosisKey;
+
 import app.coronawarn.server.common.persistence.domain.FederationBatchDownload;
 import app.coronawarn.server.common.persistence.service.FederationBatchDownloadService;
 import app.coronawarn.server.services.federationdownload.Application;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
+import app.coronawarn.server.services.federationdownload.download.BatchDownloader;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,10 +32,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+
 
 /**
  * This runner retrieves diagnosis key batches.
@@ -51,17 +47,19 @@ public class Download implements ApplicationRunner {
 
   private final ApplicationContext applicationContext;
   private final FederationBatchDownloadService federationBatchDownloadService;
-  private final RestTemplate restTemplate;
   private final String url = "https://postman-echo.com/get?foo1=bar1&foo2=bar2";
+
+  private final BatchDownloader batchDownloader;
 
   /**
    * Creates an Download, using {@link ApplicationContext}.
    */
+
   Download(ApplicationContext applicationContext, FederationBatchDownloadService federationBatchDownloadService,
-           RestTemplate restTemplate) {
+          BatchDownloader batchDownloader) {
     this.applicationContext = applicationContext;
     this.federationBatchDownloadService = federationBatchDownloadService;
-    this.restTemplate = restTemplate;
+    this.batchDownloader = batchDownloader;
   }
 
   @Override
@@ -70,6 +68,10 @@ public class Download implements ApplicationRunner {
       List<FederationBatchDownload> federationBatchDownloads =
           federationBatchDownloadService.getFederationBatchDownloads();
 
+      for (FederationBatchDownload federationBatchDownload : federationBatchDownloads) {
+        this.batchDownloader.downloadBatch(federationBatchDownload);
+
+      }
 
 
     } catch (Exception e) {
