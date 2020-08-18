8c0b514fa26c92a994155f616f4f2bb884a9583b
Eugen M <eugen.madean@sap.com>
2020-08-18 10:30:24 +0300

    Added docker file

--- services/federation-upload/build_and_run.sh
+++ services/federation-upload/build_and_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Builds and runs the federationdownload module
+# Builds and runs the federation-upload module
 
 pushd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null || exit
 
@@ -8,9 +8,9 @@ cd ../..
 
 docker build                                              \
   --rm                                                    \
-  -f services/federationdownload/Dockerfile                     \
-  -t federationdownload . &&                                    \
+  -f services/federation-upload/Dockerfile                     \
+  -t federation-upload . &&                                    \
 docker run                                                \
-  -it federationdownload
+  -it federation-upload
 
 popd > /dev/null || exit
