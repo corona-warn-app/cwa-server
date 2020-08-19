3a100a4bea9eabac7646e7c18149035199152e77
Sorin Stefan Iovita <sorin.stefan.iovita@sap.com>
2020-08-19 14:47:28 +0300

    Feature/federation protobuf spec (#716)
    * Fix typo
    
    * Adjust diagnosis key payload according to the new specs
    Adjust tests
    Update diagnosis_key table
    
    * Fix typo
    
    Co-authored-by: Michael Braun <michael.braun01@sap.com>

--- services/submission/src/main/java/app/coronawarn/server/services/submission/controller/SubmissionController.java
+++ services/submission/src/main/java/app/coronawarn/server/services/submission/controller/SubmissionController.java
@@ -22,8 +22,8 @@ package app.coronawarn.server.services.submission.controller;
 
 import app.coronawarn.server.common.persistence.domain.DiagnosisKey;
 import app.coronawarn.server.common.persistence.service.DiagnosisKeyService;
+import app.coronawarn.server.common.protocols.external.exposurenotification.ReportType;
 import app.coronawarn.server.common.protocols.external.exposurenotification.TemporaryExposureKey;
-import app.coronawarn.server.common.protocols.external.exposurenotification.VerificationType;
 import app.coronawarn.server.common.protocols.internal.SubmissionPayload;
 import app.coronawarn.server.services.submission.config.SubmissionServiceConfig;
 import app.coronawarn.server.services.submission.monitoring.SubmissionMonitor;
@@ -129,13 +129,13 @@ public class SubmissionController {
       String originCountry = StringUtils.defaultIfBlank(submissionPayload.getOrigin(),
           submissionServiceConfig.getDefaultOriginCountry());
       // The protobuf will default the enum to LAB_VERIFIED, since its index is 0
-      VerificationType verificationType = submissionPayload.getVerificationType();
+      ReportType reportType = submissionPayload.getReportType();
 
       DiagnosisKey diagnosisKey = DiagnosisKey.builder()
           .fromTemporaryExposureKey(protoBufferKey)
           .withVisitedCountries(submissionPayload.getVisitedCountriesList())
           .withCountryCode(originCountry)
-          .withVerificationType(verificationType)
+          .withReportType(reportType)
           .withConsentToFederation(submissionPayload.getConsentToFederation())
           .build();
 
@@ -161,7 +161,7 @@ public class SubmissionController {
               .withRollingPeriod(diagnosisKey.getRollingPeriod())
               .withVisitedCountries(diagnosisKey.getVisitedCountries())
               .withCountryCode(diagnosisKey.getOriginCountry())
-              .withVerificationType(diagnosisKey.getVerificationType())
+              .withReportType(diagnosisKey.getReportType())
               .withConsentToFederation(diagnosisKey.isConsentToFederation())
               .build())
           .forEach(paddedDiagnosisKeys::add);
