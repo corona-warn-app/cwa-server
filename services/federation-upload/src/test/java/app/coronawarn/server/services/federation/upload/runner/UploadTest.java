a71c2cc84dc32e55df586a48b29f537fa2c096cb
Frederico Bittencourt <fred.rbittencourt@gmail.com>
2020-08-24 09:54:54 +0200

    add spring test and fix code smell

--- services/federation-upload/src/test/java/app/coronawarn/server/services/federation/upload/runner/UploadTest.java
+++ services/federation-upload/src/test/java/app/coronawarn/server/services/federation/upload/runner/UploadTest.java
@@ -3,13 +3,11 @@ package app.coronawarn.server.services.federation.upload.runner;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {Upload.class}, initializers = ConfigFileApplicationContextInitializer.class)
+@SpringBootTest
 class UploadTest {
 
   @Autowired
