0176ed99a0192a0660ba0b3550c38be0ca732e52
Kevin Pontes <kevin.pontes@sap.com>
2020-08-24 15:52:40 +0200

    Added app version parameters to application.yaml

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/appconfig/ApplicationConfigurationPublicationConfig.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/appconfig/ApplicationConfigurationPublicationConfig.java
@@ -27,10 +27,11 @@ import app.coronawarn.server.common.protocols.internal.ApplicationVersionConfigu
 import app.coronawarn.server.common.protocols.internal.ApplicationVersionInfo;
 import app.coronawarn.server.common.protocols.internal.SemanticVersion;
 import app.coronawarn.server.services.distribution.config.DistributionServiceConfig;
-import java.util.Arrays;
+import app.coronawarn.server.services.distribution.config.DistributionServiceConfig.AppVersions;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 
 /**
@@ -61,14 +62,46 @@ public class ApplicationConfigurationPublicationConfig {
   public ApplicationConfiguration createMasterConfiguration(DistributionServiceConfig distributionServiceConfig)
       throws UnableToLoadFileException {
 
-    List<String> suggestedCountries = distributionServiceConfig.getSupportedCountries() == null ? List.of("")
-        : distributionServiceConfig.getSupportedCountries();
-
     return YamlLoader.loadYamlIntoProtobufBuilder(MASTER_FILE, Builder.class)
         .setAppFeatures(
             AppFeatures.newBuilder().addAllAppFeatures(distributionServiceConfig.getAppFeaturesProto()).build()
         )
-        .addAllSupportedCountries(suggestedCountries)
+        .addAllSupportedCountries(List.of(distributionServiceConfig.getSupportedCountries()))
+        .setAppVersion(buildApplicationVersionConfiguration(distributionServiceConfig))
         .build();
+  }
+
+  /**
+   * Fetches the master configuration as a ApplicationConfiguration instance.
+   *
+   * @return test.
+   */
+  public ApplicationVersionConfiguration buildApplicationVersionConfiguration(
+      DistributionServiceConfig distributionServiceConfig) {
+    AppVersions appVersions = distributionServiceConfig.getAppVersions();
+    return ApplicationVersionConfiguration.newBuilder()
+        .setAndroid(buildApplicationVersionInfo(appVersions.getLatestAndroid(), appVersions.getMinAndroid()))
+        .setIos(buildApplicationVersionInfo(appVersions.getLatestIos(), appVersions.getMinIos()))
+        .build();
+  }
+
+  private ApplicationVersionInfo buildApplicationVersionInfo(String latestVersion, String minVersion) {
+    return ApplicationVersionInfo.newBuilder()
+        .setLatest(buildSemanticVersion(latestVersion))
+        .setMin(buildSemanticVersion(minVersion))
+        .build();
+  }
+
+  private SemanticVersion buildSemanticVersion(String version) {
+    return SemanticVersion.newBuilder()
+        .setMajor(getSemanticVersionNumber(version, 0))
+        .setMinor(getSemanticVersionNumber(version, 1))
+        .setPatch(getSemanticVersionNumber(version, 2))
+        .build();
+  }
+
+  private int getSemanticVersionNumber(String version, int position) {
+    String[] items = version.split("\\.");
+    return Integer.valueOf(items[position]);
   }
 }
