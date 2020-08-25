5e81409fad62902edac2c1a30edf4056af26f8d8
KevponSAP <66735382+KevponSAP@users.noreply.github.com>
2020-08-25 17:25:06 +0200

    Feature/enrich app config (#726)
    * Added supported countries to app config with application.yaml parameter
    
    * Fixed Test failures
    
    * Remove unused code
    
    * Added app version parameters to application.yaml
    
    * Added tests for app version parameters
    
    * Refacted country list implementation
    
    Co-authored-by: Hilmar Falkenberg <hilmar.falkenberg@sap.com>

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/appconfig/ApplicationConfigurationPublicationConfig.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/appconfig/ApplicationConfigurationPublicationConfig.java
@@ -23,9 +23,16 @@ package app.coronawarn.server.services.distribution.assembly.appconfig;
 import app.coronawarn.server.common.protocols.internal.AppFeatures;
 import app.coronawarn.server.common.protocols.internal.ApplicationConfiguration;
 import app.coronawarn.server.common.protocols.internal.ApplicationConfiguration.Builder;
+import app.coronawarn.server.common.protocols.internal.ApplicationVersionConfiguration;
+import app.coronawarn.server.common.protocols.internal.ApplicationVersionInfo;
+import app.coronawarn.server.common.protocols.internal.SemanticVersion;
 import app.coronawarn.server.services.distribution.config.DistributionServiceConfig;
+import app.coronawarn.server.services.distribution.config.DistributionServiceConfig.AppVersions;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+
 
 /**
  * Provides the application configuration needed for the mobile client. Contains all necessary sub-configs, including:
@@ -54,9 +61,47 @@ public class ApplicationConfigurationPublicationConfig {
   @Bean
   public ApplicationConfiguration createMasterConfiguration(DistributionServiceConfig distributionServiceConfig)
       throws UnableToLoadFileException {
+
     return YamlLoader.loadYamlIntoProtobufBuilder(MASTER_FILE, Builder.class)
         .setAppFeatures(
-            AppFeatures.newBuilder().addAllAppFeatures(distributionServiceConfig.getAppFeaturesProto()).build())
+            AppFeatures.newBuilder().addAllAppFeatures(distributionServiceConfig.getAppFeaturesProto()).build()
+        )
+        .addAllSupportedCountries(List.of(distributionServiceConfig.getSupportedCountries()))
+        .setAppVersion(buildApplicationVersionConfiguration(distributionServiceConfig))
         .build();
+  }
+
+  /**
+   * Fetches the master configuration as a ApplicationConfiguration instance.
+   *
+   * @return test.
+   */
+  public ApplicationVersionConfiguration buildApplicationVersionConfiguration(
+      DistributionServiceConfig distributionServiceConfig) {
+    AppVersions appVersions = distributionServiceConfig.getAppVersions();
+    return ApplicationVersionConfiguration.newBuilder()
+        .setAndroid(buildApplicationVersionInfo(appVersions.getLatestAndroid(), appVersions.getMinAndroid()))
+        .setIos(buildApplicationVersionInfo(appVersions.getLatestIos(), appVersions.getMinIos()))
+        .build();
+  }
+
+  private ApplicationVersionInfo buildApplicationVersionInfo(String latestVersion, String minVersion) {
+    return ApplicationVersionInfo.newBuilder()
+        .setLatest(buildSemanticVersion(latestVersion))
+        .setMin(buildSemanticVersion(minVersion))
+        .build();
+  }
+
+  private SemanticVersion buildSemanticVersion(String version) {
+    return SemanticVersion.newBuilder()
+        .setMajor(getSemanticVersionNumber(version, 0))
+        .setMinor(getSemanticVersionNumber(version, 1))
+        .setPatch(getSemanticVersionNumber(version, 2))
+        .build();
+  }
+
+  private int getSemanticVersionNumber(String version, int position) {
+    String[] items = version.split("\\.");
+    return Integer.valueOf(items[position]);
   }
 }
