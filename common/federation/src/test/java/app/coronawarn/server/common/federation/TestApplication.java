87f21041d5a7316d8024d1be1b1318d23f76b78c
EugenM-SAP <67458405+EugenM-SAP@users.noreply.github.com>
2020-08-26 16:08:57 +0300

    Feature/federation gateway configure ssl (#727)
    * Create separate maven module for federation common
    Move FeignClientProvider from submission to federation module
    
    * Remove redundant classes
    
    * Remove unneeded interface
    
    * Remove failing unneeded test
    
    * Fixed compile issues
    
    * Added all required feign configuration to common module
    
    * fixed javadoc typo
    
    * Read connection-pool-size from application.yaml
    
    * Refactor, delete unused file
    
    Co-authored-by: Sorin Stefan Iovita <sorin.stefan.iovita@sap.com>
    Co-authored-by: Michael Braun <michael.braun01@sap.com>

--- common/federation/src/test/java/app/coronawarn/server/common/federation/TestApplication.java
+++ common/federation/src/test/java/app/coronawarn/server/common/federation/TestApplication.java
@@ -1,4 +1,4 @@
-/*
+/*-
  * ---license-start
  * Corona-Warn-App
  * ---
@@ -8,7 +8,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,10 +18,15 @@
  * ---license-end
  */
 
-package app.coronawarn.server.services.submission.verification;
+package app.coronawarn.server.common.federation;
 
-import feign.Client;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 
-public interface FeignClientProvider {
-  Client createFeignClient();
+@SpringBootApplication
+@EnableFeignClients
+@Configuration
+public class TestApplication {
+
 }
