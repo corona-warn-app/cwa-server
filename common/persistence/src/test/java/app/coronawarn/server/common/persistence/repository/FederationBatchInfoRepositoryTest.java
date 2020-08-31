bd15742ceb0b3d56921207e94e3f19aaf7a59c55
Michael Braun <michael.braun01@sap.com>
2020-08-31 13:34:59 +0200

    Add tests for FederationBatchInfoRepository

--- common/persistence/src/test/java/app/coronawarn/server/common/persistence/repository/FederationBatchInfoRepositoryTest.java
+++ common/persistence/src/test/java/app/coronawarn/server/common/persistence/repository/FederationBatchInfoRepositoryTest.java
@@ -7,12 +7,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import app.coronawarn.server.common.persistence.domain.FederationBatchInfo;
 import app.coronawarn.server.common.persistence.domain.FederationBatchStatus;
 import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.provider.Arguments;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,22 +20,15 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @AutoConfigureWebTestClient
 public class FederationBatchInfoRepositoryTest {
 
-  @Autowired
-  private FederationBatchInfoRepository federationBatchInfoRepository;
-
   private static final String batchTag1 = "11111";
   private static final String batchTag2 = "22222";
-  private static final String batchTag3 = "33333";
-  private static final String batchTag4 = "44444";
-
   private static final LocalDate date1 = LocalDate.parse("2020-08-15");
   private static final LocalDate date2 = LocalDate.parse("2020-08-16");
-  private static final LocalDate date3 = LocalDate.parse("2020-08-17");
-  private static final LocalDate date4 = LocalDate.parse("2020-08-18");
-
   private static final String statusError = FederationBatchStatus.ERROR.name();
   private static final String statusProcessed = FederationBatchStatus.PROCESSED.name();
   private static final String statusUnprocessed = FederationBatchStatus.UNPROCESSED.name();
+  @Autowired
+  private FederationBatchInfoRepository federationBatchInfoRepository;
 
   @AfterEach
   void tearDown() {
@@ -46,17 +36,30 @@ public class FederationBatchInfoRepositoryTest {
   }
 
   @Test
-  void testReturnsEmptyListIfNoUnprocessedBatch() {
+  public void testStatusIsReturnedCorrectly() {
     federationBatchInfoRepository.saveDoNothingOnConflict(batchTag1, date1, statusProcessed);
-    assertThat(federationBatchInfoRepository.findByStatus("UNPROCESSED")).isEmpty();
+    assertThat(federationBatchInfoRepository.findByStatus(statusProcessed))
+        .isEqualTo(singletonList(new FederationBatchInfo(batchTag1, date1, FederationBatchStatus.PROCESSED)));
   }
 
   @Test
-  void testDoesNothingOnConflict() {
-    federationBatchInfoRepository.saveDoNothingOnConflict(batchTag2, date2, statusUnprocessed);
-    federationBatchInfoRepository.saveDoNothingOnConflict(batchTag2, date1, statusError);
+  public void testReturnsEmptyIfStatusDoesNotMatch() {
+    federationBatchInfoRepository.saveDoNothingOnConflict(batchTag1, date1, statusProcessed);
+    assertThat(federationBatchInfoRepository.findByStatus(statusUnprocessed)).isEmpty();
+  }
 
-    List<FederationBatchInfo> actBatches = federationBatchInfoRepository.findByStatus("UNPROCESSED");
-    assertThat(actBatches).isEqualTo(singletonList(new FederationBatchInfo(batchTag2, date2)));
+  @Test
+  public void testDoesNothingOnConflict() {
+    federationBatchInfoRepository.saveDoNothingOnConflict(batchTag1, date1, statusUnprocessed);
+    federationBatchInfoRepository.saveDoNothingOnConflict(batchTag1, date2, statusError);
+    federationBatchInfoRepository.saveDoNothingOnConflict(batchTag2, date2, statusError);
+    assertThat(federationBatchInfoRepository.findByStatus(statusUnprocessed))
+        .isEqualTo(singletonList(new FederationBatchInfo(batchTag1, date1, FederationBatchStatus.UNPROCESSED)));
+  }
+
+  @Test
+  void testReturnsEmptyListIfNoUnprocessedBatch() {
+    federationBatchInfoRepository.saveDoNothingOnConflict(batchTag1, date1, statusProcessed);
+    assertThat(federationBatchInfoRepository.findByStatus(statusUnprocessed)).isEmpty();
   }
 }
