759a64bf2adef38ebdb38e4f56e5063399aac891
EugenM-SAP <67458405+EugenM-SAP@users.noreply.github.com>
2020-09-02 14:53:45 +0300

    Upload batch determination (#732)
    * Added batch determination logic
    
    * Externalize batch size properties to yaml config
    
    * PR review changes
    
    * Sonar code smells

--- services/upload/src/test/java/app/coronawarn/server/services/federation/upload/payload/DiagnosisKeyBatchAssemblerTest.java
+++ services/upload/src/test/java/app/coronawarn/server/services/federation/upload/payload/DiagnosisKeyBatchAssemblerTest.java
@@ -22,26 +22,47 @@ package app.coronawarn.server.services.federation.upload.payload;
 
 import app.coronawarn.server.common.persistence.domain.DiagnosisKey;
 import app.coronawarn.server.common.protocols.external.exposurenotification.ReportType;
+import app.coronawarn.server.services.federation.upload.config.UploadServiceConfig;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static java.util.Collections.emptyList;
 
+@EnableConfigurationProperties(value = UploadServiceConfig.class)
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {
     DiagnosisKeyBatchAssembler.class}, initializers = ConfigFileApplicationContextInitializer.class)
 class DiagnosisKeyBatchAssemblerTest {
 
+  private static volatile int minKeyThreshold;
+  private static volatile int maxKeyCount;
+
   @Autowired
   DiagnosisKeyBatchAssembler diagnosisKeyBatchAssembler;
+
+  @Autowired
+  UploadServiceConfig uploadServiceConfig;
+
+  @BeforeEach
+  public void setup() {
+    minKeyThreshold = uploadServiceConfig.getMinBatchKeyCount();
+    maxKeyCount = uploadServiceConfig.getMaxBatchKeyCount();
+  }
 
   private void assertKeysAreEqual(DiagnosisKey persistenceKey,
       app.coronawarn.server.common.protocols.external.exposurenotification.DiagnosisKey exportKey) {
@@ -60,7 +81,7 @@ class DiagnosisKeyBatchAssemblerTest {
         "Origin Country should be the same");
   }
 
-  private DiagnosisKey makeFakeKey(boolean consent) {
+  private static DiagnosisKey makeFakeKey(boolean consent) {
     return DiagnosisKey.builder()
         .withKeyData(new byte[16])
         .withRollingStartIntervalNumber(1)
@@ -74,8 +95,13 @@ class DiagnosisKeyBatchAssemblerTest {
         .build();
   }
 
-  private DiagnosisKey makeFakeKey() {
-    return this.makeFakeKey(true);
+  private static List<DiagnosisKey> makeFakeKeys(boolean consent, int numberOfKeys) {
+    List<DiagnosisKey> keys = new ArrayList<DiagnosisKey>(numberOfKeys);
+    while (numberOfKeys > 0) {
+      keys.add(makeFakeKey(consent));
+      numberOfKeys--;
+    }
+    return keys;
   }
 
   @Test
@@ -85,27 +111,55 @@ class DiagnosisKeyBatchAssemblerTest {
   }
 
   @Test
-  void shouldReturnSingleKeyInPackage() {
-    var fakeKey = makeFakeKey();
-    var result = diagnosisKeyBatchAssembler.assembleDiagnosisKeyBatch(List.of(fakeKey));
-    Assertions.assertEquals(1, result.size());
-    Assertions.assertEquals(1, result.get(0).getKeysCount());
-    this.assertKeysAreEqual(fakeKey, result.get(0).getKeys(0));
+  void shouldReturnEmptyListIfLessThenThresholdKeysGiven() {
+    var result = diagnosisKeyBatchAssembler.assembleDiagnosisKeyBatch(makeFakeKeys(true, minKeyThreshold - 1));
+    Assertions.assertTrue(result.isEmpty());
   }
 
   @Test
-  void shouldReturnMultipleKeysInPackage() {
-    var result = diagnosisKeyBatchAssembler.assembleDiagnosisKeyBatch(List.of(makeFakeKey(), makeFakeKey()));
-    Assertions.assertEquals(1, result.size());
-    Assertions.assertEquals(2, result.get(0).getKeysCount());
+  void packagedKeysShouldContainInitialInformation() {
+    var fakeKeys = makeFakeKeys(true, minKeyThreshold);
+    var result = diagnosisKeyBatchAssembler.assembleDiagnosisKeyBatch(fakeKeys);
+
+    Assertions.assertEquals(fakeKeys.size(),result.get(0).getKeysCount());
+    // as keys are created equal we need to compare just the first two elements of each list
+    assertKeysAreEqual(fakeKeys.get(0), result.get(0).getKeys(0));
   }
 
   @Test
   void shouldNotPackageKeysIfConsentFlagIsNotSet() {
-    var result = diagnosisKeyBatchAssembler.assembleDiagnosisKeyBatch(List.of(makeFakeKey(), makeFakeKey(false)));
+    var dataset = makeFakeKeys(true, minKeyThreshold);
+    dataset.add(makeFakeKey(false));
+    var result = diagnosisKeyBatchAssembler.assembleDiagnosisKeyBatch(dataset);
     Assertions.assertEquals(1, result.size());
-    Assertions.assertEquals(1, result.get(0).getKeysCount());
+    Assertions.assertEquals(minKeyThreshold, result.get(0).getKeysCount());
   }
 
+  @ParameterizedTest
+  @MethodSource("keysToPartitionAndBatchNumberExpectations")
+  void shouldGenerateCorrectNumberOfBatches(List<DiagnosisKey> dataset, Integer expectedBatches) {
+    var result = diagnosisKeyBatchAssembler.assembleDiagnosisKeyBatch(dataset);
+    Assertions.assertEquals(expectedBatches, result.size());
+  }
 
+  /**
+   * @return A stream of tuples which represents the dataset together with the
+   * expectation required to test batch key partioning.
+   */
+  private static Stream<Arguments> keysToPartitionAndBatchNumberExpectations() {
+    return Stream.of(
+        Arguments.of(makeFakeKeys(true, minKeyThreshold - 1), 0),
+        Arguments.of(makeFakeKeys(true, minKeyThreshold), 1),
+        Arguments.of(makeFakeKeys(true, maxKeyCount), 1),
+        Arguments.of(makeFakeKeys(true, maxKeyCount / 2), 1),
+        Arguments.of(makeFakeKeys(true, maxKeyCount - 1), 1),
+        Arguments.of(makeFakeKeys(true, maxKeyCount + 1), 2),
+        Arguments.of(makeFakeKeys(true, 2 * maxKeyCount), 2),
+        Arguments.of(makeFakeKeys(true, 3 * maxKeyCount), 3),
+        Arguments.of(makeFakeKeys(true, 4 * maxKeyCount), 4),
+        Arguments.of(makeFakeKeys(true, 2 * maxKeyCount + 1), 3),
+        Arguments.of(makeFakeKeys(true, 2 * maxKeyCount + maxKeyCount / 2), 3),
+        Arguments.of(makeFakeKeys(true, 2 * maxKeyCount - maxKeyCount / 2), 2)
+    );
+  }
 }
