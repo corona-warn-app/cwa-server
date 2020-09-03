b26941d6b635b866147ba655d910c0048dffb12c
Michael Braun <michael.braun01@sap.com>
2020-09-03 14:01:35 +0200

    Fix style

--- common/persistence/src/test/java/app/coronawarn/server/common/persistence/TestApplication.java
+++ common/persistence/src/test/java/app/coronawarn/server/common/persistence/TestApplication.java
@@ -36,7 +36,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TestApplication {
 
-
   @Bean
   ValidDiagnosisKeyFilter validKeysFilter() {
     return new ValidDiagnosisKeyFilter();
