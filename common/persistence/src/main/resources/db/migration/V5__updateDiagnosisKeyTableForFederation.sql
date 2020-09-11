bbbbdf28a1829e9ac717161e006418647cd7b3f7
Michael <66735191+mibrasap@users.noreply.github.com>
2020-09-11 18:52:46 +0200

    Set default values for diagnosis key table migration (#761)
    Co-authored-by: Kevin <kevin.pontes@sap.com>

--- common/persistence/src/main/resources/db/migration/V5__updateDiagnosisKeyTableForFederation.sql
+++ common/persistence/src/main/resources/db/migration/V5__updateDiagnosisKeyTableForFederation.sql
@@ -4,13 +4,13 @@ ALTER TABLE diagnosis_key
     ADD consent_to_federation BOOLEAN NOT NULL DEFAULT FALSE;
 
 ALTER TABLE diagnosis_key
-    ADD origin_country VARCHAR(2);
+    ADD origin_country VARCHAR(2) DEFAULT 'DE';
 
 ALTER TABLE diagnosis_key
     ADD visited_countries VARCHAR(2)[];
 
 ALTER TABLE diagnosis_key
-    ADD report_type VARCHAR(30);
+    ADD report_type VARCHAR(30) DEFAULT 'CONFIRMED_CLINICAL_DIAGNOSIS';
 
 ALTER TABLE diagnosis_key
     ADD days_since_onset_of_symptoms INTEGER;
