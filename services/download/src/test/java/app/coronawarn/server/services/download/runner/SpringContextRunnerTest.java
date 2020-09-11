24aad6f09d5af11e5a87d2d7577d6d640da5bca5
Michael Braun <michael.braun01@sap.com>
2020-09-11 11:39:50 +0200

    Enhance tests

--- services/download/src/test/java/app/coronawarn/server/services/download/runner/SpringContextRunnerTest.java
+++ services/download/src/test/java/app/coronawarn/server/services/download/runner/SpringContextRunnerTest.java
@@ -16,24 +16,26 @@ import app.coronawarn.server.services.download.FederationBatchTestHelper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.http.HttpHeader;
 import com.github.tomakehurst.wiremock.http.HttpHeaders;
-import javax.annotation.PostConstruct;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("federation-download-integration")
+// TODO rename test
 public class SpringContextRunnerTest {
 
-  public static final String keyData1 = "0123456789ABCDED";
-  public static final String keyData2 = "0123456789ABCDEE";
-  public static final String keyData3 = "0123456789ABCDEF";
-  private static final String batchTag1 = "19729de860ea";
-  private static final String batchTag2 = "19729de860eb";
-  private static final String batchTag3 = "19729de860ec";
+  public static final String KEY_DATA_1 = "0123456789ABCDED";
+  public static final String KEY_DATA_2 = "0123456789ABCDEE";
+  public static final String KEY_DATA_3 = "0123456789ABCDEF";
+  private static final String BATCH_TAG_1 = "19729de860ea";
+  private static final String BATCH_TAG_2 = "19729de860eb";
+  private static final String BATCH_TAG_3 = "19729de860ec";
+  public static final String EMPTY_BATCH_TAG = "null";
 
   private static WireMockServer server;
 
@@ -45,13 +47,16 @@ public class SpringContextRunnerTest {
 
   @BeforeAll
   static void setupWireMock() {
-    HttpHeaders batchHeaders1 = getHttpHeaders(batchTag1, batchTag2);
-    DiagnosisKeyBatch batch1 = FederationBatchTestHelper.createDiagnosisKeyBatch(keyData1);
+    HttpHeaders batchHeaders1 = getHttpHeaders(BATCH_TAG_1, BATCH_TAG_2);
+    DiagnosisKeyBatch batch1 = FederationBatchTestHelper.createDiagnosisKeyBatch(KEY_DATA_1);
 
-    HttpHeaders batchHeaders2 = getHttpHeaders(batchTag2, batchTag3);
-    DiagnosisKeyBatch batch2 = FederationBatchTestHelper.createDiagnosisKeyBatch(keyData2);
+    HttpHeaders batchHeaders2 = getHttpHeaders(BATCH_TAG_2, BATCH_TAG_3);
+    DiagnosisKeyBatch batch2 = FederationBatchTestHelper.createDiagnosisKeyBatch(KEY_DATA_2);
 
-    HttpHeaders batchHeaders3 = getHttpHeaders(batchTag3, "null");
+    HttpHeaders batchHeaders3 = getHttpHeaders(BATCH_TAG_3, EMPTY_BATCH_TAG);
+
+    HttpHeaders retryBatchHeaders = getHttpHeaders("retry_batch_tag_1", EMPTY_BATCH_TAG);
+    DiagnosisKeyBatch retryDiagnosisKeyBatch = FederationBatchTestHelper.createDiagnosisKeyBatch(KEY_DATA_3);
 
     server = new WireMockServer(options().port(1234));
     server.start();
@@ -64,7 +69,7 @@ public class SpringContextRunnerTest {
                     .withBody(batch1.toByteArray())));
     server.stubFor(
         get(anyUrl())
-            .withHeader("batchTag", equalTo(batchTag2))
+            .withHeader("batchTag", equalTo(BATCH_TAG_2))
             .willReturn(
                 aResponse()
                     .withStatus(HttpStatus.OK.value())
@@ -72,11 +77,26 @@ public class SpringContextRunnerTest {
                     .withBody(batch2.toByteArray())));
     server.stubFor(
         get(anyUrl())
-            .withHeader("batchTag", equalTo(batchTag3))
+            .withHeader("batchTag", equalTo(BATCH_TAG_3))
             .willReturn(
                 aResponse()
                     .withStatus(HttpStatus.NOT_FOUND.value())
                     .withHeaders(batchHeaders3)));
+    server.stubFor(
+        get(anyUrl())
+            .withHeader("batchTag", equalTo("retry_batch_tag_1"))
+            .willReturn(
+                aResponse()
+                    .withStatus(HttpStatus.NOT_FOUND.value())
+                    .withHeaders(batchHeaders3)));
+    server.stubFor(
+        get(anyUrl())
+            .withHeader("batchTag", equalTo("retry_batch_tag_2"))
+            .willReturn(
+                aResponse()
+                    .withStatus(HttpStatus.OK.value())
+                    .withHeaders(retryBatchHeaders)
+                    .withBody(retryDiagnosisKeyBatch.toByteArray())));
   }
 
   private static HttpHeaders getHttpHeaders(String batchTag, String nextBatchTag) {
@@ -92,19 +112,23 @@ public class SpringContextRunnerTest {
   }
 
   @Test
-  @DirtiesContext
+  //TODO failure messages in asserts
   void testDownloadRunSuccessfully() {
-    assertThat(federationBatchInfoRepository.findAll()).hasSize(3);
-    assertThat(federationBatchInfoRepository.findByStatus("PROCESSED")).hasSize(2);
+    assertThat(federationBatchInfoRepository.findAll()).hasSize(5);
+    assertThat(federationBatchInfoRepository.findByStatus("UNPROCESSED")).hasSize(0);
+    assertThat(federationBatchInfoRepository.findByStatus("PROCESSED")).hasSize(3);
     assertThat(federationBatchInfoRepository.findByStatus("ERROR")).hasSize(1);
+    assertThat(federationBatchInfoRepository.findByStatus("ERROR_WONT_RETRY")).hasSize(1);
 
     Iterable<DiagnosisKey> diagnosisKeys = diagnosisKeyRepository.findAll();
-    assertThat(diagnosisKeys).hasSize(2);
-    assertThat(diagnosisKeys).contains(
-        DiagnosisKey.builder()
-            .fromFederationDiagnosisKey(FederationBatchTestHelper.createDiagnosisKey(keyData1)).build());
-    assertThat(diagnosisKeys).contains(
-        DiagnosisKey.builder()
-            .fromFederationDiagnosisKey(FederationBatchTestHelper.createDiagnosisKey(keyData2)).build());
+    assertThat(diagnosisKeys).hasSize(3);
+    assertThat(diagnosisKeys).contains(createDiagnosisKey(KEY_DATA_1));
+    assertThat(diagnosisKeys).contains(createDiagnosisKey(KEY_DATA_2));
+    assertThat(diagnosisKeys).contains(createDiagnosisKey(KEY_DATA_3));
+  }
+
+  private DiagnosisKey createDiagnosisKey(String keyData1) {
+    return DiagnosisKey.builder()
+        .fromFederationDiagnosisKey(FederationBatchTestHelper.createDiagnosisKey(keyData1)).build();
   }
 }
---
+++ services/download/src/test/resources/db/testdata/V99__createTestDataForDownloadIntegrationTest.sql
@@ -0,0 +1,3 @@
+INSERT INTO federation_batch_info(batch_tag, date, status) VALUES ('expired_batch_tag', '2000-01-01', 'UNPROCESSED');
+INSERT INTO federation_batch_info(batch_tag, date, status) VALUES ('retry_batch_tag_1', CURRENT_DATE, 'ERROR');
+INSERT INTO federation_batch_info(batch_tag, date, status) VALUES ('retry_batch_tag_2', CURRENT_DATE, 'ERROR');
