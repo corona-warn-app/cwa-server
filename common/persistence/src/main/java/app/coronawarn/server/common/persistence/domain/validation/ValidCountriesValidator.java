abd5aca19082dd114709111ed3bc07d95239159b
Michael Burwig <michael.burwig@sap.com>
2020-09-16 09:44:24 +0200

    Remove AtomicBoolean use in validators (#774)

--- common/persistence/src/main/java/app/coronawarn/server/common/persistence/domain/validation/ValidCountriesValidator.java
+++ common/persistence/src/main/java/app/coronawarn/server/common/persistence/domain/validation/ValidCountriesValidator.java
@@ -20,29 +20,17 @@
 
 package app.coronawarn.server.common.persistence.domain.validation;
 
-
-import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
-import java.util.concurrent.atomic.AtomicBoolean;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class ValidCountriesValidator
-    implements ConstraintValidator<ValidCountries, List<String>> {
+public class ValidCountriesValidator implements ConstraintValidator<ValidCountries, List<String>> {
 
-  private static final List<String> ISO_COUNTRIES = Arrays.asList(Locale.getISOCountries());
+  private static final List<String> ISO_COUNTRIES = List.of(Locale.getISOCountries());
 
   @Override
   public boolean isValid(List<String> countries, ConstraintValidatorContext constraintValidatorContext) {
-    AtomicBoolean validCountries = new AtomicBoolean(true);
-
-    countries.forEach(country -> {
-      if (!ISO_COUNTRIES.contains(country)) {
-        validCountries.set(false);
-      }
-    });
-    return validCountries.get();
+    return ISO_COUNTRIES.containsAll(countries);
   }
-
 }
