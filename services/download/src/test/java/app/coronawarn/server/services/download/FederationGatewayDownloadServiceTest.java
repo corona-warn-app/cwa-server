bf05a02068d29a932f760ebdb7db9d54f4531054
Michael Braun <michael.braun01@sap.com>
2020-09-24 10:50:44 +0200

    Refactor test

--- services/download/src/test/java/app/coronawarn/server/services/download/FederationGatewayDownloadServiceTest.java
+++ services/download/src/test/java/app/coronawarn/server/services/download/FederationGatewayDownloadServiceTest.java
@@ -23,7 +23,6 @@ package app.coronawarn.server.services.download;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.anyUrl;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.Mockito.mock;
@@ -34,7 +33,6 @@ import java.time.LocalDate;
 import java.util.Optional;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,11 +44,10 @@ import org.springframework.test.annotation.DirtiesContext;
 @DirtiesContext
 class FederationGatewayDownloadServiceTest {
 
-  @Autowired
   private static final WireMockServer server = new WireMockServer(1234);
 
   @Autowired
-  private FederationGatewayDownloadService federationGatewayDownloadService;
+  private FederationGatewayDownloadService downloadService;
 
   @BeforeEach
   void ensureRunningServer() {
@@ -78,9 +75,9 @@ class FederationGatewayDownloadServiceTest {
                     .withStatus(HttpStatus.NOT_FOUND.value())));
 
     assertThatExceptionOfType(FederationGatewayException.class)
-        .isThrownBy(() -> federationGatewayDownloadService.downloadBatch(mock(LocalDate.class)));
+        .isThrownBy(() -> downloadService.downloadBatch(mock(LocalDate.class)));
     assertThatExceptionOfType(FederationGatewayException.class)
-        .isThrownBy(() -> federationGatewayDownloadService.downloadBatch("batchTag", mock(LocalDate.class)));
+        .isThrownBy(() -> downloadService.downloadBatch("batchTag", mock(LocalDate.class)));
   }
 
   @Test
@@ -92,9 +89,9 @@ class FederationGatewayDownloadServiceTest {
                     .withStatus(HttpStatus.OK.value())));
 
     assertThatExceptionOfType(FederationGatewayException.class)
-        .isThrownBy(() -> federationGatewayDownloadService.downloadBatch(mock(LocalDate.class)));
+        .isThrownBy(() -> downloadService.downloadBatch(mock(LocalDate.class)));
     assertThatExceptionOfType(FederationGatewayException.class)
-        .isThrownBy(() -> federationGatewayDownloadService.downloadBatch("batchTag", mock(LocalDate.class)));
+        .isThrownBy(() -> downloadService.downloadBatch("batchTag", mock(LocalDate.class)));
   }
 
   @Test
@@ -112,8 +109,10 @@ class FederationGatewayDownloadServiceTest {
                     .withHeader("nextBatchTag", nextBatchTag)));
 
     LocalDate date = mock(LocalDate.class);
-    validateResponse(federationGatewayDownloadService.downloadBatch(date), batchTag, nextBatchTag);
-    validateResponse(federationGatewayDownloadService.downloadBatch(batchTag, date), batchTag, nextBatchTag);
+    BatchDownloadResponse expResponse = new BatchDownloadResponse(batchTag, Optional.empty(),
+        Optional.of(nextBatchTag));
+    assertThat(downloadService.downloadBatch(date)).isEqualTo(expResponse);
+    assertThat(downloadService.downloadBatch(batchTag, date)).isEqualTo(expResponse);
   }
 
   @Test
@@ -133,15 +132,15 @@ class FederationGatewayDownloadServiceTest {
                     .withBody(batch.toByteArray())));
 
     LocalDate date = mock(LocalDate.class);
-    BatchDownloadResponse response = federationGatewayDownloadService.downloadBatch(date);
-    validateResponse(response, batchTag, nextBatchTag, batch);
-    validateResponse(federationGatewayDownloadService.downloadBatch(batchTag, date), batchTag, nextBatchTag, batch);
+    BatchDownloadResponse expResponse = new BatchDownloadResponse(batchTag, Optional.of(batch),
+        Optional.of(nextBatchTag));
+    assertThat(downloadService.downloadBatch(date)).isEqualTo(expResponse);
+    assertThat(downloadService.downloadBatch(batchTag, date)).isEqualTo(expResponse);
   }
 
   @Test
   void testDownloadSuccessfulWithoutNextBatchTag() {
     String batchTag = "batch-tag";
-    String nextBatchTag = "next-batch-tag";
     DiagnosisKeyBatch batch = FederationBatchTestHelper.createDiagnosisKeyBatch("batch-data");
 
     server.stubFor(
@@ -154,27 +153,8 @@ class FederationGatewayDownloadServiceTest {
                     .withBody(batch.toByteArray())));
 
     LocalDate date = mock(LocalDate.class);
-
-    validateResponse(federationGatewayDownloadService.downloadBatch(date), batchTag, batch);
-    validateResponse(federationGatewayDownloadService.downloadBatch(batchTag, date), batchTag, batch);
-  }
-
-  private void validateResponse(BatchDownloadResponse response, String batchTag, DiagnosisKeyBatch batch) {
-    assertThat(response.getBatchTag()).isEqualTo(batchTag);
-    assertThat(response.getDiagnosisKeyBatch()).isEqualTo(Optional.of(batch));
-    assertThat(response.getNextBatchTag()).isEqualTo(Optional.empty());
-  }
-
-  private void validateResponse(BatchDownloadResponse response, String batchTag, String nextBatchTag,
-      DiagnosisKeyBatch batch) {
-    assertThat(response.getBatchTag()).isEqualTo(batchTag);
-    assertThat(response.getDiagnosisKeyBatch()).isEqualTo(Optional.of(batch));
-    assertThat(response.getNextBatchTag()).isEqualTo(Optional.of(nextBatchTag));
-  }
-
-  private void validateResponse(BatchDownloadResponse response, String batchTag, String nextBatchTag) {
-    assertThat(response.getBatchTag()).isEqualTo(batchTag);
-    assertThat(response.getDiagnosisKeyBatch()).isEmpty();
-    assertThat(response.getNextBatchTag()).isEqualTo(Optional.of(nextBatchTag));
+    BatchDownloadResponse expResponse = new BatchDownloadResponse(batchTag, Optional.of(batch), Optional.empty());
+    assertThat(downloadService.downloadBatch(date)).isEqualTo(expResponse);
+    assertThat(downloadService.downloadBatch(batchTag, date)).isEqualTo(expResponse);
   }
 }
