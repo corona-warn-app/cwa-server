bde8b0120d1777dfa747c1a1b8d71ae61b0050ab
ioangut <ioan.gut@sap.com>
2020-10-01 10:13:50 +0300

    remove unused import code smell

--- services/submission/src/main/java/app/coronawarn/server/services/submission/validation/ValidSubmissionPayload.java
+++ services/submission/src/main/java/app/coronawarn/server/services/submission/validation/ValidSubmissionPayload.java
@@ -28,7 +28,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.util.Arrays;
 import java.util.List;
 import javax.validation.Constraint;
 import javax.validation.ConstraintValidator;
