03757ef89f374bbb08e6fa3d3787ba36944ca966
Frederico Bittencourt <fred.rbittencourt@gmail.com>
2020-10-06 10:54:48 +0200

    fix batchsigning

--- services/upload/src/main/java/app/coronawarn/server/services/federation/upload/payload/signing/BatchSigner.java
+++ services/upload/src/main/java/app/coronawarn/server/services/federation/upload/payload/signing/BatchSigner.java
@@ -5,6 +5,8 @@ package app.coronawarn.server.services.federation.upload.payload.signing;
 import app.coronawarn.server.common.protocols.external.exposurenotification.DiagnosisKey;
 import app.coronawarn.server.common.protocols.external.exposurenotification.DiagnosisKeyBatch;
 import app.coronawarn.server.services.federation.upload.config.UploadServiceConfig;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.ProtocolStringList;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -48,50 +50,84 @@ public class BatchSigner {
     this.uploadServiceConfig = uploadServiceConfig;
   }
 
-  private byte[] bytesToBase64(byte[] bytes) {
+  private String bytesToBase64String(byte[] bytes) {
     try {
-      return Base64.getEncoder()
-          .encodeToString(bytes)
-          .getBytes(StandardCharsets.US_ASCII);
+      return Base64.getEncoder().encodeToString(bytes);
     } catch (IllegalArgumentException e) {
-      logger.error("Failed to convert byte array to Base64");
+      logger.error("Failed to convert byte array to string");
       return null;
     }
   }
 
   private byte[] createBytesToSign(final DiagnosisKeyBatch batch) {
+    final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+    sortBatchByKeyData(batch).stream()
+        .map(this::createBytesForKey)
+        .sorted(Comparator.nullsLast(
+            Comparator.comparing(this::bytesToBase64String)
+        ))
+        .forEach(buffer::writeBytes);
+    return buffer.toByteArray();
+  }
+
+  private byte[] createBytesForKey(final DiagnosisKey diagnosisKey) {
     final ByteArrayOutputStream batchBytes = new ByteArrayOutputStream();
-    for (DiagnosisKey diagnosisKey : sortBatchByKeyData(batch)) {
-      batchBytes.writeBytes(bytesToBase64(diagnosisKey.getKeyData().toStringUtf8().getBytes(StandardCharsets.UTF_8)));
-      batchBytes.writeBytes(".".getBytes(StandardCharsets.US_ASCII));
-      batchBytes.writeBytes(bytesToBase64(ByteBuffer.allocate(4)
-          .putInt(diagnosisKey.getRollingStartIntervalNumber()).array()));
-      batchBytes.writeBytes(".".getBytes(StandardCharsets.US_ASCII));
-      batchBytes.writeBytes(bytesToBase64(ByteBuffer.allocate(4)
-          .putInt(diagnosisKey.getRollingPeriod()).array()));
-      batchBytes.writeBytes(".".getBytes(StandardCharsets.US_ASCII));
-      batchBytes.writeBytes(bytesToBase64(ByteBuffer.allocate(4)
-          .putInt(diagnosisKey.getTransmissionRiskLevel()).array()));
-      batchBytes.writeBytes(".".getBytes(StandardCharsets.US_ASCII));
-
-      String countries = diagnosisKey.getVisitedCountriesList()
-          .stream().sorted(String::compareTo).collect(Collectors.joining(","));
-      batchBytes.writeBytes(bytesToBase64(countries.getBytes()));
-
-      batchBytes.writeBytes(bytesToBase64(diagnosisKey.getOrigin().getBytes(StandardCharsets.UTF_8)));
-      batchBytes.writeBytes(".".getBytes(StandardCharsets.US_ASCII));
-      batchBytes.writeBytes(bytesToBase64(ByteBuffer.allocate(4)
-          .putInt(diagnosisKey.getReportType().getNumber()).array()));
-      batchBytes.writeBytes(".".getBytes(StandardCharsets.US_ASCII));
-      batchBytes.writeBytes(bytesToBase64(ByteBuffer.allocate(4)
-          .putInt(diagnosisKey.getDaysSinceOnsetOfSymptoms()).array()));
-      batchBytes.writeBytes(".".getBytes(StandardCharsets.US_ASCII));
-    }
+    writeBytesInByteArray(diagnosisKey.getKeyData(), batchBytes);
+    writeSeparatorInArray(batchBytes);
+    writeIntInByteArray(diagnosisKey.getRollingStartIntervalNumber(), batchBytes);
+    writeSeparatorInArray(batchBytes);
+    writeIntInByteArray(diagnosisKey.getRollingPeriod(), batchBytes);
+    writeSeparatorInArray(batchBytes);
+    writeIntInByteArray(diagnosisKey.getTransmissionRiskLevel(), batchBytes);
+    writeSeparatorInArray(batchBytes);
+    writeVisitedCountriesInByteArray(diagnosisKey.getVisitedCountriesList(),
+        batchBytes);
+    writeSeparatorInArray(batchBytes);
+    writeB64StringInByteArray(diagnosisKey.getOrigin(), batchBytes);
+    writeSeparatorInArray(batchBytes);
+    writeIntInByteArray(diagnosisKey.getReportType().getNumber(), batchBytes);
+    writeSeparatorInArray(batchBytes);
+    writeIntInByteArray(diagnosisKey.getDaysSinceOnsetOfSymptoms(), batchBytes);
+    writeSeparatorInArray(batchBytes);
     return batchBytes.toByteArray();
   }
 
-  private static List<app.coronawarn.server.common.protocols.external.exposurenotification.DiagnosisKey>
-        sortBatchByKeyData(DiagnosisKeyBatch batch) {
+  private void writeBytesInByteArray(final ByteString bytes, ByteArrayOutputStream byteArray) {
+    String base64String = bytesToBase64String(bytes.toByteArray());
+    if (base64String != null) {
+      writeStringInByteArray(base64String, byteArray);
+    }
+  }
+
+  private void writeStringInByteArray(final String batchString, final ByteArrayOutputStream byteArray) {
+    byteArray.writeBytes(batchString.getBytes(StandardCharsets.US_ASCII));
+  }
+
+  private void writeB64StringInByteArray(final String batchString, final ByteArrayOutputStream byteArray) {
+    String base64String = bytesToBase64String(batchString.getBytes(StandardCharsets.US_ASCII));
+    if (base64String != null) {
+      writeStringInByteArray(base64String, byteArray);
+    }
+  }
+
+  private void writeIntInByteArray(final int batchInt, final ByteArrayOutputStream byteArray) {
+    String base64String = bytesToBase64String(ByteBuffer.allocate(4).putInt(batchInt).array());
+    if (base64String != null) {
+      writeStringInByteArray(base64String, byteArray);
+    }
+  }
+
+  private void writeSeparatorInArray(final ByteArrayOutputStream byteArray) {
+    byteArray.writeBytes(".".getBytes(StandardCharsets.US_ASCII));
+  }
+
+  private void writeVisitedCountriesInByteArray(final ProtocolStringList countries,
+      final ByteArrayOutputStream byteArray) {
+    writeB64StringInByteArray(String.join(",", countries), byteArray);
+  }
+
+
+  private static List<DiagnosisKey> sortBatchByKeyData(DiagnosisKeyBatch batch) {
     return batch.getKeysList()
         .stream()
         .sorted(Comparator.comparing(diagnosisKey -> diagnosisKey.getKeyData().toStringUtf8()))
@@ -147,7 +183,6 @@ public class BatchSigner {
   private X509Certificate getCertificateFromPublicKey() {
     return this.cryptoProvider.getCertificate();
   }
-
 
 
 }
