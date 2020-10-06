bb08cc2bf0e0ea3ec1f202891f7f1025904f4c7e
Michael <66735191+mibrasap@users.noreply.github.com>
2020-10-06 15:52:15 +0200

    Refactoring (#860)

--- services/download/src/main/java/app/coronawarn/server/services/download/FederationBatchProcessor.java
+++ services/download/src/main/java/app/coronawarn/server/services/download/FederationBatchProcessor.java
@@ -160,7 +160,7 @@ public class FederationBatchProcessor {
           .withFieldNormalization(new FederationKeyNormalizer(config))
           .build());
     } catch (Exception ex) {
-      logger.info("Building key failed.");
+      logger.info("Building diagnosis key from federation diagnosis key failed.", ex);
       return Optional.empty();
     }
   }
