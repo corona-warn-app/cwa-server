ae3f60397af402e1b4d7e12ff9dbb8c68e2ebbc1
Eugen M <eugen.madean@sap.com>
2020-10-07 15:10:48 +0300

    sonar

--- common/persistence/src/main/java/app/coronawarn/server/common/persistence/domain/config/TekFieldDerivations.java
+++ common/persistence/src/main/java/app/coronawarn/server/common/persistence/domain/config/TekFieldDerivations.java
@@ -2,8 +2,6 @@ package app.coronawarn.server.common.persistence.domain.config;
 
 import java.util.HashMap;
 import java.util.Map;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
