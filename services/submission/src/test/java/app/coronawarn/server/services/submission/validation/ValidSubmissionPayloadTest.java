0be4931e8c10a52c049643dbe91b9f424789aa6e
Eugen M <eugen.madean@sap.com>
2020-10-30 15:26:59 +0200

    Add assertions about the actual violation message

--- services/submission/src/test/java/app/coronawarn/server/services/submission/validation/ValidSubmissionPayloadTest.java
+++ services/submission/src/test/java/app/coronawarn/server/services/submission/validation/ValidSubmissionPayloadTest.java
@@ -1,14 +1,23 @@
 package app.coronawarn.server.services.submission.validation;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import app.coronawarn.server.common.protocols.internal.SubmissionPayload;
+import app.coronawarn.server.services.submission.controller.ApiExceptionHandler;
 import app.coronawarn.server.services.submission.controller.RequestExecutor;
 import app.coronawarn.server.services.submission.controller.SubmissionPayloadMockData;
 
@@ -21,12 +30,20 @@ public class ValidSubmissionPayloadTest {
 
   private static String interpolationSideEffect;
 
+  @MockBean
+  private ApiExceptionHandler apiExceptionHandler;
+
+  @Captor
+  private ArgumentCaptor<Exception> apiExceptionCaptor;
+
+
   @Autowired
   private RequestExecutor requestExecutor;
 
   @BeforeEach
   public void setup() {
     interpolationSideEffect = "";
+    Mockito.doNothing().when(apiExceptionHandler).diagnosisKeyExceptions(apiExceptionCaptor.capture(), Mockito.any());
   }
 
   /**
@@ -41,7 +58,12 @@ public class ValidSubmissionPayloadTest {
             + "\'\'.getClass().forName(\'app.coronawarn.server.services.submission.validation.ValidSubmissionPayloadTest\')"
             + ".newInstance().showInterpolationEffect()}");
 
-    requestExecutor.executePost(payload);
+    ResponseEntity<Void> response = requestExecutor.executePost(payload);
+    assertThat(response.getStatusCode()).isEqualTo(BAD_REQUEST);
+    assertEquals(apiExceptionCaptor.getValue().getMessage(),
+        "submitDiagnosisKey.exposureKeys: Origin country java.lang.Runtime.getRuntime().exec('%s');//${''.getClass()."
+            + "forName('app.coronawarn.server.services.submission.validation.ValidSubmissionPayloadTest').newInstance().showInterpolationEffect()}"
+            + " is not part of the supported countries list");
     assertNotEquals("INTERPOLATION_OCCURRED", interpolationSideEffect);
   }
 
@@ -52,7 +74,12 @@ public class ValidSubmissionPayloadTest {
             + "\'\'.getClass().forName(\'app.coronawarn.server.services.submission.validation.ValidSubmissionPayloadTest\')"
             + ".newInstance().showInterpolationEffect()}"));
 
-    requestExecutor.executePost(payload);
+    ResponseEntity<Void> response = requestExecutor.executePost(payload);
+    assertThat(response.getStatusCode()).isEqualTo(BAD_REQUEST);
+    assertEquals(apiExceptionCaptor.getValue().getMessage(),
+        "submitDiagnosisKey.exposureKeys: [java.lang.Runtime.getRuntime().exec('%s');//${''.getClass()."
+            + "forName('app.coronawarn.server.services.submission.validation.ValidSubmissionPayloadTest').newInstance().showInterpolationEffect()}]:"
+            + " Visited country is not part of the supported countries list");
     assertNotEquals("INTERPOLATION_OCCURRED", interpolationSideEffect);
   }
 
