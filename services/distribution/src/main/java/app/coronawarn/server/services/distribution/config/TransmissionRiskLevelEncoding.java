b5ae8e076f09e3128d5b6bbb99e6023333871b02
Eugen M <eugen.madean@sap.com>
2020-11-04 11:36:47 +0200

    TranmissionRiskEncoding construction time unit test

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/config/TransmissionRiskLevelEncoding.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/config/TransmissionRiskLevelEncoding.java
@@ -2,25 +2,29 @@ package app.coronawarn.server.services.distribution.config;
 
 import app.coronawarn.server.common.persistence.domain.DiagnosisKey;
 import app.coronawarn.server.common.persistence.domain.config.YamlPropertySourceFactory;
+import app.coronawarn.server.common.protocols.external.exposurenotification.ReportType;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import javax.validation.constraints.NotNull;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 /**
  * Wrapper over properties defined in <code>master-config/tranmission-risk-encoding.yaml</code>. It
  * provides convenience methods to derive properties from one another. Please see yaml file for more
- * details on the reasoning / intent of encoding this field.
- * This class also validates its own internal state at construction time as per Spring configuration
- * class validation mechanisms.
+ * details on the reasoning / intent of encoding this field. This class also validates its own
+ * internal state at construction time as per Spring configuration class validation mechanisms.
  */
 @Configuration
 @ConfigurationProperties(prefix = "transmission-risk-encoding")
@@ -35,20 +39,12 @@ public class TransmissionRiskLevelEncoding implements Validator {
 
   /* 'transmissionRiskLevel' to 'reportType' */
   @NotNull
-  private Map<Integer, Integer> trlToRt;
+  private Map<Integer, Integer> trlToReportType;
 
 
   TransmissionRiskLevelEncoding() {
   }
 
-
-  public Map<Integer, Integer> getTransmissionRiskLevelToDaysSinceSymptomsMapping() {
-    return new HashMap<>(trlToDsos);
-  }
-
-  public Map<Integer, Integer> getTransmissionRiskLevelToReportTypeMapping() {
-    return new HashMap<>(trlToRt);
-  }
 
   /**
    * Returns a mapped ENF v2 Days Since Symptoms value for the given Transmission Risk Level or
@@ -62,8 +58,8 @@ public class TransmissionRiskLevelEncoding implements Validator {
    * Returns a mapped ENF v2 Report Type value for the given Transmission Risk Level or throws an
    * exception if the TRL is not part of the mapping.
    */
-  public Integer getReportTypeForTransmissionRiskLevel(Integer transmissionRiskLevel) {
-    return getMappedValue(transmissionRiskLevel, trlToRt, "reportType");
+  public ReportType getReportTypeForTransmissionRiskLevel(Integer transmissionRiskLevel) {
+    return ReportType.forNumber(getMappedValue(transmissionRiskLevel, trlToReportType, "reportType"));
   }
 
   private Integer getMappedValue(Integer transmissionRiskLevel, Map<Integer, Integer> encodingMap,
@@ -85,50 +81,73 @@ public class TransmissionRiskLevelEncoding implements Validator {
   public void validate(Object target, Errors errors) {
     ValidationUtils.rejectIfEmpty(errors, "trlToDsos", "trlToDsos.empty",
         "TRL to DSOS encoding map is null or empty");
-    ValidationUtils.rejectIfEmpty(errors, "trlToRt", "trlToRt.empty",
+    ValidationUtils.rejectIfEmpty(errors, "trlToReportType", "trlToReportType.empty",
         "TRL to RT encoding map is null or empty");
 
     TransmissionRiskLevelEncoding encodingMappings = (TransmissionRiskLevelEncoding) target;
 
-    if (trlKeysNotInRange(encodingMappings.getTransmissionRiskLevelToDaysSinceSymptomsMapping())) {
+    if (trlKeysNotInRange(encodingMappings.getTrlToDsos())) {
       errors.rejectValue("trlToDsos",
-          "transmissionRiskToDaysSinceOnsetSymptoms map contains invalid TRL");
+          "transmissionRisk to daysSinceOnsetSymptoms map contains invalid TRL");
     }
-    if (trlKeysNotInRange(encodingMappings.getTransmissionRiskLevelToReportTypeMapping())) {
-      errors.rejectValue("trlToDsos",
-          "transmissionRiskToReportType map contains invalid TRL");
+    if (trlKeysNotInRange(encodingMappings.getTrlToReportType())) {
+      errors.rejectValue("trlToReportType", "transmissionRisk to reportType map contains invalid TRL");
     }
   }
 
   private boolean trlKeysNotInRange(
       Map<Integer, Integer> transmissionRiskLevelToDaysSinceSymptomsMapping) {
-    List<Integer> trlRange = IntStream.rangeClosed(DiagnosisKey.MIN_TRANSMISSION_RISK_LEVEL,
-        DiagnosisKey.MAX_TRANSMISSION_RISK_LEVEL).boxed().collect(Collectors.toList());
+    Set<Integer> trlKeys = transmissionRiskLevelToDaysSinceSymptomsMapping.keySet();
 
-    return !transmissionRiskLevelToDaysSinceSymptomsMapping.keySet().stream()
-        .allMatch(trlRange::contains);
+    return !IntStream.rangeClosed(DiagnosisKey.MIN_TRANSMISSION_RISK_LEVEL,
+        DiagnosisKey.MAX_TRANSMISSION_RISK_LEVEL).allMatch(trlKeys::contains);
+  }
+
+  private void internalValidate() {
+    BeanPropertyBindingResult errorsContainer =
+        new BeanPropertyBindingResult(this, "transmissionRiskEncoding");
+    this.validate(this, errorsContainer);
+    if (errorsContainer.hasErrors()) {
+      throw new IllegalStateException(
+          "Errors while loading transmission risk level encoding configuration. Reason: "
+              + StringUtils.join(errorsContainer.getAllErrors().stream().map(ObjectError::toString)
+                  .collect(Collectors.toList()), ","));
+    }
   }
 
   /**
    * Constructs the configuration class from the given mappings.
    */
-  public static TransmissionRiskLevelEncoding from(Map<Integer, Integer> transmissionRiskLevelToDaysSinceSymptoms,
+  public static TransmissionRiskLevelEncoding from(
+      Map<Integer, Integer> transmissionRiskLevelToDaysSinceSymptoms,
       Map<Integer, Integer> transmissionRiskLevelToReportType) {
     TransmissionRiskLevelEncoding transmissionRiskEncoding = new TransmissionRiskLevelEncoding();
     transmissionRiskEncoding.setTrlToDsos(transmissionRiskLevelToDaysSinceSymptoms);
-    transmissionRiskEncoding.setTrlToRt(transmissionRiskLevelToReportType);
+    transmissionRiskEncoding.setTrlToReportType(transmissionRiskLevelToReportType);
+    transmissionRiskEncoding.internalValidate();
     return transmissionRiskEncoding;
   }
 
 
-  /* Setters below are used by Spring to inject the maps loaded from the yaml.
-   * For now this should be used because @ConstructorBinding does not work in the current setup. */
+  /*
+   * Setters below are package protected and used by Spring to inject the maps loaded from the yaml.
+   * For now this should be used because @ConstructorBinding does not work in the current setup.
+   * Getters are needed for spring validation, but they return copies of the internal state.
+   */
 
   void setTrlToDsos(Map<Integer, Integer> trlToDsos) {
     this.trlToDsos = trlToDsos;
   }
 
-  void setTrlToRt(Map<Integer, Integer> trlToRt) {
-    this.trlToRt = trlToRt;
+  void setTrlToReportType(Map<Integer, Integer> trlToRt) {
+    this.trlToReportType = trlToRt;
+  }
+
+  public Map<Integer, Integer> getTrlToDsos() {
+    return new HashMap<Integer, Integer>(trlToDsos);
+  }
+
+  public Map<Integer, Integer> getTrlToReportType() {
+    return new HashMap<Integer, Integer>(trlToReportType);
   }
 }
--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/config/TransmissionRiskLevelEncodingTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/config/TransmissionRiskLevelEncodingTest.java
@@ -5,40 +5,56 @@ import static org.mockito.Mockito.*;
 
 import java.util.Map;
 import org.junit.jupiter.api.Test;
-import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
+import app.coronawarn.server.common.protocols.external.exposurenotification.ReportType;
 
 class TransmissionRiskLevelEncodingTest {
 
   @Test
   void shouldDeriveDsosBasedOnMappingWhenProvided() {
-    TransmissionRiskLevelEncoding encodings = TransmissionRiskLevelEncoding.from(Map.of(6, 2), Map.of());
-    assertEquals(2, encodings.getDaysSinceSymptomsForTransmissionRiskLevel(6));
+    TransmissionRiskLevelEncoding encodings = TransmissionRiskLevelEncoding.from(
+        Map.of(1,1,2,2,3,2,4,2,5,2,6,1,7,2,8,1),
+        Map.of(1,1,2,2,3,3,4,4,5,1,6,2,7,3,8,4));
+    assertEquals(1, encodings.getDaysSinceSymptomsForTransmissionRiskLevel(6));
 
     assertThrows(IllegalArgumentException.class, () -> {
-      encodings.getDaysSinceSymptomsForTransmissionRiskLevel(1);
+      encodings.getDaysSinceSymptomsForTransmissionRiskLevel(9);
     });
   }
 
   @Test
   void shouldDeriveReportTypeBasedOnMappingWhenProvided() {
-    TransmissionRiskLevelEncoding encodings = TransmissionRiskLevelEncoding.from(Map.of(), Map.of(5, 3));
-    assertEquals(3, encodings.getReportTypeForTransmissionRiskLevel(5));
+    TransmissionRiskLevelEncoding encodings = TransmissionRiskLevelEncoding.from(
+        Map.of(1,1,2,2,3,2,4,2,5,2,6,1,7,2,8,1),
+        Map.of(1,1,2,2,3,3,4,4,5,1,6,2,7,3,8,4));
+    assertEquals(ReportType.CONFIRMED_TEST, encodings.getReportTypeForTransmissionRiskLevel(5));
 
     assertThrows(IllegalArgumentException.class, () -> {
-      encodings.getReportTypeForTransmissionRiskLevel(1);
+      encodings.getReportTypeForTransmissionRiskLevel(9);
     });
   }
 
   @Test
   void shouldValidateTranmissionRiskLevelKeys() {
-    TransmissionRiskLevelEncoding encodings = TransmissionRiskLevelEncoding.from(Map.of(1,1,2,3), Map.of(5, 3));
+    /* Test both construction and individual method that is invoked by Spring validation */
+    TransmissionRiskLevelEncoding validEncodings = TransmissionRiskLevelEncoding.from(
+        Map.of(1,1,2,2,3,2,4,2,5,2,6,1,7,2,8,1),
+        Map.of(1,1,2,2,3,3,4,4,5,1,6,2,7,3,8,4));
     BindingResult errorsMock = spy(BindingResult.class);
-    encodings.validate(encodings, errorsMock);
+    validEncodings.validate(validEncodings, errorsMock);
     verify(errorsMock, times(0)).rejectValue(any(), any());
 
-    encodings = TransmissionRiskLevelEncoding.from(Map.of(12,1,0,3), Map.of(13, 3));
-    encodings.validate(encodings, errorsMock);
+    /* Test construction time validation */
+    assertThrows(IllegalStateException.class, () -> {
+      TransmissionRiskLevelEncoding.from(Map.of(12,1,0,3), Map.of(13, 3));
+    });
+
+    /* Test method that is invoked by Spring validation */
+    TransmissionRiskLevelEncoding invalidEncoding = new TransmissionRiskLevelEncoding();
+    invalidEncoding.setTrlToDsos(Map.of(12,1,0,3));
+    invalidEncoding.setTrlToReportType(Map.of(13, 3));
+
+    validEncodings.validate(invalidEncoding, errorsMock);
     verify(errorsMock, times(2)).rejectValue(any(), any());
   }
 }
