183348384ccc59f24989ebf21db19dcec3b69aae
Jeremias Rößler <jeremias.roessler@sap.com>
2020-11-13 12:11:19 +0100

    Working SecurityConfig

--- services/callback/src/main/java/app/coronawarn/server/services/callback/config/SecurityConfig.java
+++ services/callback/src/main/java/app/coronawarn/server/services/callback/config/SecurityConfig.java
@@ -2,6 +2,8 @@
 
 package app.coronawarn.server.services.callback.config;
 
+import static java.util.Collections.emptyList;
+
 import app.coronawarn.server.services.callback.controller.CallbackController;
 import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +15,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
 
@@ -35,13 +40,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-
     ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry expressionInterceptUrlRegistry
         = http.authorizeRequests();
     expressionInterceptUrlRegistry
-        .mvcMatchers(HttpMethod.GET, CALLBACK_ROUTE).authenticated().and().x509();
+        .mvcMatchers(HttpMethod.GET, CALLBACK_ROUTE).authenticated().and().x509()
+        .userDetailsService(userDetailsService());
     expressionInterceptUrlRegistry
         .anyRequest().denyAll();
     http.headers().contentSecurityPolicy("default-src 'self'");
+  }
+
+  /**
+   * We do not use UserDetails, so return stub.
+   *
+   * @return UserDetailsService stub
+   */
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return new UserDetailsService() {
+      @Override
+      public UserDetails loadUserByUsername(String username) {
+        return new User(username, "", emptyList());
+      }
+    };
   }
 }
