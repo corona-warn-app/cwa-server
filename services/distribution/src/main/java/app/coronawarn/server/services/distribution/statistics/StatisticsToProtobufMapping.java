f3323ecf812833c9a2b04dbbd2291c2182611a6e
ioangut <ioan.gut@sap.com>
2020-11-17 17:37:04 +0200

    Delete space in json file

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/statistics/StatisticsToProtobufMapping.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/statistics/StatisticsToProtobufMapping.java
@@ -1,6 +1,6 @@
 package app.coronawarn.server.services.distribution.statistics;
 
-import app.coronawarn.server.common.protocols.internal.stats.KeyFigure;
+import app.coronawarn.server.common.protocols.internal.stats.CardHeader;
 import app.coronawarn.server.common.protocols.internal.stats.KeyFigureCard;
 import app.coronawarn.server.common.protocols.internal.stats.Statistics;
 import app.coronawarn.server.services.distribution.config.DistributionServiceConfig;
@@ -8,6 +8,8 @@ import app.coronawarn.server.services.distribution.utils.SerializationUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.io.FileUtils;
@@ -29,28 +31,40 @@ public class StatisticsToProtobufMapping {
   public Statistics constructProtobufStatistics() throws IOException {
     String content = FileUtils
         .readFileToString(new File("./src/test/resources/stats/statistic_data.json"), StandardCharsets.UTF_8);
-    List<StatisticsJsonStringObject> jsonStringObject = SerializationUtils.deserializeJson(content, typeFactory -> typeFactory
-        .constructCollectionType(List.class,StatisticsJsonStringObject.class));
+    List<StatisticsJsonStringObject> jsonStringObjects = SerializationUtils
+        .deserializeJson(content, typeFactory -> typeFactory
+            .constructCollectionType(List.class, StatisticsJsonStringObject.class));
 
     return Statistics.newBuilder()
-        .addAllCardIdSequence(buildAllCardIdSequence())
-        .addAllKeyFigureCards(buildAllKeyFigureCards(jsonStringObject))
+        .addAllCardIdSequence(getAllCardIdSequence())
+        //.addAllKeyFigureCards(buildAllKeyFigureCards(jsonStringObject))
         .build();
   }
 
-  private List<Integer> buildAllCardIdSequence() {
+  private List<Integer> getAllCardIdSequence() {
     List<Integer> idSequence = new ArrayList<>();
     String[] idSequenceArray = distributionServiceConfig.getCardIdSequence().
-        replace("[","").replace("]","")
-        .split( ",");
+        replace("[", "").replace("]", "")
+        .split(",");
     List<Integer> idIntegerSequence = new ArrayList<>();
-    for(String id: idSequenceArray){
+    for (String id : idSequenceArray) {
       idIntegerSequence.add(Integer.parseInt(id));
     }
     return idIntegerSequence;
   }
 
-  private List<KeyFigureCard> buildAllKeyFigureCards(List<StatisticsJsonStringObject> jsonStringObject) {
+  private List<KeyFigureCard> buildAllKeyFigureCards(List<StatisticsJsonStringObject> jsonStringObjects) {
+    List<KeyFigureCard> figureCards = new ArrayList<>();
+    jsonStringObjects.forEach(jsonObject -> {
+      getAllCardIdSequence().forEach(id -> {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime dateTime = LocalDateTime.parse(jsonObject.getUpdateTimestamp(), formatter);
+        CardHeader cardHeader = CardHeader.newBuilder()
+            .setCardId(id)
+            .build();
+      });
+    });
+
     return null;
   }
 }
