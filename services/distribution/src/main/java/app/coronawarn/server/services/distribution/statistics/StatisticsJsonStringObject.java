59a54e997713055257628647bb84301bd93a35c2
emmetsap <emmet.power@sap.com>
2020-11-17 14:18:49 +0000

    updated statistic json

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/statistics/StatisticsJsonStringObject.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/statistics/StatisticsJsonStringObject.java
@@ -4,70 +4,154 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class StatisticsJsonStringObject {
 
-  private String date;
-  @JsonProperty("app_downloads_cum")
-  private Integer appDownloadsCum;
-  @JsonProperty("key_uploads")
-  private Integer keyUploads;
-  @JsonProperty("test_total")
-  private Integer testTotal;
-  @JsonProperty("total_redeemed")
-  private Integer totalRedeemed;
-  @JsonProperty("total_not_redeemed")
-  private Integer totalNotRedeemed;
-  @JsonProperty("labs_total")
-  private Integer labsTotal;
-  @JsonProperty("labs_done")
-  private Integer labsDone;
-  private Integer infections;
-  @JsonProperty("day_of_week")
-  private String dayOfWeek;
-  @JsonProperty("week_of_year")
-  private Integer weekOfYear;
+  @JsonProperty("update_timestamp")
+  private String updateTimestamp;
+  @JsonProperty("effective_date")
+  private String effectiveDate;
+  @JsonProperty("app_downloads_daily")
+  private Integer appDownloadsDaily;
+  @JsonProperty("app_downloads_cumulated")
+  private Integer appDownloadsCumulated;
+  @JsonProperty("app_downloads_7days_sum")
+  private Integer appDownloads7daysSum;
+  @JsonProperty("app_downloads_7days_growthrate")
+  private Integer appDownloads7daysGrowthrate;
+  @JsonProperty("app_downloads_7days_trend_5percent")
+  private Integer appDownloads7daysTrend5percent;
+  @JsonProperty("persons_who_shared_keys_daily")
+  private Integer personsWhoSharedKeysDaily;
+  @JsonProperty("persons_who_shared_keys_cumulated")
+  private Integer personsWhoSharedKeysCumulated;
+  @JsonProperty("persons_who_shared_keys_7days_sum")
+  private Integer personsWhoSharedKeys7daysSum;
+  @JsonProperty("persons_who_shared_keys_7days_growthrate")
+  private Integer personsWhoSharedKeys7daysGrowthrate;
+  @JsonProperty("persons_who_shared_keys_7days_trend_5percent")
+  private Integer personsWhoSharedKeys7daysTrend5percent;
+  @JsonProperty("tests_total_daily")
+  private Integer testsTotalDaily;
+  @JsonProperty("positive_tests_used_to_share_keys_daily")
+  private Integer positiveTestsUsedToShareKeysDaily;
+  @JsonProperty("positive_tests_not_used_to_share_keys_daily")
+  private Integer positiveTestsNotUsedToShareKeysDaily;
+  @JsonProperty("labs_registered_absolut")
+  private Integer labsRegisteredAbsolut;
+  @JsonProperty("labs_connected_absolut")
+  private Integer labsConnectedAbsolut;
+  @JsonProperty("seven_day_incidence")
+  private Integer sevenDayIncidence;
+  @JsonProperty("seven_day_incidence_growthrate")
+  private Integer sevenDayIncidenceGrowthrate;
+  @JsonProperty("seven_day_incidence_trend_5percent")
+  private Integer sevenDayIncidenceTrend5percent;
+  @JsonProperty("infections_reported_daily")
+  private Integer infectionsReportedDaily;
+  @JsonProperty("infections_reported_7days_avg")
+  private Integer infectionsReported7daysAvg;
+  @JsonProperty("infections_reported_7days_growthrate")
+  private Integer infectionsReported7daysGrowthrate;
+  @JsonProperty("infections_reported_7days_trend_5percent")
+  private Integer infectionsReported7daysTrend5percent;
+  @JsonProperty("infections_reported_cumulated")
+  private Integer infectionsReportedCumulated;
 
-
-  public String getDate() {
-    return date;
+  public String getUpdateTimestamp() {
+    return updateTimestamp;
   }
 
-  public Integer getAppDownloadsCum() {
-    return appDownloadsCum;
+  public String getEffectiveDate() {
+    return effectiveDate;
   }
 
-  public Integer getKeyUploads() {
-    return keyUploads;
+  public Integer getAppDownloadsDaily() {
+    return appDownloadsDaily;
   }
 
-  public Integer getTestTotal() {
-    return testTotal;
+  public Integer getAppDownloadsCumulated() {
+    return appDownloadsCumulated;
   }
 
-  public Integer getTotalRedeemed() {
-    return totalRedeemed;
+  public Integer getAppDownloads7daysSum() {
+    return appDownloads7daysSum;
   }
 
-  public Integer getTotalNotRedeemed() {
-    return totalNotRedeemed;
+  public Integer getAppDownloads7daysGrowthrate() {
+    return appDownloads7daysGrowthrate;
   }
 
-  public Integer getLabsTotal() {
-    return labsTotal;
+  public Integer getAppDownloads7daysTrend5percent() {
+    return appDownloads7daysTrend5percent;
   }
 
-  public Integer getLabsDone() {
-    return labsDone;
+  public Integer getPersonsWhoSharedKeysDaily() {
+    return personsWhoSharedKeysDaily;
   }
 
-  public Integer getInfections() {
-    return infections;
+  public Integer getPersonsWhoSharedKeysCumulated() {
+    return personsWhoSharedKeysCumulated;
   }
 
-  public String getDayOfWeek() {
-    return dayOfWeek;
+  public Integer getPersonsWhoSharedKeys7daysSum() {
+    return personsWhoSharedKeys7daysSum;
   }
 
-  public Integer getWeekOfYear() {
-    return weekOfYear;
+  public Integer getPersonsWhoSharedKeys7daysGrowthrate() {
+    return personsWhoSharedKeys7daysGrowthrate;
   }
 
+  public Integer getPersonsWhoSharedKeys7daysTrend5percent() {
+    return personsWhoSharedKeys7daysTrend5percent;
+  }
+
+  public Integer getTestsTotalDaily() {
+    return testsTotalDaily;
+  }
+
+  public Integer getPositiveTestsUsedToShareKeysDaily() {
+    return positiveTestsUsedToShareKeysDaily;
+  }
+
+  public Integer getPositiveTestsNotUsedToShareKeysDaily() {
+    return positiveTestsNotUsedToShareKeysDaily;
+  }
+
+  public Integer getLabsRegisteredAbsolut() {
+    return labsRegisteredAbsolut;
+  }
+
+  public Integer getLabsConnectedAbsolut() {
+    return labsConnectedAbsolut;
+  }
+
+  public Integer getSevenDayIncidence() {
+    return sevenDayIncidence;
+  }
+
+  public Integer getSevenDayIncidenceGrowthrate() {
+    return sevenDayIncidenceGrowthrate;
+  }
+
+  public Integer getSevenDayIncidenceTrend5percent() {
+    return sevenDayIncidenceTrend5percent;
+  }
+
+  public Integer getInfectionsReportedDaily() {
+    return infectionsReportedDaily;
+  }
+
+  public Integer getInfectionsReported7daysAvg() {
+    return infectionsReported7daysAvg;
+  }
+
+  public Integer getInfectionsReported7daysGrowthrate() {
+    return infectionsReported7daysGrowthrate;
+  }
+
+  public Integer getInfectionsReported7daysTrend5percent() {
+    return infectionsReported7daysTrend5percent;
+  }
+
+  public Integer getInfectionsReportedCumulated() {
+    return infectionsReportedCumulated;
+  }
 }
