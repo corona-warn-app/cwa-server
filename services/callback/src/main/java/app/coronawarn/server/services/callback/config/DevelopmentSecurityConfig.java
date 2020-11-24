1a391f2e020ee7440a0615344fd7448930d0372b
Jeremias Rößler <jeremias.roessler@sap.com>
2020-11-24 08:36:23 +0100

    Feature/callback certificates (#992)
    * First draft of working state ... needs some more review
    
    * Add .yaml for new spring profile, adjust test to use new spring profile, extract variable in SecurityConfig.configure(), optimize imports
    
    * Working SecurityConfig
    
    * Add disable-certificate-authentication to docker-compose
    
    * Add the "@Override" annotation and make anonymous inner class a lambda, as requested by sonarcloud
    
    * Add random batch and check whether it was inserted into the database
    
    * Add test that fails without client certificates—to validate that those are needed
    
    * Fix wrong argument in assertion
    
    Co-authored-by: Philipp Kilb <philipp.kilb@sap.com>
    Co-authored-by: Hilmar Falkenberg <hilmar.falkenberg@sap.com>

--- services/callback/src/main/java/app/coronawarn/server/services/callback/config/DevelopmentSecurityConfig.java
+++ services/callback/src/main/java/app/coronawarn/server/services/callback/config/DevelopmentSecurityConfig.java
@@ -2,30 +2,22 @@
 
 package app.coronawarn.server.services.callback.config;
 
-import static java.util.Collections.emptyList;
-
 import app.coronawarn.server.services.callback.controller.CallbackController;
 import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@Profile("!disable-certificate-authentication")
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Profile("disable-certificate-authentication")
+public class DevelopmentSecurityConfig extends WebSecurityConfigurerAdapter {
 
   private static final String CALLBACK_ROUTE =
       "/version/v1" + CallbackController.CALLBACK_ROUTE;
@@ -40,24 +32,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry expressionInterceptUrlRegistry
-        = http.authorizeRequests();
-    expressionInterceptUrlRegistry
-        .mvcMatchers(HttpMethod.GET, CALLBACK_ROUTE).authenticated().and().x509()
-        .userDetailsService(userDetailsService());
-    expressionInterceptUrlRegistry
+    http.authorizeRequests()
+        .mvcMatchers(HttpMethod.GET, CALLBACK_ROUTE).permitAll()
         .anyRequest().denyAll();
     http.headers().contentSecurityPolicy("default-src 'self'");
-  }
-
-  /**
-   * We do not use UserDetails, so return stub.
-   *
-   * @return UserDetailsService stub
-   */
-  @Bean
-  @Override
-  public UserDetailsService userDetailsService() {
-    return username -> new User(username, "", emptyList());
   }
 }
