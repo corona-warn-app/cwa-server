380a1fc92df8bdabee27ca5939d6dfa70ec95d98
ioangut <ioan.gut@sap.com>
2020-11-26 15:41:03 +0200

    Update DiagnosisKeyUploadIT.java

--- services/upload/src/test/java/app/coronawarn/server/services/federation/upload/integration/DiagnosisKeyUploadIT.java
+++ services/upload/src/test/java/app/coronawarn/server/services/federation/upload/integration/DiagnosisKeyUploadIT.java
@@ -21,7 +21,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-
+@ActiveProfiles("disable-ssl-client-federation")
 class DiagnosisKeyUploadIT extends UploadKeyIT {
 
   @Autowired
