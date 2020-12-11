a90ad617e009967b137f04b705ce395268a29fe0
Eugen M <eugen.madean@sap.com>
2020-12-11 12:15:21 +0200

    Create the new key table for SGS uploads

---
+++ common/persistence/src/main/resources/db/migration/V7__createTablesForSGS.sql
@@ -0,0 +1,13 @@
+CREATE TABLE sgs_upload_key (
+    key_data bytea PRIMARY KEY,
+    rolling_period integer NOT NULL,
+    rolling_start_interval_number integer NOT NULL,
+    submission_timestamp bigint NOT NULL,
+    transmission_risk_level integer NOT NULL,
+    consent_to_sgs_upload boolean NOT NULL DEFAULT FALSE,
+    origin_country varchar (2),
+    visited_countries varchar (2) [],
+    report_type varchar(30),
+    days_since_onset_of_symptoms INTEGER,
+    batch_tag VARCHAR(50)
+);
