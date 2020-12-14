960d64d5af0169bb1326bbf5fcb641fddbbf9ae1
Sorin Stefan Iovita <sorin.stefan.iovita@sap.com>
2020-12-14 15:08:54 +0200

    remove unused code

--- services/download/src/main/java/app/coronawarn/server/services/download/BatchAuditException.java
+++ services/download/src/main/java/app/coronawarn/server/services/download/BatchAuditException.java
@@ -2,10 +2,6 @@ package app.coronawarn.server.services.download;
 
 public class BatchAuditException extends RuntimeException {
 
-  public BatchAuditException(String msg) {
-    super(msg);
-  }
-
   public BatchAuditException(String msg, Throwable cause) {
     super(msg, cause);
   }
