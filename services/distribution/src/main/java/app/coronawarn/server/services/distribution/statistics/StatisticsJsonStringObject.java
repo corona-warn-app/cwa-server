72bb406f64e0e09d75c9813bbbf257de1cc9c5f9
Frederico Bittencourt <fred.rbittencourt@gmail.com>
2021-01-12 11:57:15 +0100

    add tag to ignore missing fields

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/statistics/StatisticsJsonStringObject.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/statistics/StatisticsJsonStringObject.java
@@ -1,10 +1,12 @@
 package app.coronawarn.server.services.distribution.statistics;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class StatisticsJsonStringObject {
 
   @JsonProperty("update_timestamp")
