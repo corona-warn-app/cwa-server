b79562c86a651dd35f1147f584ff4232930781c4
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2021-01-13 09:16:29 +0100

    unused import

--- services/callback/src/main/java/app/coronawarn/server/services/callback/config/SecurityConfig.java
+++ services/callback/src/main/java/app/coronawarn/server/services/callback/config/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
