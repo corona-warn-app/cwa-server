3f0164dbd9b6145d51bc87dbe4ad539f8b6987d6
Jeremias Rößler <jeremias.roessler@sap.com>
2021-01-18 19:35:04 +0100

    Fix failing test

--- services/submission/src/test/java/app/coronawarn/server/services/submission/ServerApplicationTests.java
+++ services/submission/src/test/java/app/coronawarn/server/services/submission/ServerApplicationTests.java
@@ -1,9 +1,9 @@
 
-
 package app.coronawarn.server.services.submission;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import app.coronawarn.server.common.federation.client.NoopHostnameVerifierProvider;
 import app.coronawarn.server.services.submission.controller.SubmissionController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +12,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest
+@SpringBootTest(classes = { NoopHostnameVerifierProvider.class })
 @DirtiesContext
 class ServerApplicationTests {
 
