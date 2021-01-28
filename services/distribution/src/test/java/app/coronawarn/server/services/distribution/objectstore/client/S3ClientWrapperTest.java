d9ffa5cda9a8d6904af6052e6748838046d24a40
Frederico Bittencourt <fred.rbittencourt@gmail.com>
2021-01-28 09:22:30 +0100

    add missing tests to S3ClientWrapper

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/objectstore/client/S3ClientWrapperTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/objectstore/client/S3ClientWrapperTest.java
@@ -17,10 +17,12 @@ import static org.mockito.Mockito.when;
 
 import app.coronawarn.server.services.distribution.config.DistributionServiceConfig;
 import app.coronawarn.server.services.distribution.objectstore.client.ObjectStoreClient.HeaderKey;
+import java.io.ByteArrayInputStream;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import app.coronawarn.server.services.distribution.statistics.exceptions.NotModifiedException;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -41,14 +43,17 @@ import org.springframework.retry.ExhaustedRetryException;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.core.exception.SdkException;
 import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.http.AbortableInputStream;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.Delete;
 import software.amazon.awssdk.services.s3.model.DeleteObjectsRequest;
 import software.amazon.awssdk.services.s3.model.DeleteObjectsResponse;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
 import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
@@ -296,5 +301,72 @@ class S3ClientWrapperTest {
         .isThrownBy(() -> s3ClientWrapper.removeObjects(VALID_BUCKET_NAME, targetObjects));
 
     verify(s3Client, times(configuredNumberOfRetries)).deleteObjects(any(DeleteObjectsRequest.class));
+  }
+
+  private <T> ResponseInputStream<T> makeGetObjectStreamedResponse(T response, String body) {
+    var stream = new ByteArrayInputStream(body.getBytes());
+    return new ResponseInputStream<T>(response, AbortableInputStream.create(stream));
+  }
+
+  @Test
+  void getSingleObjectContent() {
+    GetObjectResponse response = GetObjectResponse.builder()
+        .eTag("abc")
+        .build();
+    var httpResponse = makeGetObjectStreamedResponse(response, "[]");
+    when(s3Client.getObject(any(GetObjectRequest.class))).thenReturn(httpResponse);
+    var jsonFile = s3ClientWrapper.getSingleObjectContent("example", "key1");
+    assertThat(jsonFile.getETag()).isEqualTo("abc");
+    assertThat(jsonFile.getContent()).isEqualTo("[]");
+  }
+
+  @Test
+  void jsonFileEtagIsNullWhenNotReceived() {
+    GetObjectResponse response = GetObjectResponse.builder()
+        .build();
+    var httpResponse = makeGetObjectStreamedResponse(response, "[]");
+    when(s3Client.getObject(any(GetObjectRequest.class))).thenReturn(httpResponse);
+    var jsonFile = s3ClientWrapper.getSingleObjectContent("example", "key1");
+    assertThat(jsonFile.getETag()).isNull();
+    assertThat(jsonFile.getContent()).isEqualTo("[]");
+  }
+
+  @ParameterizedTest
+  @ValueSource(classes = {NoSuchBucketException.class, S3Exception.class, SdkClientException.class, SdkException.class})
+  void shouldThrowSameExceptionAsAwsClient(Class<Exception> cause) {
+    when(s3Client.getObject(any(GetObjectRequest.class))).thenThrow(cause);
+    assertThatExceptionOfType(ExhaustedRetryException.class)
+        .isThrownBy(() -> s3ClientWrapper.getSingleObjectContent("", ""))
+        .withCauseExactlyInstanceOf(cause);
+  }
+
+  @Test
+  void shouldReturnNewObjectIfEtagDoesntMatch() throws NotModifiedException {
+    GetObjectResponse response = GetObjectResponse.builder()
+        .eTag("abc")
+        .build();
+    var httpResponse = makeGetObjectStreamedResponse(response, "[]");
+    when(s3Client.getObject(any(GetObjectRequest.class))).thenReturn(httpResponse);
+    var jsonFile = s3ClientWrapper.getSingleObjectContent("example", "key1", "cba");
+    assertThat(jsonFile.getETag()).isEqualTo("abc");
+    assertThat(jsonFile.getContent()).isEqualTo("[]");
+  }
+
+  @Test
+  void shouldThrowNotModifiedExceptionWhenStatusCodeIs304() {
+    when(s3Client.getObject(any(GetObjectRequest.class)))
+        .thenThrow(S3Exception.builder().statusCode(304).build());
+    assertThatExceptionOfType(ExhaustedRetryException.class)
+        .isThrownBy(() -> s3ClientWrapper.getSingleObjectContent("abc", "key1", "abc"))
+        .withCauseExactlyInstanceOf(NotModifiedException.class);
+  }
+
+  @ParameterizedTest
+  @ValueSource(classes = {NoSuchBucketException.class, S3Exception.class, SdkClientException.class, SdkException.class})
+  void shouldThrowSameExceptionAsAwsClientIfNot304(Class<Exception> cause) {
+    when(s3Client.getObject(any(GetObjectRequest.class))).thenThrow(cause);
+    assertThatExceptionOfType(ExhaustedRetryException.class)
+        .isThrownBy(() -> s3ClientWrapper.getSingleObjectContent("", "", "etag"))
+        .withCauseExactlyInstanceOf(cause);
   }
 }
