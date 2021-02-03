c043f1b97112ebf2ed1c1af9348c146d0dfea62a
Frederico <fred.rbittencourt@gmail.com>
2021-02-03 10:43:13 +0100

    Consider ETag when downloading Statistics JSON File (#1162)
    * draft: store eTag from statistic json file every download
    
    * adjust tests to consider latest etag + add tests for new code paths
    
    * implement retention policy for statistics download records
    
    * add missing tests to S3ClientWrapper
    
    * Update services/distribution/src/main/java/app/coronawarn/server/services/distribution/statistics/StatisticsToProtobufMapping.java
    
    Co-authored-by: ioangut <67064882+ioangut@users.noreply.github.com>
    
    * let's not ignore the reason for our own runtime exceptions
    
    Co-authored-by: ioangut <67064882+ioangut@users.noreply.github.com>
    Co-authored-by: Hilmar Falkenberg <hilmar.falkenberg@sap.com>

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/statistics/file/RemoteStatisticJsonFileLoader.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/statistics/file/RemoteStatisticJsonFileLoader.java
@@ -5,6 +5,8 @@ import app.coronawarn.server.services.distribution.objectstore.client.ObjectStor
 import app.coronawarn.server.services.distribution.statistics.exceptions.BucketNotFoundException;
 import app.coronawarn.server.services.distribution.statistics.exceptions.ConnectionException;
 import app.coronawarn.server.services.distribution.statistics.exceptions.FilePathNotFoundException;
+import app.coronawarn.server.services.distribution.statistics.exceptions.NotModifiedException;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
 import org.springframework.retry.ExhaustedRetryException;
@@ -17,9 +19,7 @@ import software.amazon.awssdk.services.s3.model.S3Exception;
 public class RemoteStatisticJsonFileLoader implements JsonFileLoader {
 
   ObjectStoreClient s3Stats;
-
   DistributionServiceConfig config;
-
 
   RemoteStatisticJsonFileLoader(@Qualifier("stats-s3") ObjectStoreClient s3Stats, DistributionServiceConfig config) {
     this.s3Stats = s3Stats;
@@ -27,21 +27,56 @@ public class RemoteStatisticJsonFileLoader implements JsonFileLoader {
   }
 
   /**
+   * Map parent retryable exception {@link ExhaustedRetryException} to cwa owned exceptions. The inner exception will be
+   * an S3 AwsException.
+   *
+   * @param ex {@link software.amazon.awssdk.core.exception.SdkException} wrapped in a {@link ExhaustedRetryException}.
+   * @return cwa owned RuntimeException.
+   */
+  private RuntimeException mapException(ExhaustedRetryException ex) {
+    if (ex.getCause() instanceof NoSuchBucketException) {
+      return new BucketNotFoundException(config.getStatistics().getBucket(), ex);
+    } else if (ex.getCause() instanceof S3Exception) {
+      return new FilePathNotFoundException(config.getStatistics().getStatisticPath(), ex);
+    } else {
+      return new ConnectionException(ex);
+    }
+  }
+
+  /**
    * Connects to remote storage to load file.
    *
-   * @return String content of file
+   * @return String content of file.
+   * @throws RuntimeException if errors found using AWS SDK.
    */
-  public String getContent() {
+  @Override
+  public JsonFile getFile() {
     try {
       return s3Stats.getSingleObjectContent(config.getStatistics().getBucket(),
           config.getStatistics().getStatisticPath());
     } catch (ExhaustedRetryException ex) {
-      if (ex.getCause() instanceof NoSuchBucketException) {
-        throw new BucketNotFoundException(config.getStatistics().getBucket());
-      } else if (ex.getCause() instanceof S3Exception) {
-        throw new FilePathNotFoundException(config.getStatistics().getStatisticPath());
+      throw mapException(ex);
+    }
+  }
+
+  /**
+   * Connects to remote storage to load file if not modified compared to {@param eTag}.
+   *
+   * @param etag only loads file if remote eTag is different from {@param eTag}.
+   * @return String content of file.
+   * @throws RuntimeException if errors found using AWS SDK.
+   */
+  @Override
+  public Optional<JsonFile> getFileIfUpdated(String etag) {
+    try {
+      var result = s3Stats.getSingleObjectContent(config.getStatistics().getBucket(),
+          config.getStatistics().getStatisticPath(), etag);
+      return Optional.of(result);
+    } catch (ExhaustedRetryException | NotModifiedException ex) {
+      if (ex.getCause() instanceof NotModifiedException || ex instanceof NotModifiedException) {
+        return Optional.empty();
       } else {
-        throw new ConnectionException();
+        throw mapException((ExhaustedRetryException) ex);
       }
     }
   }
