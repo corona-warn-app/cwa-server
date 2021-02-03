65d091aca715fc485a323e229a510d4ab621263d
Michael Braun <michael.braun01@sap.com>
2021-02-03 16:26:38 +0100

    Fix test imports

--- services/callback/src/test/java/app/coronawarn/server/services/callback/registration/CallbackRegistrationRunnerIntegrationTest.java
+++ services/callback/src/test/java/app/coronawarn/server/services/callback/registration/CallbackRegistrationRunnerIntegrationTest.java
@@ -7,21 +7,21 @@ import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.put;
 import static com.github.tomakehurst.wiremock.client.WireMock.putRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 import app.coronawarn.server.common.federation.client.callback.RegistrationResponse;
 import app.coronawarn.server.services.callback.config.CallbackServiceConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.http.HttpHeader;
 import com.github.tomakehurst.wiremock.http.HttpHeaders;
 import java.util.List;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.internal.verification.VerificationModeFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.SpyBean;
@@ -44,7 +44,7 @@ class CallbackRegistrationRunnerIntegrationTest {
     RegistrationResponse registrationResponse1 = new RegistrationResponse(computeHash("url1"), "url1");
     List<RegistrationResponse> responses = List.of(registrationResponse1);
 
-    server = new WireMockServer(WireMockConfiguration.options().port(1234));
+    server = new WireMockServer(options().port(1234));
     server.start();
     server.stubFor(
         get(urlEqualTo("/diagnosiskeys/callback"))
@@ -90,7 +90,7 @@ class CallbackRegistrationRunnerIntegrationTest {
         + computeHash("url") + "?url=url";
     server.verify(1, putRequestedFor(urlEqualTo(expectedPutUrl)));
 
-    verify(callbackServiceConfig, VerificationModeFactory.times(1)).isRegisterOnStartup();
-    verify(callbackServiceConfig, VerificationModeFactory.times(1)).getEndpointUrl();
+    verify(callbackServiceConfig, times(1)).isRegisterOnStartup();
+    verify(callbackServiceConfig, times(1)).getEndpointUrl();
   }
 }
