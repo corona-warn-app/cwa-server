e65a721298604a3318ed4e14d53a04adcadd12fe
ioangut <ioan.gut@sap.com>
2021-02-26 16:35:07 +0200

    update risk-calculation file name for new app config

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/appconfig/ApplicationConfigurationV2PublicationConfig.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/appconfig/ApplicationConfigurationV2PublicationConfig.java
@@ -61,7 +61,7 @@ public class ApplicationConfigurationV2PublicationConfig {
    * The location of the exposure configuration source files for Android and Ios.
    */
   private static final String V1_RISK_PARAMETERS_FILE = "main-config/v2/risk-calculation-parameters.yaml";
-  private static final String V2_RISK_PARAMETERS_FILE = "main-config/v2/risk-calculation-parameters-v2.yaml";
+  private static final String V2_RISK_PARAMETERS_FILE = "main-config/v2/risk-calculation-parameters-1.15.yaml";
   private static final String ANDROID_V2_DATA_MAPPING_FILE = "main-config/v2/diagnosis-keys-data-mapping.yaml";
   private static final String ANDROID_V2_DAILY_SUMMARIES_FILE = "main-config/v2/daily-summaries-config.yaml";
   private static final String IOS_V2_EXPOSURE_CONFIGURATION_FILE = "main-config/v2/exposure-configuration.yaml";
