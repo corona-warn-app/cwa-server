5ba3b58920d4175ecbb47d00871e59026eb1b627
FelixRottler <fero429@gmail.com>
2021-03-03 12:02:31 +0100

    refactor: add normal javadoc for enum source

--- common/persistence/src/main/java/app/coronawarn/server/common/persistence/domain/FederationBatchSourceSystem.java
+++ common/persistence/src/main/java/app/coronawarn/server/common/persistence/domain/FederationBatchSourceSystem.java
@@ -4,9 +4,14 @@ package app.coronawarn.server.common.persistence.domain;
  * Source for downloading  federation batches from different sources.
  */
 public enum FederationBatchSourceSystem {
-  // EFGS = European Federation Gateway Service
+  /**
+   * EFGS = European Federation Gateway Service
+   */
   EFGS,
-  // CHGS = Swiss Gateway Service
+  
+  /**
+   * CHGS = Swiss Gateway Service
+   */
   CHGS;
 
 
