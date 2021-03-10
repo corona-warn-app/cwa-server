a1d0ef4963b0f2fab483107ab9a0d19512de7e31
FelixRottler <fero429@gmail.com>
2021-03-10 16:48:03 +0100

    fix: adjust local setup

--- services/event-registration/src/main/java/app/coronawarn/server/services/eventregistration/EventRegistrationApplication.java
+++ services/event-registration/src/main/java/app/coronawarn/server/services/eventregistration/EventRegistrationApplication.java
@@ -2,10 +2,16 @@ package app.coronawarn.server.services.eventregistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
-@EnableConfigurationProperties
+@EnableConfigurationProperties({EventRegistrationConfiguration.class})
+@EnableJdbcRepositories(basePackages = {"app.coronawarn.server.common.persistence"})
+@EntityScan(basePackages = "app.coronawarn.server.common.persistence")
+@ComponentScan({"app.coronawarn.server.common.persistence"})
 public class EventRegistrationApplication {
 
   public static void main(String[] args) {
