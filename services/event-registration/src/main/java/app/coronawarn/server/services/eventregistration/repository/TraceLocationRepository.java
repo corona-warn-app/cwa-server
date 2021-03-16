485fa496c72b7c6fb073d59595680791b5479288
FelixRottler <fero429@gmail.com>
2021-03-16 17:22:25 +0100

    refactor: use tracelocation hash as primary key

--- services/event-registration/src/main/java/app/coronawarn/server/services/eventregistration/repository/TraceLocationRepository.java
+++ services/event-registration/src/main/java/app/coronawarn/server/services/eventregistration/repository/TraceLocationRepository.java
@@ -2,25 +2,24 @@ package app.coronawarn.server.services.eventregistration.repository;
 
 
 import app.coronawarn.server.services.eventregistration.domain.TraceLocation;
+import java.util.Optional;
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface TraceLocationRepository extends CrudRepository<TraceLocation, Long> {
+public interface TraceLocationRepository extends CrudRepository<TraceLocation, String> {
 
   @Modifying
   @Query("INSERT INTO trace_location (trace_location_guid_hash, version, created_at)"
-      + " VALUES (:traceLocationGuidHash,:version,:createdAt)")
-  void save(@Param("traceLocationGuidHash") byte[] traceLocationGuidHash,
+      + " VALUES (:traceLocationGuidHash,:version,:createdAt) ON CONFLICT DO NOTHING")
+  void saveOnConflictDoNothing(@Param("traceLocationGuidHash") String traceLocationGuidHash,
       @Param("version") Integer version,
       @Param("createdAt") Long createdAt);
 
   @Query("SELECT * FROM trace_location AS tl WHERE tl.trace_location_guid_hash=:traceLocationGuidHash")
   Optional<TraceLocation> findTraceLocationByGuidHash(
-      @Param("traceLocationGuidHash") byte[] traceLocationGuidHash);
+      @Param("traceLocationGuidHash") String traceLocationGuidHash);
 }
--- services/event-registration/src/test/java/app/coronawarn/server/services/eventregistration/repository/TraceLocationRepositoryTest.java
+++ services/event-registration/src/test/java/app/coronawarn/server/services/eventregistration/repository/TraceLocationRepositoryTest.java
@@ -5,19 +5,18 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
-import org.springframework.test.context.ActiveProfiles;
-
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Base64;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJdbcTest
-@ActiveProfiles("event-registration")
 public class TraceLocationRepositoryTest {
 
   @Autowired
@@ -31,15 +30,11 @@ public class TraceLocationRepositoryTest {
 
   @Test
   public void saveNewTraceLocationShouldBePersisted() throws Exception {
-    byte[] traceLocationGuidHash = MessageDigest.getInstance("SHA-256")
-        .digest(UUID
-            .randomUUID()
-            .toString()
-            .getBytes(
-                Charset.defaultCharset()));
-    Long createdAt = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+    String traceLocationGuidHash = buildUuidHash();
+    final Instant now = Instant.now();
+    Long createdAt = now.getEpochSecond();
     int version = 0;
-    underTest.save(traceLocationGuidHash, version, createdAt);
+    underTest.saveOnConflictDoNothing(traceLocationGuidHash, version, createdAt);
 
     Optional<TraceLocation> traceLocationOptional = underTest
         .findTraceLocationByGuidHash(traceLocationGuidHash);
@@ -47,6 +42,25 @@ public class TraceLocationRepositoryTest {
     assertThat(traceLocationOptional.get().getTraceLocationGuidHash()).isEqualTo(traceLocationGuidHash);
     assertThat(traceLocationOptional.get().getVersion()).isEqualTo(version);
     assertThat(traceLocationOptional.get().getCreatedAt()).isEqualTo(createdAt);
-    assertThat(traceLocationOptional.get().getId()).isNotNull();
+
+    int versionSecond = 1;
+    Long createdAtSecond = now.plus(1, ChronoUnit.HOURS).getEpochSecond();
+    underTest.saveOnConflictDoNothing(traceLocationGuidHash, versionSecond, createdAtSecond);
+
+    final Iterable<TraceLocation> all = underTest.findAll();
+    for (TraceLocation tr : all) {
+      assertThat(tr.getCreatedAt()).isEqualTo(createdAt);
+      assertThat(tr.getVersion()).isEqualTo(version);
+    }
+  }
+
+
+  private String buildUuidHash() throws NoSuchAlgorithmException {
+    return Base64.getEncoder().encodeToString(MessageDigest.getInstance("SHA-256")
+        .digest(UUID
+            .randomUUID()
+            .toString()
+            .getBytes(
+                Charset.defaultCharset())));
   }
 }
