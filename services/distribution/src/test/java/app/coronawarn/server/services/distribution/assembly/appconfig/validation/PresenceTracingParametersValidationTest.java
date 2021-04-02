1486d6fa2d52f2312d2c748eae1202764886ba44
ioangut <ioan.gut@sap.com>
2021-04-02 15:57:05 +0300

    Add Presence tracing parameters mapping tests

---
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/appconfig/validation/PresenceTracingParametersValidationTest.java
@@ -0,0 +1,43 @@
+package app.coronawarn.server.services.distribution.assembly.appconfig.validation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import app.coronawarn.server.common.protocols.internal.v2.PresenceTracingParameters;
+import app.coronawarn.server.services.distribution.assembly.appconfig.UnableToLoadFileException;
+import app.coronawarn.server.services.distribution.assembly.appconfig.YamlLoader;
+import org.junit.jupiter.api.Test;
+
+public class PresenceTracingParametersValidationTest {
+
+  private static final String PRESENCE_TRACING_PARAMETERS_FILE = "configtests/presence-tracing-parameters.yaml";
+
+  @Test
+  void testIfTheYamlFileWasLoadedInTheObject() throws UnableToLoadFileException {
+    PresenceTracingParameters.Builder presenceTracingParameters =
+        YamlLoader.loadYamlIntoProtobufBuilder(PRESENCE_TRACING_PARAMETERS_FILE,
+        PresenceTracingParameters.Builder.class);
+
+    assertThat(presenceTracingParameters.getSubmissionParameters()).isNotNull();
+    assertThat(presenceTracingParameters.getSubmissionParameters().getDurationFiltersCount()).isOne();
+    assertThat(presenceTracingParameters.getSubmissionParameters().getAerosoleDecayLinearFunctionsCount()).isEqualTo(2);
+    assertThat(presenceTracingParameters.getQrCodeErrorCorrectionLevelValue()).isZero();
+    assertThat(presenceTracingParameters.getRiskCalculationParameters()).isNotNull();
+    assertThat(presenceTracingParameters.getRiskCalculationParameters()
+        .getTransmissionRiskValueMappingCount()).isEqualTo(8);
+    assertThat(presenceTracingParameters.getRiskCalculationParameters()
+        .getNormalizedTimePerCheckInToRiskLevelMappingCount()).isEqualTo(2);
+    assertThat(presenceTracingParameters.getRiskCalculationParameters()
+        .getNormalizedTimePerDayToRiskLevelMappingCount()).isEqualTo(2);
+    assertThat(presenceTracingParameters.getRevokedTraceLocationVersionsList()).isEmpty();
+    assertThat(presenceTracingParameters.getPlausibleDeniabilityParameters()).isNotNull();
+    assertThat(presenceTracingParameters.getPlausibleDeniabilityParameters()
+        .getCheckInSizesInBytesCount()).isEqualTo(0);
+    assertThat(presenceTracingParameters.getPlausibleDeniabilityParameters()
+        .getProbabilityToFakeCheckInsIfNoCheckIns()).isZero();
+    assertThat(presenceTracingParameters.getPlausibleDeniabilityParameters()
+        .getProbabilityToFakeCheckInsIfSomeCheckIns()).isEqualTo(0);
+    assertThat(presenceTracingParameters.getPlausibleDeniabilityParameters()
+        .getNumberOfFakeCheckInsFunctionParametersCount()).isOne();
+    assertThat(presenceTracingParameters.getQrCodeDescriptorsCount()).isEqualTo(1);
+  }
+}
