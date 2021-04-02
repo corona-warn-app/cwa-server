37529890b6066f8d19f6ec9d8c1343f956225ef1
ioangut <ioan.gut@sap.com>
2021-04-02 15:09:22 +0300

    Add presence tracing parameters files and adjust the app config code to include the new configured parameters

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/appconfig/ApplicationConfigurationV2PublicationConfig.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/appconfig/ApplicationConfigurationV2PublicationConfig.java
@@ -25,11 +25,16 @@ import app.coronawarn.server.common.protocols.internal.v2.PPDDPrivacyPreservingA
 import app.coronawarn.server.common.protocols.internal.v2.PPDDPrivacyPreservingAnalyticsParametersAndroid;
 import app.coronawarn.server.common.protocols.internal.v2.PPDDPrivacyPreservingAnalyticsParametersCommon;
 import app.coronawarn.server.common.protocols.internal.v2.PPDDPrivacyPreservingAnalyticsParametersIOS;
+import app.coronawarn.server.common.protocols.internal.v2.PresenceTracingParameters;
+import app.coronawarn.server.common.protocols.internal.v2.PresenceTracingParameters.Builder;
+import app.coronawarn.server.common.protocols.internal.v2.PresenceTracingPlausibleDeniabilityParameters;
 import app.coronawarn.server.common.protocols.internal.v2.RiskCalculationParameters;
 import app.coronawarn.server.common.protocols.internal.v2.SemanticVersion;
 import app.coronawarn.server.services.distribution.assembly.appconfig.parsing.v2.DeserializedDailySummariesConfig;
 import app.coronawarn.server.services.distribution.assembly.appconfig.parsing.v2.DeserializedDiagnosisKeysDataMapping;
 import app.coronawarn.server.services.distribution.assembly.appconfig.parsing.v2.DeserializedExposureConfiguration;
+import app.coronawarn.server.services.distribution.assembly.appconfig.parsing.v2.DeserializedPlausibleDeniabilityParameters;
+import app.coronawarn.server.services.distribution.assembly.appconfig.parsing.v2.DeserializedRevokedTraceLocationVersions;
 import app.coronawarn.server.services.distribution.config.DistributionServiceConfig;
 import app.coronawarn.server.services.distribution.config.DistributionServiceConfig.AppConfigParameters.AndroidEventDrivenUserSurveyParameters;
 import app.coronawarn.server.services.distribution.config.DistributionServiceConfig.AppConfigParameters.AndroidExposureDetectionParameters;
@@ -62,6 +67,9 @@ public class ApplicationConfigurationV2PublicationConfig {
    */
   private static final String V1_RISK_PARAMETERS_FILE = "main-config/v2/risk-calculation-parameters.yaml";
   private static final String V2_RISK_PARAMETERS_FILE = "main-config/v2/risk-calculation-parameters-1.15.yaml";
+  private static final String PRESENCE_TRACING_PARAMETERS_FILE = "main-config/v2/presence-tracing-parameters.yaml";
+  private static final String PLAUSIBLE_DENIABILITY_PARAMETERS = "main-config/v2/plausible-deniability-parameters.yaml";
+  private static final String REVOKED_TRACE_LOCATION = "main-config/v2/revoked-trace-location-versions.yaml";
   private static final String ANDROID_V2_DATA_MAPPING_FILE = "main-config/v2/diagnosis-keys-data-mapping.yaml";
   private static final String ANDROID_V2_DAILY_SUMMARIES_FILE = "main-config/v2/daily-summaries-config.yaml";
   private static final String IOS_V2_EXPOSURE_CONFIGURATION_FILE = "main-config/v2/exposure-configuration.yaml";
@@ -106,6 +114,7 @@ public class ApplicationConfigurationV2PublicationConfig {
         .setDiagnosisKeysDataMapping(buildDataMapping(dataMapping))
         .setEventDrivenUserSurveyParameters(buildAndroidEdusParameters(distributionServiceConfig))
         .setPrivacyPreservingAnalyticsParameters(buildAndroidPpaParameters(distributionServiceConfig))
+        .setPresenceTracingParameters(buildPresenceTracingParameters(distributionServiceConfig))
         .build();
   }
 
@@ -146,7 +155,35 @@ public class ApplicationConfigurationV2PublicationConfig {
         .setEventDrivenUserSurveyParameters(buildAndroidEdusParameters(distributionServiceConfig))
         .setPrivacyPreservingAnalyticsParameters(buildAndroidPpaParameters(distributionServiceConfig))
         .setErrorLogSharingParameters(buildErrorLogSharingParametersAndroid(distributionServiceConfig))
+        .setPresenceTracingParameters(buildPresenceTracingParameters(distributionServiceConfig))
         .build();
+  }
+
+  private Builder buildPresenceTracingParameters(DistributionServiceConfig distributionServiceConfig)
+      throws UnableToLoadFileException {
+    PresenceTracingParameters.Builder presenceTracingParameters =
+        YamlLoader.loadYamlIntoProtobufBuilder(PRESENCE_TRACING_PARAMETERS_FILE,
+            PresenceTracingParameters.Builder.class);
+    DeserializedPlausibleDeniabilityParameters deserializedPlausibleDeniabilityParameters = YamlLoader
+        .loadYamlIntoClass(
+            PLAUSIBLE_DENIABILITY_PARAMETERS, DeserializedPlausibleDeniabilityParameters.class);
+
+    DeserializedRevokedTraceLocationVersions deserializedRevokedTraceLocationVersions = YamlLoader.loadYamlIntoClass(
+        REVOKED_TRACE_LOCATION, DeserializedRevokedTraceLocationVersions.class);
+
+    presenceTracingParameters
+        .setQrCodeErrorCorrectionLevelValue(
+            distributionServiceConfig.getPresenceTracingParameters().getQrCodeErrorCorrectionLevel())
+        .addAllRevokedTraceLocationVersions(deserializedRevokedTraceLocationVersions.getRevokedTraceLocationVersions())
+        .setPlausibleDeniabilityParameters(PresenceTracingPlausibleDeniabilityParameters.newBuilder()
+            .addAllCheckInSizesInBytes(deserializedPlausibleDeniabilityParameters.getCheckInSizesInBytes())
+            .setProbabilityToFakeCheckInsIfNoCheckIns(
+                distributionServiceConfig.getPresenceTracingParameters().getProbabilityToFakeCheckInsIfNoCheckIns())
+            .setProbabilityToFakeCheckInsIfSomeCheckIns(
+                distributionServiceConfig.getPresenceTracingParameters().getProbabilityToFakeCheckInsIfSomeCheckIns())
+            .build())
+        .build();
+    return presenceTracingParameters;
   }
 
   private PPDDErrorLogSharingParametersAndroid buildErrorLogSharingParametersAndroid(
@@ -291,6 +328,7 @@ public class ApplicationConfigurationV2PublicationConfig {
         .setExposureDetectionParameters(buildExposureDetectionParametersIos(distributionServiceConfig))
         .setEventDrivenUserSurveyParameters(buildIosEdusParameters(distributionServiceConfig))
         .setPrivacyPreservingAnalyticsParameters(buildIosPpaParameters(distributionServiceConfig))
+        .setPresenceTracingParameters(buildPresenceTracingParameters(distributionServiceConfig))
         .build();
   }
 
@@ -326,6 +364,7 @@ public class ApplicationConfigurationV2PublicationConfig {
         .setEventDrivenUserSurveyParameters(buildIosEdusParameters(distributionServiceConfig))
         .setPrivacyPreservingAnalyticsParameters(buildIosPpaParameters(distributionServiceConfig))
         .setErrorLogSharingParameters(buildErrorLogSharingParametersIos())
+        .setPresenceTracingParameters(buildPresenceTracingParameters(distributionServiceConfig))
         .build();
   }
 
