f49e0eef6068d63c4060f8e37afda82dbcab1be5
ioangut <ioan.gut@sap.com>
2021-04-12 13:32:16 +0300

    Add coronaTestParameters to the app config

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/appconfig/ApplicationConfigurationV2PublicationConfig.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/appconfig/ApplicationConfigurationV2PublicationConfig.java
@@ -5,6 +5,7 @@ import app.coronawarn.server.common.protocols.internal.v2.AppFeature;
 import app.coronawarn.server.common.protocols.internal.v2.AppFeatures;
 import app.coronawarn.server.common.protocols.internal.v2.ApplicationConfigurationAndroid;
 import app.coronawarn.server.common.protocols.internal.v2.ApplicationConfigurationIOS;
+import app.coronawarn.server.common.protocols.internal.v2.CoronaTestParameters;
 import app.coronawarn.server.common.protocols.internal.v2.DailySummariesConfig;
 import app.coronawarn.server.common.protocols.internal.v2.DayPackageMetadata;
 import app.coronawarn.server.common.protocols.internal.v2.DiagnosisKeysDataMapping;
@@ -68,6 +69,7 @@ public class ApplicationConfigurationV2PublicationConfig {
   private static final String V1_RISK_PARAMETERS_FILE = "main-config/v2/risk-calculation-parameters.yaml";
   private static final String V2_RISK_PARAMETERS_FILE = "main-config/v2/risk-calculation-parameters-1.15.yaml";
   private static final String PRESENCE_TRACING_PARAMETERS_FILE = "main-config/v2/presence-tracing-parameters.yaml";
+  private static final String CORONA_TEST_PARAMETERS_FILE = "main-config/v2/corona-test-parameters.yaml";
   private static final String PLAUSIBLE_DENIABILITY_PARAMETERS = "main-config/v2/plausible-deniability-parameters.yaml";
   private static final String REVOKED_TRACE_LOCATION = "main-config/v2/revoked-trace-location-versions.yaml";
   private static final String ANDROID_V2_DATA_MAPPING_FILE = "main-config/v2/diagnosis-keys-data-mapping.yaml";
@@ -94,6 +96,10 @@ public class ApplicationConfigurationV2PublicationConfig {
     riskCalculationParameterBuilder.addAllTransmissionRiskValueMapping(
         trlValueMappingProvider.getTransmissionRiskValueMappingAsProto());
 
+    CoronaTestParameters.Builder coronaTestParameters =
+        YamlLoader.loadYamlIntoProtobufBuilder(CORONA_TEST_PARAMETERS_FILE,
+            CoronaTestParameters.Builder.class);
+
     DeserializedDiagnosisKeysDataMapping dataMapping = YamlLoader.loadYamlIntoClass(
         ANDROID_V2_DATA_MAPPING_FILE, DeserializedDiagnosisKeysDataMapping.class);
 
@@ -115,6 +121,7 @@ public class ApplicationConfigurationV2PublicationConfig {
         .setEventDrivenUserSurveyParameters(buildAndroidEdusParameters(distributionServiceConfig))
         .setPrivacyPreservingAnalyticsParameters(buildAndroidPpaParameters(distributionServiceConfig))
         .setPresenceTracingParameters(buildPresenceTracingParameters(distributionServiceConfig))
+        .setCoronaTestParameters(coronaTestParameters)
         .build();
   }
 
@@ -138,6 +145,10 @@ public class ApplicationConfigurationV2PublicationConfig {
     riskCalculationParameterBuilder.addAllTransmissionRiskValueMapping(
         trlValueMappingProvider.getTransmissionRiskValueMappingAsProto());
 
+    CoronaTestParameters.Builder coronaTestParameters =
+        YamlLoader.loadYamlIntoProtobufBuilder(CORONA_TEST_PARAMETERS_FILE,
+            CoronaTestParameters.Builder.class);
+
     DeserializedDiagnosisKeysDataMapping dataMapping = YamlLoader.loadYamlIntoClass(
         ANDROID_V2_DATA_MAPPING_FILE, DeserializedDiagnosisKeysDataMapping.class);
 
@@ -160,6 +171,7 @@ public class ApplicationConfigurationV2PublicationConfig {
         .setPrivacyPreservingAnalyticsParameters(buildAndroidPpaParameters(distributionServiceConfig))
         .setErrorLogSharingParameters(buildErrorLogSharingParametersAndroid(distributionServiceConfig))
         .setPresenceTracingParameters(buildPresenceTracingParameters(distributionServiceConfig))
+        .setCoronaTestParameters(coronaTestParameters)
         .build();
   }
 
@@ -323,6 +335,10 @@ public class ApplicationConfigurationV2PublicationConfig {
     riskCalculationParameterBuilder.addAllTransmissionRiskValueMapping(
         trlValueMappingProvider.getTransmissionRiskValueMappingAsProto());
 
+    CoronaTestParameters.Builder coronaTestParameters =
+        YamlLoader.loadYamlIntoProtobufBuilder(CORONA_TEST_PARAMETERS_FILE,
+            CoronaTestParameters.Builder.class);
+
     DeserializedExposureConfiguration exposureConfiguration = YamlLoader.loadYamlIntoClass(
         IOS_V2_EXPOSURE_CONFIGURATION_FILE, DeserializedExposureConfiguration.class);
 
@@ -339,6 +355,7 @@ public class ApplicationConfigurationV2PublicationConfig {
         .setEventDrivenUserSurveyParameters(buildIosEdusParameters(distributionServiceConfig))
         .setPrivacyPreservingAnalyticsParameters(buildIosPpaParameters(distributionServiceConfig))
         .setPresenceTracingParameters(buildPresenceTracingParameters(distributionServiceConfig))
+        .setCoronaTestParameters(coronaTestParameters)
         .build();
   }
 
@@ -362,6 +379,10 @@ public class ApplicationConfigurationV2PublicationConfig {
     riskCalculationParameterBuilder.addAllTransmissionRiskValueMapping(
         trlValueMappingProvider.getTransmissionRiskValueMappingAsProto());
 
+    CoronaTestParameters.Builder coronaTestParameters =
+        YamlLoader.loadYamlIntoProtobufBuilder(CORONA_TEST_PARAMETERS_FILE,
+            CoronaTestParameters.Builder.class);
+
     DeserializedExposureConfiguration exposureConfiguration = YamlLoader.loadYamlIntoClass(
         IOS_V2_EXPOSURE_CONFIGURATION_FILE, DeserializedExposureConfiguration.class);
 
@@ -379,6 +400,7 @@ public class ApplicationConfigurationV2PublicationConfig {
         .setPrivacyPreservingAnalyticsParameters(buildIosPpaParameters(distributionServiceConfig))
         .setErrorLogSharingParameters(buildErrorLogSharingParametersIos())
         .setPresenceTracingParameters(buildPresenceTracingParameters(distributionServiceConfig))
+        .setCoronaTestParameters(coronaTestParameters)
         .build();
   }
 
