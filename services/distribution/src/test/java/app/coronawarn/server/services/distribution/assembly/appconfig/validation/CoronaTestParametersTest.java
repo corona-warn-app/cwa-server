6bdb56f55183d9d290acd2409e3895ce852f1ea5
ioangut <67064882+ioangut@users.noreply.github.com>
2021-04-15 15:26:40 +0300

    Add coronaTestParameters to the app config (#1331)
    * Add coronaTestParameters to the app config
    
    * Remove checkstyles errors

---
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/appconfig/validation/CoronaTestParametersTest.java
@@ -0,0 +1,24 @@
+package app.coronawarn.server.services.distribution.assembly.appconfig.validation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import app.coronawarn.server.common.protocols.internal.v2.CoronaTestParameters;
+import app.coronawarn.server.services.distribution.assembly.appconfig.UnableToLoadFileException;
+import app.coronawarn.server.services.distribution.assembly.appconfig.YamlLoader;
+import org.junit.jupiter.api.Test;
+
+public class CoronaTestParametersTest {
+
+  private static final String CORONA_TEST_PARAMETERS_FILE = "configtests/corona-test-parameters.yaml";
+
+
+  @Test
+  void testLoadingOfCoronaTestParametersFile() throws UnableToLoadFileException {
+    CoronaTestParameters.Builder coronaTestParameters =
+        YamlLoader.loadYamlIntoProtobufBuilder(CORONA_TEST_PARAMETERS_FILE,
+            CoronaTestParameters.Builder.class);
+
+    assertThat(coronaTestParameters.getCoronaRapidAntigenTestParameters()).isNotNull();
+    assertThat(coronaTestParameters.getCoronaRapidAntigenTestParameters().getHoursToDeemTestOutdated()).isEqualTo(48);
+  }
+}
