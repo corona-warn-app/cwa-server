ff4c43458ad1e2dbdedcb4cbffafbfe8d25c72ed
alicebilc <alice.bilc@gmail.com>
2021-04-16 11:37:28 +0300

    Changed @Depricated ConfigFileApplicationContextInitializer.class with ConfigDataApplicationContextInitializer.class

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/component/QrCodePosterTemplateStructureProviderTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/component/QrCodePosterTemplateStructureProviderTest.java
@@ -1,9 +1,16 @@
 package app.coronawarn.server.services.distribution.assembly.component;
 
 import static org.mockito.Mockito.when;
+
+import app.coronawarn.server.common.persistence.service.TraceTimeIntervalWarningService;
+import app.coronawarn.server.services.distribution.assembly.qrcode.QrCodeTemplateLoader;
+import app.coronawarn.server.services.distribution.assembly.structure.WritableOnDisk;
+import app.coronawarn.server.services.distribution.assembly.structure.directory.Directory;
+import app.coronawarn.server.services.distribution.assembly.structure.directory.DirectoryOnDisk;
+import app.coronawarn.server.services.distribution.config.DistributionServiceConfig;
+import app.coronawarn.server.services.distribution.config.TransmissionRiskLevelEncoding;
 import java.io.File;
 import java.io.IOException;
-import java.util.Collections;
 import org.junit.Rule;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,27 +20,17 @@ import org.junit.rules.TemporaryFolder;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import app.coronawarn.server.common.persistence.service.TraceTimeIntervalWarningService;
-import app.coronawarn.server.common.persistence.service.common.KeySharingPoliciesChecker;
-import app.coronawarn.server.services.distribution.assembly.qrcode.QrCodeTemplateLoader;
-import app.coronawarn.server.services.distribution.assembly.structure.WritableOnDisk;
-import app.coronawarn.server.services.distribution.assembly.structure.directory.Directory;
-import app.coronawarn.server.services.distribution.assembly.structure.directory.DirectoryOnDisk;
-import app.coronawarn.server.services.distribution.assembly.tracewarnings.TraceTimeIntervalWarningsPackageBundler;
-import app.coronawarn.server.services.distribution.assembly.transformation.EnfParameterAdapter;
-import app.coronawarn.server.services.distribution.config.DistributionServiceConfig;
-import app.coronawarn.server.services.distribution.config.TransmissionRiskLevelEncoding;
 
 @EnableConfigurationProperties(
     value = {DistributionServiceConfig.class, TransmissionRiskLevelEncoding.class})
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(
     classes = {DistributionServiceConfig.class,QrCodeTemplateLoader.class},
-    initializers = ConfigFileApplicationContextInitializer.class)
+    initializers = ConfigDataApplicationContextInitializer.class)
 public class QrCodePosterTemplateStructureProviderTest {
 
   private static final String PARENT_TEST_FOLDER = "parent";
