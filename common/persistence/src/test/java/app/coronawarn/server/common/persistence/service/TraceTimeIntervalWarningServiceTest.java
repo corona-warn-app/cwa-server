56f04133cf8e13af8ba12b7767e1b6fe44398d55
ioangut <ioan.gut@sap.com>
2021-04-19 10:40:59 +0300

    Update TraceTimeIntervalWarningServiceTest.java

--- common/persistence/src/test/java/app/coronawarn/server/common/persistence/service/TraceTimeIntervalWarningServiceTest.java
+++ common/persistence/src/test/java/app/coronawarn/server/common/persistence/service/TraceTimeIntervalWarningServiceTest.java
@@ -207,57 +207,6 @@ class TraceTimeIntervalWarningServiceTest {
     }
   }
 
-  @Test
-  void testSortedRetrievalResult() {
-    traceWarningsRepository
-        .saveDoNothingOnConflict(hashLocationId(ByteString.copyFromUtf8("sorted-uuid2")),
-            56,
-            10,
-            3,
-            CheckinsDateSpecification.HOUR_SINCE_EPOCH_DERIVATION
-                .apply(Instant.now().getEpochSecond()));
-    traceWarningsRepository
-        .saveDoNothingOnConflict(hashLocationId(ByteString.copyFromUtf8("sorted-uuid1")),
-            456,
-            20,
-            2,
-            CheckinsDateSpecification.HOUR_SINCE_EPOCH_DERIVATION
-                .apply(Instant.now().getEpochSecond()) - 10);
-
-    List<CheckIn> checkins = new ArrayList<>(List.of(
-        CheckIn.newBuilder().setStartIntervalNumber(56).setEndIntervalNumber(66)
-            .setTransmissionRiskLevel(3)
-            .setLocationId(ByteString.copyFromUtf8("sorted-uuid2"))
-            .build(),
-        CheckIn.newBuilder().setStartIntervalNumber(456).setEndIntervalNumber(476)
-            .setTransmissionRiskLevel(2)
-            .setLocationId(ByteString.copyFromUtf8("sorted-uuid1"))
-            .build()));
-
-    // Reverse as we tempered with submission timestamp
-    Collections.reverse(checkins);
-
-    List<TraceTimeIntervalWarning> checkinsFromDB = new ArrayList<>(
-        traceWarningsService.getTraceTimeIntervalWarnings());
-
-    assertCheckinsAndWarningsAreEqual(checkins, checkinsFromDB);
-  }
-
-  /**
-   * Contract for hashing between client and server.
-   */
-  @Test
-  public void testHashingOfTraceLocationId() {
-    String locationId = "afa27b44d43b02a9fea41d13cedc2e4016cfcf87c5dbf990e593669aa8ce286d";
-    byte[] locationIdByte = Hex.decode(locationId);
-    byte[] hashedLocationId = hashLocationId(ByteString.copyFrom(locationIdByte));
-
-    final byte[] encode = Hex.encode(hashedLocationId);
-    String s = new String(encode);
-
-    assertEquals("0f37dac11d1b8118ea0b44303400faa5e3b876da9d758058b5ff7dc2e5da8230", s);
-  }
-
   private byte[] hashLocationId(ByteString locationId) {
     try {
       return MessageDigest.getInstance("SHA-256").digest(locationId.toByteArray());
