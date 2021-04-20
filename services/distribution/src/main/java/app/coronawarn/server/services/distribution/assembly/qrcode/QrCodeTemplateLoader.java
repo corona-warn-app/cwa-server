33422475dbae91e4181f958ee0d355086a32e907
Jeremias Rößler <jeremias.roessler@sap.com>
2021-04-20 16:23:11 +0200

    Load the QRCode template dynamically via env var...

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/qrcode/QrCodeTemplateLoader.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/qrcode/QrCodeTemplateLoader.java
@@ -1,6 +1,9 @@
 package app.coronawarn.server.services.distribution.assembly.qrcode;
 
+import app.coronawarn.server.services.distribution.config.DistributionServiceConfig;
 import com.google.protobuf.ByteString;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import org.slf4j.Logger;
@@ -12,21 +15,27 @@ public class QrCodeTemplateLoader {
 
   private static final Logger logger = LoggerFactory.getLogger(QrCodeTemplateLoader.class);
 
+  private final DistributionServiceConfig config;
+
+  public QrCodeTemplateLoader(DistributionServiceConfig config) {
+    this.config = config;
+  }
+
   public ByteString loadAndroidTemplateAsBytes() {
-    return loadPosterTemplate("pt-android-poster-1.0.0.pdf");
+    return loadPosterTemplate(config.getAndroidQrCodePosterTemplate().getTemplate());
   }
 
   public ByteString loadIosTemplateAsBytes() {
-    return loadPosterTemplate("pt-ios-poster-1.0.0.pdf");
+    return loadPosterTemplate(config.getIosQrCodePosterTemplate().getTemplate());
   }
 
-  private ByteString loadPosterTemplate(String filename) {
-    try (InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(filename)) {
+  private ByteString loadPosterTemplate(File file) {
+    try (InputStream resourceAsStream = new FileInputStream(file)) {
       return ByteString.readFrom(resourceAsStream);
     } catch (IOException e) {
       logger.error(
-          "Could not load '" + filename + "' QR poster template from the application package", e);
-      //At the moment just log the error but do not interfere with normal application startup
+          "Could not load '" + file.getAbsolutePath() + "' QR poster template from the application package", e);
+      // At the moment just log the error but do not interfere with normal application startup
       return ByteString.copyFromUtf8("");
     }
   }
