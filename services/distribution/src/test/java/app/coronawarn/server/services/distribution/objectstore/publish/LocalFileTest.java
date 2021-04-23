c049cd89e624a10541cbdb4370ecd38318eaf3d9
alicebilc <alice.bilc@gmail.com>
2021-04-23 15:27:19 +0300

    Set proper content-type for QR code templates

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/objectstore/publish/LocalFileTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/objectstore/publish/LocalFileTest.java
@@ -11,13 +11,14 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class LocalFileTest {
+
   @ParameterizedTest
   @ValueSource(strings = {
       "version",
       "version/v1/configuration/country",
       "version/v1/diagnosis-keys/country",
       "version/v1/diagnosis-keys/country/DE/date",
-      "version/v1/diagnosis-keys/country/DE/date/2020-06-11/hour" })
+      "version/v1/diagnosis-keys/country/DE/date/2020-06-11/hour"})
   void testGetContentTypeJson(String path) {
     LocalFile test = new LocalIndexFile(Path.of("/root", path, "/index"), Path.of("/root"));
     assertEquals("application/json", test.getContentType());
@@ -30,7 +31,7 @@ class LocalFileTest {
       "version/v1/app_config_android",
       "version/v1/configuration/country/DE/app_config",
       "version/v1/diagnosis-keys/country/DE/date/2020-06-11",
-      "version/v1/diagnosis-keys/country/DE/date/2020-06-11/hour/13" })
+      "version/v1/diagnosis-keys/country/DE/date/2020-06-11/hour/13"})
   void testGetContentTypeZip(String path) {
     LocalFile test = new LocalIndexFile(Path.of("/root", path, "/index"), Path.of("/root"));
     assertEquals("application/zip", test.getContentType());
@@ -39,7 +40,7 @@ class LocalFileTest {
   @ParameterizedTest
   @ValueSource(strings = {
       "version/v1/diagnosis-keys/country/DE/date/2020-06-11",
-      "version/v1/diagnosis-keys/country/DE/date/2020-06-11/hour/13" })
+      "version/v1/diagnosis-keys/country/DE/date/2020-06-11/hour/13"})
   void testIsKeyFile(String path) {
     LocalFile test = new LocalIndexFile(Path.of("/root", path, "/index"), Path.of("/root"));
     assertTrue(test.isKeyFile());
@@ -52,9 +53,33 @@ class LocalFileTest {
       "version/v1/configuration/country/DE/app_config",
       "version/v1/diagnosis-keys/country",
       "version/v1/diagnosis-keys/country/DE/date",
-      "version/v1/diagnosis-keys/country/DE/date/2020-06-11/hour" })
+      "version/v1/diagnosis-keys/country/DE/date/2020-06-11/hour"})
   void testIsNotKeyFile(String path) {
     LocalFile test = new LocalIndexFile(Path.of("/root", path, "/index"), Path.of("/root"));
     assertFalse(test.isKeyFile());
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {
+      "version/v1/qr_code_poster_template_android",
+      "version/v1/qr_code_poster_template_android.checksum",
+      "version/v1/qr_code_poster_template_ios",
+      "version/v1/qr_code_poster_template_ios.checksum"
+  })
+  void testIsQrPosterTemplate(String path) {
+    LocalFile test = new LocalIndexFile(Path.of("/root", path, "/index"), Path.of("/root"));
+    Assertions.assertTrue(test.isQrPosterTemplate());
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {
+      "version",
+      "version/v1",
+      "version/v1/app_config_ios",
+      "version/v1/app_config_android"
+  })
+  void testIsNotQrPosterTemplate(String path) {
+    LocalFile test = new LocalIndexFile(Path.of("/root", path, "/index"), Path.of("/root"));
+    Assertions.assertFalse(test.isQrPosterTemplate());
   }
 }
