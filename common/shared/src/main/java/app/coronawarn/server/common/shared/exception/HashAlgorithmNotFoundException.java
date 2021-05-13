fe1a0b762582d5e3d3cb76f955cd5658e671e121
alexmoroianu <alexandru.moroianu@sap.com>
2021-05-13 10:27:30 +0300

    TESTS - increase coverage

--- common/shared/src/main/java/app/coronawarn/server/common/shared/exception/HashAlgorithmNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package app.coronawarn.server.common.shared.exception;
-
-public class HashAlgorithmNotFoundException extends RuntimeException {
-
-  public HashAlgorithmNotFoundException(String msg, Throwable rootCause) {
-    super(msg, rootCause);
-  }
-}
