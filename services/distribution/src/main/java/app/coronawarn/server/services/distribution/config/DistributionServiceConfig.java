e40c1008b88db78def9e5f97d9fd84d70f451a08
Jeremias Rößler <jeremias.roessler@sap.com>
2021-05-17 09:44:24 +0200

    Change the regex to allow for 'file://' prefix

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/config/DistributionServiceConfig.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/config/DistributionServiceConfig.java
@@ -19,7 +19,8 @@ import org.springframework.validation.annotation.Validated;
 public class DistributionServiceConfig {
 
   private static final String PATH_REGEX = "^[/]?[a-zA-Z0-9_]{1,1024}(/[a-zA-Z0-9_]{1,1024}){0,256}[/]?$";
-  private static final String PATH_OR_EMPTY_REGEX = "(" + PATH_REGEX + ")?";
+  private static final String RESOURCE_REGEX = "^(classpath:|file:[/]{1,8})?[a-zA-Z0-9_]{1,1024}([a-zA-Z0-9_]{1,1024}){0,256}[/]?$";
+  private static final String RESOURCE_OR_EMPTY_REGEX = "(" + RESOURCE_REGEX + ")?";
   private static final String FILE_NAME_REGEX = "^[a-zA-Z0-9_-]{1,1024}$";
   private static final String FILE_NAME_WITH_TYPE_REGEX = "^[a-zA-Z0-9_-]{1,1024}\\.[a-z]{1,64}$";
   private static final String CHAR_AND_NUMBER_REGEX = "^[a-zA-Z0-9_-]{1,1024}$";
@@ -1477,13 +1478,13 @@ public class DistributionServiceConfig {
 
   public static class DigitalGreenCertificate {
 
-    @Pattern(regexp = PATH_OR_EMPTY_REGEX)
+    @Pattern(regexp = RESOURCE_OR_EMPTY_REGEX)
     private String mahJsonPath;
 
-    @Pattern(regexp = PATH_OR_EMPTY_REGEX)
+    @Pattern(regexp = RESOURCE_OR_EMPTY_REGEX)
     private String prophylaxisJsonPath;
 
-    @Pattern(regexp = PATH_OR_EMPTY_REGEX)
+    @Pattern(regexp = RESOURCE_OR_EMPTY_REGEX)
     private String medicinalProductsJsonPath;
 
     @NotNull
