fb13668356ec8fce99aec3d3f390b734e9d580e8
Jeremias Rößler <jeremias.roessler@sap.com>
2021-05-17 15:46:05 +0200

    Improve code using DigitalGreenCertificateToProtobufMapping as role model

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/qrcode/QrCodeTemplateLoader.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/qrcode/QrCodeTemplateLoader.java
@@ -1,7 +1,5 @@
 package app.coronawarn.server.services.distribution.assembly.qrcode;
 
-import static org.springframework.core.io.ResourceLoader.CLASSPATH_URL_PREFIX;
-
 import app.coronawarn.server.services.distribution.config.DistributionServiceConfig;
 import com.google.protobuf.ByteString;
 import java.io.IOException;
@@ -11,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
 @Component
 public class QrCodeTemplateLoader {
@@ -24,28 +23,28 @@ public class QrCodeTemplateLoader {
   ResourceLoader resourceLoader;
 
   public ByteString loadAndroidTemplateAsBytes() {
-    return loadPosterTemplate(config.getAndroidQrCodePosterTemplate().getTemplate(),
-        CLASSPATH_URL_PREFIX + "pt-android-poster-1.0.0.pdf");
+    return loadPosterTemplate(config.getAndroidQrCodePosterTemplate().getTemplate(), "pt-android-poster-1.0.0.pdf");
   }
 
   public ByteString loadIosTemplateAsBytes() {
-    return loadPosterTemplate(config.getIosQrCodePosterTemplate().getTemplate(),
-        CLASSPATH_URL_PREFIX + "pt-ios-poster-1.0.0.pdf");
+    return loadPosterTemplate(config.getIosQrCodePosterTemplate().getTemplate(), "pt-ios-poster-1.0.0.pdf");
   }
 
   private ByteString loadPosterTemplate(String filename, String fallback) {
-    try (InputStream fileStream = resourceLoader.getResource(filename).getInputStream()) {
-      return ByteString.readFrom(fileStream);
-    } catch (IOException e) {
-      logger.error(
-          "Error loading QR poster template from '" + filename + "', loading default from application package: {} ({})",
-          e.getClass().getName(), e.getMessage());
-      try (InputStream resourceAsStream = resourceLoader.getResource(fallback).getInputStream()) {
-        return ByteString.readFrom(resourceAsStream);
-      } catch (Exception e2) {
-        logger.error(
-            "Could not load default QR poster template from the application package!", e);
+    if (!ObjectUtils.isEmpty(filename)) {
+      try (InputStream fileStream = resourceLoader.getResource(filename).getInputStream()) {
+        logger.debug("Loading QR poster template from {}.", filename);
+        return ByteString.readFrom(fileStream);
+      } catch (IOException e) {
+        logger.error("Error loading QR poster template from '{}'.", filename, e);
       }
+    }
+    try (InputStream resourceAsStream = resourceLoader.getResource(fallback).getInputStream()) {
+      // fallback to default
+      logger.debug("QR poster template to load was empty or invalid, falling back to loading from {}.", fallback);
+      return ByteString.readFrom(resourceAsStream);
+    } catch (Exception e) {
+      logger.error("Could not load default QR poster template {}. This shouldn't happen!", fallback, e);
     }
     // At the moment just log the error but do not interfere with normal application startup
     return ByteString.copyFromUtf8("");
