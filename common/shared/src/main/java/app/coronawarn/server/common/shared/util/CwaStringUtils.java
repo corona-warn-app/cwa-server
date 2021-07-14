c0402c5c7eb648b9311caf9ca6af93fc054eccba
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2021-07-14 10:03:14 +0200

    util classes should not have public constructors
    this should increase the code coverage

--- common/shared/src/main/java/app/coronawarn/server/common/shared/util/CwaStringUtils.java
+++ common/shared/src/main/java/app/coronawarn/server/common/shared/util/CwaStringUtils.java
@@ -2,6 +2,9 @@ package app.coronawarn.server.common.shared.util;
 
 public class CwaStringUtils {
 
+  private CwaStringUtils() {
+  }
+
   public static char[] emptyCharrArrayIfNull(String input) {
     return input != null ? input.toCharArray() : new char[] {};
   }
