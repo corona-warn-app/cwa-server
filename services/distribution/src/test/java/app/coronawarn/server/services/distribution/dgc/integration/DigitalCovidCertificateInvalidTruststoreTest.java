c8a68d9fc919ddb017c4449ae7e259238c8c91b2
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2021-07-14 11:52:19 +0200

    Certificate pinning - establish TSL connection with DCC (#1483) (#1486)
    * add jackson-cbor
    
    * ADD - Feign server for DCC, unstable
    
    * ENHANCE - DCC client
    
    * ADD - country list in distribution package
    
    * ENHANCE - business rules, unstable
    
    * ADD - Schema validation and new samples, first full distribution complete run
    
    * ADD - Feign client configuration, first DEV delpoyment
    
    * ADD - loggers
    
    * FIX - dgc structure provider test
    
    * FIX - distribution IT
    
    * let's centrally manage all versions
    
    * obsolete
    
    * REMOVE - duplicate sample json files
    
    * REFACTOR - acceptance rules
    
    * TESTS - increase coverage
    
    * TESTS - increase coverage for country rules
    
    * MERGE - rules with valuesets
    
    * FIX - merge conflict leftovers
    
    * TESTS - increase coverage for country rules
    
    * TESTS - increase coverage for country rules
    
    * TESTS - increase coverage for country rules
    
    * REMOVE - unused variables
    
    * FIX - javadoc typo
    
    * ADAPT - fail mechanism to exception to optional
    
    * REMOVE - exception thrown
    
    * FIX - distribution error on feign client configuration
    
    * FIX - distribution error on feign client configuration
    
    * ADD - retry mechanism for DCC feign client
    
    * Certificate pinning - establish TSL connection with DCC
    
    * FIX - readme formatting
    
    * FIX - PR requests and code coverage
    
    * FIX - checkstyles
    
    * Update common/shared/src/test/java/app/coronawarn/server/common/shared/util/CwaStringTest.java
    
    Co-authored-by: Hilmar Falkenberg <hilmar.falkenberg@sap.com>
    
    * Update common/shared/src/test/java/app/coronawarn/server/common/shared/util/CwaStringTest.java
    
    * Business rules - overwrite on empty fetch
    
    * organize imports
    
    * util classes should not have public constructors
    
    this should increase the code coverage
    
    Co-authored-by: Hilmar Falkenberg <hilmar.falkenberg@sap.com>
    
    Co-authored-by: alexmoroianu <32899407+alexmoroianu@users.noreply.github.com>

---
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/dgc/integration/DigitalCovidCertificateInvalidTruststoreTest.java
@@ -0,0 +1,47 @@
+package app.coronawarn.server.services.distribution.dgc.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import app.coronawarn.server.common.persistence.exception.InvalidDiagnosisKeyException;
+import app.coronawarn.server.services.distribution.config.DistributionServiceConfig;
+import app.coronawarn.server.services.distribution.dgc.client.CloudDccFeignClientConfiguration;
+import app.coronawarn.server.services.distribution.dgc.client.CloudDccFeignHttpClientProvider;
+import app.coronawarn.server.services.distribution.dgc.client.DigitalCovidCertificateClient;
+import app.coronawarn.server.services.distribution.dgc.client.ProdDigitalCovidCertificateClient;
+import java.io.FileNotFoundException;
+import app.coronawarn.server.services.distribution.dgc.exception.FetchBusinessRulesException;
+import feign.RetryableException;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import javax.net.ssl.SSLHandshakeException;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {DistributionServiceConfig.class, ProdDigitalCovidCertificateClient.class,
+    CloudDccFeignClientConfiguration.class, CloudDccFeignHttpClientProvider.class},
+    initializers = ConfigDataApplicationContextInitializer.class)
+@ImportAutoConfiguration({FeignAutoConfiguration.class, HttpMessageConvertersAutoConfiguration.class})
+@ActiveProfiles("dcc-invalid-truststore")
+public class DigitalCovidCertificateInvalidTruststoreTest {
+
+  @Autowired
+  private DigitalCovidCertificateClient digitalCovidCertificateClient;
+
+  @Test
+  public void shouldNotEstablishSslConnection() {
+    Exception exception = Assert.assertThrows(FetchBusinessRulesException.class,
+        () -> digitalCovidCertificateClient.getRules());
+    assertThat(exception.getCause()).isInstanceOf(RetryableException.class);
+    assertThat(exception.getCause().getCause()).isInstanceOf(SSLHandshakeException.class);
+  }
+
+}
