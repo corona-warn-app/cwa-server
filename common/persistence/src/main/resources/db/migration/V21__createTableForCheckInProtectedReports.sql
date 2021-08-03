1cb96d151cac59867a785229ba0befa2f7bb0f9c
Gut Ioan <ioan.gut@sap.com>
2021-08-03 18:33:04 +0300

    Update V21__createTableForCheckInProtectedReports.sql

--- common/persistence/src/main/resources/db/migration/V21__createTableForCheckInProtectedReports.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE check_in_protected_reports (
-  id SERIAL PRIMARY KEY,
-  trace_location_id_hash BYTEA NOT NULL,
-  initialization_vector BYTEA NOT NULL,
-  encrypted_check_in_record BYTEA NOT NULL,
-  submission_timestamp BIGINT NOT NULL
-);
