66df983b077093b02d03531da3ca8eaf545f8ae7
alexmoroianu <32899407+alexmoroianu@users.noreply.github.com>
2021-08-10 16:11:49 +0300

    ADD - audit logger for response body. dcc and dsc. (#1526)
    * ADD - audit logger for response body. dcc and dsc.
    
    * let's spend a dedicated log-marker for this kiond of audit-logging
    
    * FIX - logger for DSC
    
    * Adapt the logging config to treat markers "AUDIT" accordingly
    
    Co-authored-by: Hilmar Falkenberg <hilmar.falkenberg@sap.com>
    Co-authored-by: Jeremias Rößler <jeremias.roessler@sap.com>

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/dgc/client/ProdDigitalCovidCertificateClient.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/dgc/client/ProdDigitalCovidCertificateClient.java
@@ -1,5 +1,6 @@
-
 package app.coronawarn.server.services.distribution.dgc.client;
+
+import static app.coronawarn.server.common.shared.util.SerializationUtils.stringifyObject;
 
 import app.coronawarn.server.services.distribution.dgc.BusinessRule;
 import app.coronawarn.server.services.distribution.dgc.BusinessRuleItem;
@@ -14,6 +15,8 @@ import java.util.function.BiFunction;
 import java.util.function.Supplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -27,6 +30,8 @@ import org.springframework.stereotype.Component;
 public class ProdDigitalCovidCertificateClient implements DigitalCovidCertificateClient {
 
   private static final Logger logger = LoggerFactory.getLogger(ProdDigitalCovidCertificateClient.class);
+  
+  public static final Marker AUDIT = MarkerFactory.getMarker("AUDIT"); 
 
   private final DigitalCovidCertificateFeignClient digitalCovidCertificateClient;
 
@@ -83,6 +88,7 @@ public class ProdDigitalCovidCertificateClient implements DigitalCovidCertificat
     logger.debug("Get " + fetchEntityName + " from DCC");
     try {
       ResponseEntity<T> response = responseSupplier.get();
+      logger.info(AUDIT,  "{} - {}", fetchEntityName, stringifyObject(response.getBody()));
       return response.getBody();
     } catch (Exception e) {
       throw exceptionConverter.apply(fetchEntityName + " could not be fetched because of: ", e);
