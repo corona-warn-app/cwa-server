499873859f20850a606000b89d4aaacb5e9ca2ba
FelixRottler <fero429@gmail.com>
2021-08-23 08:47:16 +0200

    fix: remove method due to merge

--- services/submission/src/test/java/app/coronawarn/server/services/submission/integration/DataHelpers.java
+++ services/submission/src/test/java/app/coronawarn/server/services/submission/integration/DataHelpers.java
@@ -1,5 +1,11 @@
 package app.coronawarn.server.services.submission.integration;
 
+import static app.coronawarn.server.common.persistence.service.utils.checkins.CheckinsDateSpecification.TEN_MINUTE_INTERVAL_DERIVATION;
+import static app.coronawarn.server.common.shared.util.HashUtils.generateSecureRandomByteArrayData;
+import static app.coronawarn.server.services.submission.SubmissionPayloadGenerator.buildTemporaryExposureKeyWithoutMultiplying;
+import static app.coronawarn.server.services.submission.SubmissionPayloadGenerator.buildTemporaryExposureKeys;
+import static java.time.ZoneOffset.UTC;
+
 import app.coronawarn.server.common.protocols.external.exposurenotification.ReportType;
 import app.coronawarn.server.common.protocols.external.exposurenotification.TemporaryExposureKey;
 import app.coronawarn.server.common.protocols.internal.SubmissionPayload;
@@ -12,12 +18,6 @@ import com.google.protobuf.ByteString;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.List;
-
-import static app.coronawarn.server.common.persistence.service.utils.checkins.CheckinsDateSpecification.TEN_MINUTE_INTERVAL_DERIVATION;
-import static app.coronawarn.server.common.shared.util.HashUtils.generateSecureRandomByteArrayData;
-import static app.coronawarn.server.services.submission.SubmissionPayloadGenerator.buildTemporaryExposureKeyWithoutMultiplying;
-import static app.coronawarn.server.services.submission.SubmissionPayloadGenerator.buildTemporaryExposureKeys;
-import static java.time.ZoneOffset.UTC;
 
 public class DataHelpers {
 
@@ -61,13 +61,7 @@ public class DataHelpers {
         ByteString.copyFrom(generateSecureRandomByteArrayData(16)),
         ByteString.copyFrom(generateSecureRandomByteArrayData(16)),
         ByteString.copyFrom(generateSecureRandomByteArrayData(32))
-        );
-  }
-
-  public static CheckInProtectedReport buildDefaultEncryptedCheckIn(byte[] locationIdHash) {
-    return buildEncryptedCheckIn(ByteString.copyFrom(generateSecureRandomByteArrayData(16)),
-        ByteString.copyFrom(generateSecureRandomByteArrayData(16)),
-        ByteString.copyFrom(locationIdHash));
+    );
   }
 
   public static CheckIn buildCheckIn(int startInterval, int endInterval, int transmissionRiskLevel,
