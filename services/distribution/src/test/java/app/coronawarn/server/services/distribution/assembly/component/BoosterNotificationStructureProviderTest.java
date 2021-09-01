52ad614a1b514ba4a39b62113a5bd23235c01ab8
Gut Ioan <ioan.gut@sap.com>
2021-09-01 15:12:31 +0300

    Use @autowired to inject the boosterNotificationStructureProvider in Tests

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/component/BoosterNotificationStructureProviderTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/component/BoosterNotificationStructureProviderTest.java
@@ -1,46 +1,40 @@
 package app.coronawarn.server.services.distribution.assembly.component;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import app.coronawarn.server.common.shared.collection.ImmutableStack;
 import app.coronawarn.server.services.distribution.assembly.structure.Writable;
 import app.coronawarn.server.services.distribution.assembly.structure.WritableOnDisk;
-import app.coronawarn.server.services.distribution.assembly.structure.directory.Directory;
-import app.coronawarn.server.services.distribution.assembly.structure.directory.DirectoryOnDisk;
+import app.coronawarn.server.services.distribution.assembly.structure.archive.Archive;
 import app.coronawarn.server.services.distribution.config.DistributionServiceConfig;
 import app.coronawarn.server.services.distribution.dgc.DigitalGreenCertificateToCborMapping;
 import app.coronawarn.server.services.distribution.dgc.client.DigitalCovidCertificateClient;
-import app.coronawarn.server.services.distribution.dgc.client.ProdDigitalCovidCertificateClient;
 import app.coronawarn.server.services.distribution.dgc.dsc.DigitalSigningCertificatesClient;
-import app.coronawarn.server.services.distribution.dgc.exception.FetchBusinessRulesException;
-import org.junit.Rule;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.rules.TemporaryFolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 @EnableConfigurationProperties(value = {DistributionServiceConfig.class})
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(
     classes = {DigitalGreenCertificateToCborMapping.class,
-        CryptoProvider.class, DistributionServiceConfig.class, ProdDigitalCovidCertificateClient.class,
-        DigitalSigningCertificatesClient.class},
+        CryptoProvider.class, DistributionServiceConfig.class,
+        DigitalSigningCertificatesClient.class, DigitalCovidCertificateClient.class,
+        BoosterNotificationStructureProvider.class},
     initializers = ConfigDataApplicationContextInitializer.class)
+@ActiveProfiles({"fake-dcc-client", "fake-dsc-client"})
 public class BoosterNotificationStructureProviderTest {
-
-  private static final String PARENT_TEST_FOLDER = "parent";
 
   @Autowired
   DistributionServiceConfig distributionServiceConfig;
@@ -52,40 +46,24 @@ public class BoosterNotificationStructureProviderTest {
   DigitalGreenCertificateToCborMapping dgcToCborMappingMock;
 
   @MockBean
-  OutputDirectoryProvider outputDirectoryProvider;
-
-  @MockBean
   DigitalCovidCertificateClient digitalCovidCertificateClient;
 
   @MockBean
   DigitalSigningCertificatesClient digitalSigningCertificatesClient;
 
-  @Rule
-  TemporaryFolder testOutputFolder = new TemporaryFolder();
-
-  @BeforeEach
-  public void setup() throws IOException {
-    // create a specific test folder for later assertions of structures.
-    testOutputFolder.create();
-    File outputDirectory = testOutputFolder.newFolder(PARENT_TEST_FOLDER);
-    Directory<WritableOnDisk> testDirectory = new DirectoryOnDisk(outputDirectory);
-    when(outputDirectoryProvider.getDirectory()).thenReturn(testDirectory);
-  }
+  @Autowired
+  BoosterNotificationStructureProvider underTest;
 
   @Test
-  void should_contain_booster_notification_rules() throws FetchBusinessRulesException {
-    when(digitalCovidCertificateClient.getBnRules()).thenThrow(FetchBusinessRulesException.class);
+  void should_create_correct_file_structure_for_booster_notification_business_rules() {
+    Writable<WritableOnDisk> boosterNotificationRules = underTest.getBoosterNotificationRules();
+    boosterNotificationRules.prepare(new ImmutableStack<>());
 
-    Writable<WritableOnDisk> boosterNotification = getBoosterNotificationStructure();
+    assertEquals("booster-notification-rules", boosterNotificationRules.getName());
 
-    assertThat(boosterNotification.getName()).isEqualTo("booster-notification-rules");
-  }
-
-  private Writable<WritableOnDisk> getBoosterNotificationStructure() {
-    BoosterNotificationStructureProvider underTest = new BoosterNotificationStructureProvider(
-        distributionServiceConfig, cryptoProvider, dgcToCborMappingMock);
-    Writable<WritableOnDisk> digitalGreenCertificates = underTest.getBoosterNotificationRules();
-
-    return digitalGreenCertificates;
+    Collection<String> archiveContent;
+    archiveContent = ((Archive<WritableOnDisk>) boosterNotificationRules).getWritables().stream()
+        .map(Writable::getName).collect(Collectors.toList());
+    assertThat(archiveContent).containsAll(Set.of("export.bin", "export.sig"));
   }
 }
