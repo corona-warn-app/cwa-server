391d21085aac9b3c77ae6ec5bc10408af8d79b3b
ioangut <67064882+ioangut@users.noreply.github.com>
2021-11-05 16:56:40 +0200

    Add block list for DCCs - HF (#1602)
    * Add block list for DCCs
    
    * add test and change default value

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/appconfig/ApplicationConfigurationV2MainFileTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/appconfig/ApplicationConfigurationV2MainFileTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import app.coronawarn.server.common.persistence.domain.config.PreDistributionTrlValueMappingProvider;
 import app.coronawarn.server.common.protocols.internal.v2.ApplicationConfigurationAndroid;
 import app.coronawarn.server.common.protocols.internal.v2.ApplicationConfigurationIOS;
+import app.coronawarn.server.common.protocols.internal.v2.DGCBlocklistParameters;
 import app.coronawarn.server.common.protocols.internal.v2.DGCTestCertificateParameters;
 import app.coronawarn.server.services.distribution.config.DistributionServiceConfig;
 import org.junit.jupiter.api.Nested;
@@ -64,6 +65,24 @@ class ApplicationConfigurationV2MainFileTest {
       assertThat(testCertificateParametersIos.getWaitAfterPublicKeyRegistrationInSeconds()).isEqualTo(10);
       assertThat(testCertificateParametersIos.getWaitForRetryInSeconds()).isEqualTo(10);
       assertThat(testCertificateParametersIos.getWaitForRetryInSeconds()).isEqualTo(10);
+
+      /* given: blocked-uvci-chunks: ${DGC_BLOCKED_UVCI_CHUNKS:[{"indices":[0],
+       "hash":"9B09CAFEC0A6808411C348880C9C2D920646DFB980B5C959DC6EBF8A19B98120",
+       "validFrom":1636040446},{"indices":[0,2],
+       "hash":"7D5D5B336E903086D64D1207EC6E957A4B1301026699011026F84A5156317C2B","validFrom":1951642095}]},
+       in test/application.yaml
+
+       when: one entry is not valid
+
+       then: should return only one BlockedUvciChunk
+       */
+      final DGCBlocklistParameters dgcBlocklistParameters =
+          applicationConfigurationAndroid.getDgcParameters().getBlockListParameters();
+      assertThat(dgcBlocklistParameters.getBlockedUvciChunksCount()).isEqualTo(1);
+
+      final DGCBlocklistParameters dgcBlocklistParametersIos =
+          applicationConfigurationIos.getDgcParameters().getBlockListParameters();
+      assertThat(dgcBlocklistParametersIos.getBlockedUvciChunksCount()).isEqualTo(1);
     }
   }
 }
