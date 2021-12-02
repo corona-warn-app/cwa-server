de5e9ede8fcc9619c757a009f079b33620eb48e2
Cristian Mihai Manole <cristian.manole@sap.com>
2021-12-02 22:19:03 +0200

    improve coverage

--- common/persistence/src/test/java/app/coronawarn/server/common/persistence/service/TraceTimeIntervalWarningServiceTest.java
+++ common/persistence/src/test/java/app/coronawarn/server/common/persistence/service/TraceTimeIntervalWarningServiceTest.java
@@ -26,6 +26,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -114,6 +115,14 @@ class TraceTimeIntervalWarningServiceTest {
     String s = new String(encode);
 
     assertEquals("0f37dac11d1b8118ea0b44303400faa5e3b876da9d758058b5ff7dc2e5da8230", s);
+  }
+
+  @Test
+  void testHashingString() {
+    byte[] result = HashUtils.byteStringDigest("abcd", SHA_256);
+    final byte[] encode = Hex.encode(result);
+    String s = new String(encode);
+    Assertions.assertThat(s).isEqualTo("88d4266fd4e6338d13b845fcf289579d209c897823b9217da3e161936f031589");
   }
 
   @DisplayName("Assert a positive retention period is accepted.")
--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/dgc/DccValidationAllowListSignatureTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/dgc/DccValidationAllowListSignatureTest.java
@@ -1,8 +1,17 @@
 package app.coronawarn.server.services.distribution.dgc;
 
+import static app.coronawarn.server.common.shared.util.SecurityUtils.ecdsaSignatureVerification;
+import static app.coronawarn.server.common.shared.util.SecurityUtils.getPublicKeyFromString;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
 import app.coronawarn.server.services.distribution.config.DistributionServiceConfig;
 import app.coronawarn.server.services.distribution.config.DistributionServiceConfig.AllowList;
 import app.coronawarn.server.services.distribution.dgc.dsc.DigitalCovidValidationCertificateToProtobufMapping;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,18 +19,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
-import java.security.SignatureException;
-import java.security.spec.InvalidKeySpecException;
-
-import static app.coronawarn.server.common.shared.util.SecurityUtils.ecdsaSignatureVerification;
-import static app.coronawarn.server.common.shared.util.SecurityUtils.getPublicKeyFromString;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
 
 @EnableConfigurationProperties(value = DistributionServiceConfig.class)
 @ExtendWith(SpringExtension.class)
