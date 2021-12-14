4660f9a03b454b87239511dbc1b4e97299b23d79
ioangut <67064882+ioangut@users.noreply.github.com>
2021-12-14 11:19:27 +0200

    Merge2.15main (#1638)
    * release 2.15
    
    * Fix that the value is hex-encoded, not utf-8 (#1619)
    
    * Feature/verify signature allowlist (#1620)
    
    * Add signKey
    
    * Change type to ValidationServicePublicKeyJWK
    
    * refactor: remove signKey since no longer in spec
    
    * Protocol Buffer update for release 2.15.x at Mon Nov 29 09:51:49 UTC 2021 (#1617)
    
    * Update from https://github.com/corona-warn-app/cwa-protocol-buffers/commit/68d8d72efc8cd00a81740fc2d14f689aec2e9302
    
    * Update from https://github.com/corona-warn-app/cwa-protocol-buffers/commit/daa6ab48b1e6551d13118eb025cab030500d335f
    
    * feat: preview of certificate pinning
    
    * feat!: add certificate pinning and add ServiceProviderAllowItem's to final protobuf
    
    * feat!: add certificate pinning and add ServiceProviderAllowItem's to final protobuf
    
    * refactor: added tests and small refactoring to handle errors in case obtaining service provider allowlist fails
    
    * refactor: extract compare criterion to end of try-catch to make code more precise
    
    * update exception
    
    * fix build
    
    * fix tests
    
    * ,
    
    * ,
    
    * improve coverage
    
    * ,
    
    * ,
    
    * improve coverage
    
    * return empty dto if invalid fingerprint
    
    * improve coverage
    
    * Fix checkstyle and add one more test
    
    * Adjust the test to check invalid fingerprint
    
    Co-authored-by: Alice Bilc <36674707+AliceBilc@users.noreply.github.com>
    Co-authored-by: CWA Technical User <67319337+Corona-Warn-App-Technical-User@users.noreply.github.com>
    Co-authored-by: ioangut <67064882+ioangut@users.noreply.github.com>
    Co-authored-by: Cristian Mihai Manole <cristian.manole@sap.com>
    Co-authored-by: Gut Ioan <ioan.gut@sap.com>
    
    * Handle empty response for validation service (#1625)
    
    * Handle empty response for validation service
    
    * Update DigitalCovidValidationCertificateToProtobufMapping.java
    
    * Adjust validation services from new specs (#1626)
    
    * Adjust validation services from new specs
    
    * Update application-allow-list-invalid.yaml
    
    * enhance logging (#1627)
    
    * Feature/app feature changes (#1630)
    
    * Add the config changes as described here: https://github.com/corona-warn-app/cwa-app-tech-spec/pull/144/files
    
    * Add the config changes as described here: https://github.com/corona-warn-app/cwa-app-tech-spec/pull/145/files
    
    * Fix blank
    
    * Delete space (#1631)
    
    * Fix bracket (#1635)
    
    * Delete min and max restrictions for app feature values (#1636)
    
    * upgrade apache logging log4j dependency (#1637)
    
    * Update maven.config
    
    * improve coverage
    
    * ,
    
    * ,
    
    * ,
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * ,
    
    Co-authored-by: Alice Bilc <36674707+AliceBilc@users.noreply.github.com>
    Co-authored-by: Jeremias Rößler <jeremias.roessler@sap.com>
    Co-authored-by: FelixRottler <72494737+FelixRottler@users.noreply.github.com>
    Co-authored-by: CWA Technical User <67319337+Corona-Warn-App-Technical-User@users.noreply.github.com>
    Co-authored-by: Cristian Mihai Manole <cristian.manole@sap.com>

--- common/shared/src/test/java/app/coronawarn/server/common/shared/util/HashUtilsTest.java
+++ common/shared/src/test/java/app/coronawarn/server/common/shared/util/HashUtilsTest.java
@@ -1,14 +1,19 @@
 package app.coronawarn.server.common.shared.util;
 
+import static app.coronawarn.server.common.shared.util.HashUtils.Algorithms.SHA_256;
 import static app.coronawarn.server.common.shared.util.HashUtils.byteStringDigest;
 import static app.coronawarn.server.common.shared.util.HashUtils.generateSecureRandomByteArrayData;
 import static app.coronawarn.server.common.shared.util.HashUtils.md5DigestAsHex;
-import static app.coronawarn.server.common.shared.util.HashUtils.Algorithms.SHA_256;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 
 import com.google.protobuf.ByteString;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 class HashUtilsTest {
 
@@ -20,7 +25,7 @@ class HashUtilsTest {
   void testMd5DigestAsHex() {
     String hash = md5DigestAsHex(HEX_DIGESTED_ORIGIN);
     assertThat(hash).hasSize(32);
-    assertEquals(hash, HEX_DIGESTED);
+    assertEquals(HEX_DIGESTED, hash);
   }
 
   @Test
@@ -34,8 +39,23 @@ class HashUtilsTest {
   }
 
   @Test
-  void testByteStringDigest() {
+  void testByteStringDigest1() {
+    byte[] hash = byteStringDigest(TEST_STRING, SHA_256);
+    assertThat(hash).hasSize(32);
+  }
+  @Test
+  void testByteStringDigest2() {
     byte[] hash = byteStringDigest(ByteString.copyFromUtf8(TEST_STRING), SHA_256);
     assertThat(hash).hasSize(32);
+  }
+
+  @Test
+  void testInvalidAlgorithm() {
+    try (MockedStatic<MessageDigest> utilities = Mockito.mockStatic(MessageDigest.class)) {
+      utilities.when(() -> MessageDigest.getInstance(any())).thenThrow(new NoSuchAlgorithmException());
+
+      byte[] result = byteStringDigest(new byte[] {0,0}, SHA_256);
+      assertEquals(0, result.length);
+    }
   }
 }
--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/dgc/DigitalGreenCertificateJsonToCborUnitTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/dgc/DigitalGreenCertificateJsonToCborUnitTest.java
@@ -99,5 +99,4 @@ class DigitalGreenCertificateJsonToCborUnitTest {
 
     return businessRule;
   }
-
 }
