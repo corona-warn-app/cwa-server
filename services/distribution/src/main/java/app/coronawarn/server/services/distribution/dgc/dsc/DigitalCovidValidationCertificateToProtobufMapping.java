c8349db429caa7979abe4adb06f5cb9546b00829
Gut Ioan <ioan.gut@sap.com>
2021-12-16 18:14:08 +0200

    Update CloseableHttp requests handling

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/dgc/dsc/DigitalCovidValidationCertificateToProtobufMapping.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/dgc/dsc/DigitalCovidValidationCertificateToProtobufMapping.java
@@ -184,8 +184,12 @@ public class DigitalCovidValidationCertificateToProtobufMapping {
             fingerPrintToCompare))
         .build()) {
       HttpGet getMethod = new HttpGet(serviceProviderAllowlistEndpoint);
-      final HttpEntity httpEntity = executeRequest(httpClient, getMethod);
-      return buildServiceProviderDto(objectMapper, httpEntity);
+      try (CloseableHttpResponse response = httpClient.execute(getMethod)) {
+        return buildServiceProviderDto(objectMapper, response.getEntity());
+      } catch (Exception e) {
+        LOGGER.warn("Request to obtain the service providers failed: ", e);
+        throw new InvalidFingerprintException();
+      }
     } catch (Exception e) {
       LOGGER.warn(e.getMessage(), e);
       ServiceProviderDto dto = new ServiceProviderDto();
@@ -236,15 +240,5 @@ public class DigitalCovidValidationCertificateToProtobufMapping {
       LOGGER.error("Certificate Pinning failed: certificate could not be encoded.");
     }
     return fingerprint.map(it -> it.equals(fingerPrintToCompare.toLowerCase())).orElse(false);
-  }
-
-  private HttpEntity executeRequest(CloseableHttpClient httpClient, HttpGet getMethod)
-      throws InvalidFingerprintException {
-    try (final CloseableHttpResponse response = httpClient.execute(getMethod)) {
-      return response.getEntity();
-    } catch (Exception e) {
-      LOGGER.warn("Request to obtain the service providers failed: ", e);
-      throw new InvalidFingerprintException();
-    }
   }
 }
