ff7a00250b8e1dc5a1bfe7cc97c533ee3da8b11b
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2022-01-13 13:00:25 +0100

    filter TEKs with TRL (1 or 2) before distributing them (#1692) (#1696)
    * filter TEKs with TRL (1 or 2) before distributing them (#1692)
    
    * hardcoded filter on lower than 3
    
    * Update maven.config
    
    * Fix code smell
    
    * Add test for filtering out the keys with TRL < 3
    
    * import static Assertions
    
    Co-authored-by: Gut Ioan <ioan.gut@sap.com>
    
    * Add configurable environment variable.
    
    Co-authored-by: Gut Ioan <ioan.gut@sap.com>

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/component/DiagnosisKeysStructureProvider.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/component/DiagnosisKeysStructureProvider.java
@@ -51,6 +51,10 @@ public class DiagnosisKeysStructureProvider {
   public Directory<WritableOnDisk> getDiagnosisKeys() {
     logger.debug("Querying diagnosis keys from the database...");
     Collection<DiagnosisKey> diagnosisKeys = diagnosisKeyService.getDiagnosisKeys();
+
+    diagnosisKeys.removeIf(
+        key -> key.getTransmissionRiskLevel() < distributionServiceConfig.getMinimumTrlValueAllowed());
+
     diagnosisKeyBundler.setDiagnosisKeys(enfParameterEncoder.adaptKeys(diagnosisKeys), getCurrentUtcHour());
     return new DiagnosisKeysDirectory(diagnosisKeyBundler, cryptoProvider, distributionServiceConfig);
   }
