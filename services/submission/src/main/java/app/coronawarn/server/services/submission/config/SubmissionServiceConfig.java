0c41b849cf14fbddfef95c7a67ea1f0811f1204e
Alice Bilc <36674707+AliceBilc@users.noreply.github.com>
2022-01-14 11:04:08 +0200

    Fix after review - Deprecated since 2.8 on unencryptedCheckinsEnabled

--- services/submission/src/main/java/app/coronawarn/server/services/submission/config/SubmissionServiceConfig.java
+++ services/submission/src/main/java/app/coronawarn/server/services/submission/config/SubmissionServiceConfig.java
@@ -66,7 +66,7 @@ public class SubmissionServiceConfig {
    *
    * @deprecated should be removed when false
    */
-  @Deprecated(since = "12.08.21", forRemoval = false)
+  @Deprecated(since = "2.8", forRemoval = true)
   private Boolean unencryptedCheckinsEnabled;
 
   @Autowired
@@ -217,7 +217,7 @@ public class SubmissionServiceConfig {
    *
    * @deprecated should be removed when false
    */
-  @Deprecated(since = "12.08.21", forRemoval = false)
+  @Deprecated(since = "2.8", forRemoval = true)
   public Boolean isUnencryptedCheckinsEnabled() {
     return unencryptedCheckinsEnabled == null ? false : unencryptedCheckinsEnabled;
   }
