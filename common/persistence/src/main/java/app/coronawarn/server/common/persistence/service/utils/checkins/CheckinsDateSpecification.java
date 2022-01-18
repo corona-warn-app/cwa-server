3c01e744c77b6716224bf05e43fd2a633ee30764
Gut Ioan <ioan.gut@sap.com>
2022-01-18 16:03:24 +0200

    Use predefined LongFunction

--- common/persistence/src/main/java/app/coronawarn/server/common/persistence/service/utils/checkins/CheckinsDateSpecification.java
+++ common/persistence/src/main/java/app/coronawarn/server/common/persistence/service/utils/checkins/CheckinsDateSpecification.java
@@ -1,6 +1,6 @@
 package app.coronawarn.server.common.persistence.service.utils.checkins;
 
-import java.util.function.Function;
+import java.util.function.LongFunction;
 
 public final class CheckinsDateSpecification {
 
@@ -8,7 +8,7 @@ public final class CheckinsDateSpecification {
    * Derivation function that requires a UNIX timestamp (in seconds) and returns the corresponding
    * 10-minute interval since UNIX epoch.
    */
-  public static final Function<Long, Integer> TEN_MINUTE_INTERVAL_DERIVATION = (unixTimestamp) -> {
+  public static final LongFunction<Integer> TEN_MINUTE_INTERVAL_DERIVATION = unixTimestamp -> {
     if (unixTimestamp < 0) {
       throw new IllegalArgumentException(
           "Ten minute interval cannot be computed from a negative timestamp");
@@ -20,7 +20,7 @@ public final class CheckinsDateSpecification {
    * Derivation function that requires a UNIX timestamp (in seconds) and returns the corresponding
    * hour since UNIX epoch.
    */
-  public static final Function<Long, Integer> HOUR_SINCE_EPOCH_DERIVATION = (unixTimestamp) -> {
+    public static final LongFunction<Integer> HOUR_SINCE_EPOCH_DERIVATION = unixTimestamp -> {
     if (unixTimestamp < 0) {
       throw new IllegalArgumentException(
           "Hour since epoch cannot be computed from a negative timestamp");
