73c8b070402020a96c6273313a0fbe69add75491
Gut Ioan <ioan.gut@sap.com>
2022-03-29 15:49:09 +0300

    Create V23_createDccRevocationTables.sql

---
+++ common/persistence/src/main/resources/db/specific/postgresql/V23_createDccRevocationTables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE revocation_entry (
+    kid bytea PRIMARY KEY,
+    type bytea NOT NULL,
+    hash bytea NOT NULL,
+    x bytea NOT NULL,
+    y bytea NOT NULL
+);
+
+CREATE TABLE revocation_etag (
+    path varchar(255) PRIMARY KEY,
+    etag varchar(255) NOT NULL
+);
+
+GRANT ALL ON TABLE revocation_entry TO "cwa_distribution";
+GRANT USAGE, SELECT ON SEQUENCE revocation_entry_id_seq TO "cwa_distribution";
+
+GRANT SELECT,INSERT, DELETE ON TABLE revocation_etag TO "cwa_distribution";
+GRANT USAGE, SELECT ON SEQUENCE revocation_etag_id_seq TO "cwa_distribution";
