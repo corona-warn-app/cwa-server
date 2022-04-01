7ba12503a9a8c39e7529f4e1ae2a46cb5a6fae63
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2022-04-01 12:38:34 +0200

    fix checkstyle issue

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/dcc/CloudDccRevocationFeignClientConfiguration.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/dcc/CloudDccRevocationFeignClientConfiguration.java
@@ -3,13 +3,13 @@ package app.coronawarn.server.services.distribution.dcc;
 import app.coronawarn.server.services.distribution.config.DistributionServiceConfig;
 import feign.Client;
 import feign.Retryer;
+import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableFeignClients
