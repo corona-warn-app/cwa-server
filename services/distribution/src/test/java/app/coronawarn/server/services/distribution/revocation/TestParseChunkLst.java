d305c8f3d94f0639e29cf7d5b3a9096997f315fb
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2022-04-01 15:35:00 +0200

    access JsonTokenId.ID_EMBEDDED_OBJECT

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/revocation/TestParseChunkLst.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/revocation/TestParseChunkLst.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertNotNull;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonTokenId;
 import com.fasterxml.jackson.dataformat.cbor.CBORFactory;
 import com.fasterxml.jackson.dataformat.cbor.CBORParser;
 import java.io.IOException;
@@ -23,10 +24,23 @@ public class TestParseChunkLst {
     JsonFactory jsonFactory = new JsonFactory();
     StringWriter stringWriter = new StringWriter();
     JsonGenerator jsonGenerator = jsonFactory.createGenerator(stringWriter);
+    StringWriter stringWriter2 = new StringWriter();
+    JsonGenerator jsonGenerator2 = jsonFactory.createGenerator(stringWriter);
+    int i = 0;
     while (cborParser.nextToken() != null) {
+      if (JsonTokenId.ID_EMBEDDED_OBJECT == cborParser.getCurrentToken().id() && i < 2 /* 3rd ebbeded object is the signature */) {
+        byte[] obj = (byte[]) cborParser.getEmbeddedObject();
+        CBORParser payload = cborFactory.createParser(obj);
+        while (payload.nextToken() != null) {
+          jsonGenerator2.copyCurrentEvent(payload);
+        }
+        i++;
+      }
       jsonGenerator.copyCurrentEvent(cborParser);
     }
     jsonGenerator.flush();
+    jsonGenerator2.flush();
     System.out.println(stringWriter.toString());
+    System.out.println(stringWriter2.toString());
   }
 }
