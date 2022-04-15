42661196854b5931d64262ead4b4e7dc564959e5
Gut Ioan <ioan.gut@sap.com>
2022-04-15 09:29:56 +0300

    Update FetchDccListException.java

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/dcc/FetchDccListException.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/dcc/FetchDccListException.java
@@ -2,10 +2,6 @@ package app.coronawarn.server.services.distribution.dcc;
 
 public class FetchDccListException extends Exception {
 
-  public FetchDccListException(String message) {
-    super(message);
-  }
-
   public FetchDccListException(String message, Throwable cause) {
     super(message, cause);
   }
