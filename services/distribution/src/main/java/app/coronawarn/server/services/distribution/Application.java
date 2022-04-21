16a70bd478608ab6d47e9d3bd5ac89bb43d1a637
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2022-04-21 14:26:11 +0200

    revert changes

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/Application.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/Application.java
@@ -26,9 +26,9 @@ import org.springframework.validation.Validator;
 @SpringBootApplication
 @EnableJdbcRepositories(basePackages = "app.coronawarn.server.common.persistence")
 @EntityScan(basePackages = "app.coronawarn.server.common.persistence")
-@ComponentScan({ "app.coronawarn.server.common.persistence", "app.coronawarn.server.services.distribution",
-    "app.coronawarn.server.common.federation.client.hostname" })
-@EnableConfigurationProperties({ DistributionServiceConfig.class })
+@ComponentScan({"app.coronawarn.server.common.persistence", "app.coronawarn.server.services.distribution",
+    "app.coronawarn.server.common.federation.client.hostname"})
+@EnableConfigurationProperties({DistributionServiceConfig.class})
 public class Application implements EnvironmentAware {
 
   private static final Logger logger = LoggerFactory.getLogger(Application.class);
@@ -44,7 +44,6 @@ public class Application implements EnvironmentAware {
 
   /**
    * Terminates this application with exit code 1 (general error).
-   * 
    * @param appContext type ApplicationContext
    */
   public static void killApplication(ApplicationContext appContext) {
