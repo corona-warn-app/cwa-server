b5eaddd970279a328f4d64febb15429eb0024467
ioangut <67064882+ioangut@users.noreply.github.com>
2022-04-27 11:47:36 +0300

    Payload Signature verification (#1811)

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/dcc/decode/DccRevocationListDecoder.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/dcc/decode/DccRevocationListDecoder.java
@@ -1,9 +1,16 @@
 package app.coronawarn.server.services.distribution.dcc.decode;
 
+import static app.coronawarn.server.common.shared.util.SecurityUtils.ecdsaSignatureVerification;
+import static app.coronawarn.server.common.shared.util.SecurityUtils.getEcdsaEncodeFromSignature;
+import static app.coronawarn.server.common.shared.util.SecurityUtils.getPublicKeyFromString;
+import static app.coronawarn.server.common.shared.util.SerializationUtils.cborEncode;
 import static app.coronawarn.server.common.shared.util.SerializationUtils.jsonExtractCosePayload;
 
 import app.coronawarn.server.common.persistence.domain.RevocationEntry;
+import app.coronawarn.server.services.distribution.config.DistributionServiceConfig;
 import app.coronawarn.server.services.distribution.dgc.exception.DscListDecodeException;
+import com.upokecenter.cbor.CBORObject;
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,7 +20,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class DccRevocationListDecoder {
 
-  public DccRevocationListDecoder() {
+  private DistributionServiceConfig distributionServiceConfig;
+
+  public DccRevocationListDecoder(DistributionServiceConfig distributionServiceConfig) {
+    this.distributionServiceConfig = distributionServiceConfig;
   }
 
   /**
@@ -28,6 +38,16 @@ public class DccRevocationListDecoder {
   public List<RevocationEntry> decode(byte[] data) throws DccRevocationListDecodeException {
     ArrayList<RevocationEntry> revocationEntries = new ArrayList<>();
     try {
+
+      PublicKey publicKey = getPublicKeyFromString(
+          distributionServiceConfig.getDccRevocation().getClient().getPublicKey());
+      CBORObject cborObject = CBORObject.DecodeFromBytes(data);
+      byte[] ecdsaSignature = getEcdsaEncodeFromSignature(cborObject.get(3).GetByteString());
+      ArrayList<Object> signedPayload = new ArrayList<>(Arrays.asList("Signature1", cborObject.get(0).GetByteString(),
+          new byte[0], cborObject.get(2).GetByteString()));
+
+      ecdsaSignatureVerification(ecdsaSignature, publicKey, cborEncode(signedPayload));
+
       Map<byte[], List<byte[]>> jsonPayload = jsonExtractCosePayload(data);
 
       jsonPayload.forEach((keyAndType, values) -> {
