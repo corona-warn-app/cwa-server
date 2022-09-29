71bf71f743da46beb5156a08c407f2d45523002e
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2022-09-29 10:58:45 +0200

    !match.isPresent() -> match.isEmpty()

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/dgc/client/JsonSchemaMappingLookup.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/dgc/client/JsonSchemaMappingLookup.java
@@ -2,7 +2,6 @@ package app.coronawarn.server.services.distribution.dgc.client;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,8 +11,8 @@ public class JsonSchemaMappingLookup {
   public static final String JSON_SCHEMA_PATH = "dgc";
   public static final String CCL_JSON_SCHEMA = JSON_SCHEMA_PATH + "/ccl-configuration.json";
   public static final String DCC_VALIDATION_RULE_JSON_CLASSPATH = JSON_SCHEMA_PATH + "/dcc-validation-rule.json";
-  public static final String ALLOW_LIST_JSON_CLASSPATH =
-      JSON_SCHEMA_PATH + "/dcc-validation-service-allowlist-rule.json";
+  public static final String ALLOW_LIST_JSON_CLASSPATH = JSON_SCHEMA_PATH
+      + "/dcc-validation-service-allowlist-rule.json";
 
   static final Map<String, String> businessObjectToJsonSchema = new HashMap<>();
 
@@ -23,7 +22,7 @@ public class JsonSchemaMappingLookup {
    * Constructor to initialize the schema mappings.
    */
   public JsonSchemaMappingLookup() {
-    //initialize all mappings from URLs to the respective schema
+    // initialize all mappings from URLs to the respective schema
     businessObjectToJsonSchema.put("/rules", DCC_VALIDATION_RULE_JSON_CLASSPATH);
     businessObjectToJsonSchema.put("/bnrules", DCC_VALIDATION_RULE_JSON_CLASSPATH);
     businessObjectToJsonSchema.put("/cclrules", CCL_JSON_SCHEMA);
@@ -34,26 +33,24 @@ public class JsonSchemaMappingLookup {
    *
    * @return The path to the schema matching the request URL.
    */
-  public String getSchemaPath(String requestUrl) {
-    //find out if we have a known route, e.g. /rules, /bnrules, etc.
-    Optional<String> match = businessObjectToJsonSchema.keySet().stream().filter(requestUrl::contains).findAny();
+  public String getSchemaPath(final String requestUrl) {
+    // find out if we have a known route, e.g. /rules, /bnrules, etc.
+    final Optional<String> match = businessObjectToJsonSchema.keySet().stream().filter(requestUrl::contains).findAny();
 
-    if (!match.isPresent()) {
+    if (match.isEmpty()) {
       logger.debug("Schema validation not configured for: {}", requestUrl);
       return null; // the request has not mapping, meaning there is no schema for this request URL's payload
     }
 
-    String route = match.get();
-
+    final String route = match.get();
     // find out if there are trailing elements after the base mapping (e.g. rules/<hash>). If so, we validate.
     if (requestUrl.lastIndexOf(route) >= requestUrl.lastIndexOf("/")) {
       // this means we have no hash or any other trailing path components after the route. We are not validating this.
       // The payloads we are interested in are always qualified with a hash, a country, etc.
       return null;
     }
-    //since there is currently only one schema per route, we can take the route as a key for the schema
-    //e.g. if we have /rules/<something>, we use the same schema, no matter what <something> is.
+    // since there is currently only one schema per route, we can take the route as a key for the schema
+    // e.g. if we have /rules/<something>, we use the same schema, no matter what <something> is.
     return businessObjectToJsonSchema.get(route);
   }
-
 }
