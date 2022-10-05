e0e9ecb0e1111dbdeaa3786202337b907150a994
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2022-10-05 19:30:17 +0200

    coverage

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/runner/RetentionPolicyDccRevocationTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/runner/RetentionPolicyDccRevocationTest.java
@@ -12,11 +12,9 @@ import app.coronawarn.server.services.distribution.config.DistributionServiceCon
 import app.coronawarn.server.services.distribution.dcc.DccRevocationClient;
 import app.coronawarn.server.services.distribution.dcc.FetchDccListException;
 import app.coronawarn.server.services.distribution.objectstore.S3RetentionPolicy;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -25,9 +23,12 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@EnableConfigurationProperties(value = DistributionServiceConfig.class)
+@EnableConfigurationProperties(
+    value = DistributionServiceConfig.class)
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = { RevRetentionPolicy.class }, initializers = ConfigDataApplicationContextInitializer.class)
+@ContextConfiguration(
+    classes = { RevRetentionPolicy.class },
+    initializers = ConfigDataApplicationContextInitializer.class)
 @ActiveProfiles("revocation")
 class RetentionPolicyDccRevocationTest {
 
@@ -71,23 +72,8 @@ class RetentionPolicyDccRevocationTest {
   }
 
   @Test
-  void shouldCheckForEtag() throws FetchDccListException {
-    when(dccRevocationListService.etagExists(dccRevocationClient.getETag())).thenReturn(true);
-    Assertions.assertThat(SpringApplication.exit(applicationContext));
-    retentionPolicy.run(null);
-    verify(s3RetentionPolicy, times(1)).deleteDccRevocationDir();
-  }
-
-  @Test
   void shouldThrowExceptionForEtag() throws FetchDccListException {
     when(dccRevocationClient.getETag()).thenThrow(FetchDccListException.class);
-    retentionPolicy.run(null);
-    verify(s3RetentionPolicy, times(0)).deleteDccRevocationDir();
-  }
-
-  @Test
-  void shouldThrowAnyException() throws FetchDccListException {
-    when(dccRevocationClient.getETag()).thenThrow(RuntimeException.class);
     retentionPolicy.run(null);
     verify(s3RetentionPolicy, times(0)).deleteDccRevocationDir();
   }
