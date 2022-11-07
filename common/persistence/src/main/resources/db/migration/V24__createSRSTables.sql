4dfbf8ad6742dfc6694876c4afee326b4acfe7a7
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2022-11-07 11:16:47 +0100

    ensure enough space for the submission_type

--- common/persistence/src/main/resources/db/migration/V24__createSRSTables.sql
+++ common/persistence/src/main/resources/db/migration/V24__createSRSTables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS self_report_submissions (
-    submission_type VARCHAR(30) NOT NULL,
+    submission_type VARCHAR(40) NOT NULL,
     submission_date DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
@@ -10,3 +10,8 @@ CREATE OR REPLACE VIEW self_reports AS
 		FROM self_report_submissions
 	GROUP BY submission_date, submission_type
 ;
+
+ALTER TABLE diagnosis_key ALTER COLUMN submission_type TYPE VARCHAR(40);
+ALTER TABLE federation_upload_key ALTER COLUMN submission_type TYPE VARCHAR(40);
+ALTER TABLE chgs_upload_key ALTER COLUMN submission_type TYPE VARCHAR(40);
+ALTER TABLE trace_time_interval_warning ALTER COLUMN submission_type TYPE VARCHAR(40);
