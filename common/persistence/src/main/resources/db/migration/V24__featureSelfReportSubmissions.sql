8169cf6e8c6fc704d2e587d103a3c942af5302d1
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2022-11-10 11:38:52 +0100

    SQL

--- common/persistence/src/main/resources/db/migration/V24__featureSelfReportSubmissions.sql
+++ common/persistence/src/main/resources/db/migration/V24__featureSelfReportSubmissions.sql
@@ -3,8 +3,6 @@ CREATE TABLE IF NOT EXISTS self_report_submissions (
     submission_date DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
-GRANT INSERT ON TABLE self_report_submissions TO "cwa_submission";
-
 CREATE OR REPLACE VIEW self_reports AS
 	SELECT COUNT(*), submission_date, submission_type
 		FROM self_report_submissions
