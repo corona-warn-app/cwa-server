95422443b25b9b943f9188eddc3c6a1e963325ad
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2023-02-06 16:11:49 +0100

    bump tomcat version

--- services/submission/src/main/java/app/coronawarn/server/services/submission/config/SubmissionPayloadSizeFilter.java
+++ services/submission/src/main/java/app/coronawarn/server/services/submission/config/SubmissionPayloadSizeFilter.java
@@ -1,12 +1,10 @@
-
-
 package app.coronawarn.server.services.submission.config;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -36,5 +34,4 @@ public class SubmissionPayloadSizeFilter extends OncePerRequestFilter {
       filterChain.doFilter(request, response);
     }
   }
-
 }
